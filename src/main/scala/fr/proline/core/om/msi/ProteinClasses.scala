4ac5588dfdad64aad5c4db0c7631c8a25a332eb4
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-01-10 18:43:00 +0000

    - Replace the Protein class definition by the BioSequence class definition - Encapsulates some contained optional objects in scala Option - ProteinMatch.resultSetId is now public - Revert the ProteinSet to a version which references a PeptideSet (this has to be discussed)

--- src/main/scala/fr/proline/core/om/msi/ProteinClasses.scala
+++ src/main/scala/fr/proline/core/om/msi/ProteinClasses.scala
@@ -3,18 +3,19 @@ package fr.proline.core.om.msi
 package ProteinClasses {
 
   import scala.collection.mutable.HashMap
-import fr.proline.core.om.msi.PeptideClasses.PeptideSet
-import fr.proline.core.om.msi.PeptideClasses.PeptideMatch
-import fr.proline.core.om.msi.PeptideClasses.Peptide
-import fr.proline.core.om.msi.ResultSetClasses.ResultSet
-import fr.proline.core.om.msi.MsiSearchClasses.SeqDatabase
-import fr.proline.core.om.msi.PeptideClasses.PeptideInstance
+  import fr.proline.core.om.msi.PeptideClasses.PeptideSet
+  import fr.proline.core.om.msi.PeptideClasses.PeptideMatch
+  import fr.proline.core.om.msi.PeptideClasses.Peptide
+  import fr.proline.core.om.msi.ResultSetClasses.ResultSet
+  import fr.proline.core.om.msi.MsiSearchClasses.SeqDatabase
+  import fr.proline.core.om.msi.PeptideClasses.PeptideInstance
   
-  class Protein ( // Required fields                     
+  class BioSequence ( // Required fields
                      val sequence: String,
                      
                      // Immutable optional fields
                      val id: Int = 0,
+                     val alphabet: String = "aa",
                      
                      // Mutable optional fields
                      var mass: Double = 0,
@@ -41,9 +42,10 @@ import fr.proline.core.om.msi.PeptideClasses.PeptideInstance
                      
                      // Mutable optional fields
                      private var peptideId: Int = 0,
-                     var peptide: Peptide = null,
+                     var peptide: Option[Peptide] = null,
+                     
                      private var bestPeptideMatchId: Int = 0,
-                     var bestPeptideMatch: PeptideMatch = null,
+                     var bestPeptideMatch: Option[PeptideMatch] = null,
                      
                      var properties : HashMap[String, Any] = new collection.mutable.HashMap[String, Any]
                      
@@ -51,28 +53,32 @@ import fr.proline.core.om.msi.PeptideClasses.PeptideInstance
     
     // Requirements
     require( start > 0 && end > start )
+    require( peptideId > 0 || peptide != null )
     
-    def getPeptideId : Int = {if(peptide != null) peptide.id else peptideId }
+    def getPeptideId : Int = { if(peptide != null && peptide != None) peptide.get.id else peptideId }
   
-    def getBestPeptideMatchId : Int = {if(bestPeptideMatch != null) bestPeptideMatch.id else bestPeptideMatchId }        
+    def getBestPeptideMatchId : Int = { if(bestPeptideMatch != null && bestPeptideMatch != None) bestPeptideMatch.get.id else bestPeptideMatchId }
     
   }
   
   class ProteinMatch ( 
-                     // Required fields                    
+                     // Required fields
                      val accession: String,
                      val description: String,
                      
                      // Immutable optional fields
+                     val isDecoy: Boolean = false,
                      
                      // Mutable optional fields                     
                      var id: Int = 0,
-                     private var proteinId: Int = 0,
-                     var protein: Protein = null,
-                     var taxonId: Int = 0,                     
-                     private var resultSetId: Int = 0,                     
+                     var taxonId: Int = 0,
+                     var resultSetId: Int = 0,
+                     
+                     private var bioSequenceId: Int = 0,
+                     var bioSequence: Option[BioSequence] = null,
+                     
                      private var seqDatabaseIds: Array[Int] = null,
-                     var seqDatabases: Array[SeqDatabase] = null,
+                     var seqDatabases: Option[Array[SeqDatabase]] = null,
                      
                      var score: Float = 0,
                      var scoreType: String = null,
@@ -87,43 +93,45 @@ import fr.proline.core.om.msi.PeptideClasses.PeptideInstance
     // Requirements
     require( accession != null && description != null )
   
-    def getProteinId : Int = {if(protein != null) protein.id else proteinId }
+    def getProteinId : Int = { if(bioSequence != null && bioSequence != None) bioSequence.get.id else bioSequenceId }
    
-    def getSeqDatabasesIds : Array[Int] = { if(seqDatabases != null) seqDatabases.map(_.id)  else seqDatabaseIds  }
+    def getSeqDatabasesIds : Array[Int] = { if(seqDatabases != null && seqDatabases != None) seqDatabases.get.map(_.id)  else seqDatabaseIds  }
     
   }
  
  
   class ProteinSet ( 
-                     // Required fields                    
-                     private val proteinMatchIds: Array[Int] = null,//One of these 2 values should be specified
-                     val proteinMatches: Array[ProteinMatch] = null ,
-                     
+                     // Required fields
+                     val peptideSet: PeptideSet,
+      
                      // Immutable optional fields
-                     val peptideInstances: Array[PeptideInstance] = null,
-                     val peptideInstanceIds: Array[Int] = null,
-                     val resultSummaryId: Int = 0,
-                      
+                     
                      // Mutable optional fields
                      var id: Int = 0,
+                     var resultSummaryId: Int = 0,
+                     
+                     private var proteinMatchIds: Array[Int] = null, //One of these 2 values should be specified
+                     var proteinMatches: Option[Array[ProteinMatch]] = null,
+                     
                      private var typicalProteinMatchId: Int = 0,
-                     var typicalProteinMatch: ProteinMatch = null,                    
+                     var typicalProteinMatch: Option[ProteinMatch] = null,
                      
                      var score: Float = 0,
                      var scoreType: String = null,
                      var isValidated: Boolean = true,
                      var selectionLevel: Int = 2,
+                     var hasPeptideSubset: Boolean = false,
 
                      var properties : HashMap[String, Any] = new collection.mutable.HashMap[String, Any]
                      
                      ) {
     
     // Requirements
-    require( proteinMatchIds != null || proteinMatches != null)
+    require( proteinMatchIds != null || proteinMatches != null )
     
-    def getProteinMatchIds : Array[Int] = { if(proteinMatches != null) proteinMatches.map(_.id)  else proteinMatchIds  }
+    def getProteinMatchIds : Array[Int] = { if(proteinMatches != null && proteinMatches != None) proteinMatches.get.map(_.id)  else proteinMatchIds  }
   
-    def getTypicalProteinMatchId : Int = {if(typicalProteinMatch != null) typicalProteinMatch.id else typicalProteinMatchId }
+    def getTypicalProteinMatchId : Int = { if(typicalProteinMatch != null && typicalProteinMatch != None) typicalProteinMatch.get.id else typicalProteinMatchId }
     
   }   
 }
