ccc3100813aaa143d811fd093e8afd6f67071594
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-01-10 15:57:00 +0000

    #4079  Move test code in test packages / JUnit

---
+++ src/test/scala/fr/proline/core/om/factory/sql/PeptideMatchLoaderTest.scala
@@ -0,0 +1,33 @@
+package fr.proline.core.om.factory
+
+import org.junit._
+import Assert._
+import fr.proline.core.om.msi.PeptideClasses._
+import net.noerd.prequel.DatabaseConfig
+import net.noerd.prequel.IsolationLevels
+    
+@Test
+class PeptideMatchLoaderTest {
+      
+    var database : DatabaseConfig = null
+    
+    @Before
+    def initialize() = {
+    	database = DatabaseConfig(
+        driver = "org.sqlite.JDBC",
+        jdbcURL = "jdbc:sqlite:msi-db.sqlite",
+        isolationLevel = IsolationLevels.Serializable
+      )
+    }
+   
+	@Test
+    def testLoadPeptideMatches() = {
+//	   
+    val pepMatchLoader = new PeptideMatchLoader(msiDb = database, psDb = database)
+    val pepMatches = pepMatchLoader.getPeptideMatches(rsIds = Array(1,2,3,4))
+    println(pepMatches.length)
+//    assertEquals(12, pepMatches.length )
+	  	 assertTrue(true)
+	}
+	
+}
