b1cab27619cf1d2d579cd0ba8781117f4af88338
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-01-19 07:49:01 +0000

    #4079 renomme deltaMz en deltaMoz

--- src/test/scala/fr/proline/core/om/msi/PeptidesTest.scala
+++ src/test/scala/fr/proline/core/om/msi/PeptidesTest.scala
@@ -14,8 +14,8 @@ class PeptidesTest {
 	  
 	  val pep1 = new Peptide( id = 0, sequence = "STLLIR", ptmString = "", ptms = null, calculatedMass = 100  )
 	  val pepInst1 = new PeptideInstance( id = 0, peptide = pep1, peptideMatchIds = Array(1,2) )	  
-	  val pepMatch1 = new PeptideMatch( id = 1, rank=1, score = 45.5f, scoreType="Mascot", deltaMz=0.05, isDecoy= false, peptide=pep1,missedCleavage=1  )
-	  val pepMatch2 = new PeptideMatch( id = 2, rank=2, score = 13.21f, scoreType="Mascot", deltaMz=0.15, isDecoy= false, peptide=pep1,missedCleavage=1 )
+	  val pepMatch1 = new PeptideMatch( id = 1, rank=1, score = 45.5f, scoreType="Mascot", deltaMoz=0.05, isDecoy= false, peptide=pep1,missedCleavage=1  )
+	  val pepMatch2 = new PeptideMatch( id = 2, rank=2, score = 13.21f, scoreType="Mascot", deltaMoz=0.15, isDecoy= false, peptide=pep1,missedCleavage=1 )
 	  
 	  assertEquals(pepInst1.getPeptideMatchIds.length, 2);
 	}
@@ -24,8 +24,8 @@ class PeptidesTest {
     def testGetPeptideMatchIDs2() = {
 	  	  
 	  val pep1 = new Peptide( id = 0, sequence = "STLLIR", ptmString = "", ptms = null, calculatedMass = 100 )	 	 
-	  val pepMatch1 = new PeptideMatch( id = 1, rank=1, score = 45.5f, scoreType="Mascot", deltaMz=0.05, isDecoy= false, peptide=pep1, missedCleavage=1  )
-	  val pepMatch2 = new PeptideMatch( id = 2, rank=2, score = 13.21f, scoreType="Mascot", deltaMz=0.15, isDecoy= false, peptide=pep1, missedCleavage=1 )
+	  val pepMatch1 = new PeptideMatch( id = 1, rank=1, score = 45.5f, scoreType="Mascot", deltaMoz=0.05, isDecoy= false, peptide=pep1, missedCleavage=1  )
+	  val pepMatch2 = new PeptideMatch( id = 2, rank=2, score = 13.21f, scoreType="Mascot", deltaMoz=0.15, isDecoy= false, peptide=pep1, missedCleavage=1 )
 	  val pepInst1 = new PeptideInstance( id = 0, peptide = pep1, peptideMatches = Array(pepMatch1, pepMatch2) )
 	  
 	  assertEquals(pepInst1.getPeptideMatchIds.length, 2);
