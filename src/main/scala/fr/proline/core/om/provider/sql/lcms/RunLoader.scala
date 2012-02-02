378b7c277b649f8c5ec2182b3f2d2c9af2179404
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-02-02 10:42:48 +0000

    - Add some loaders for LCMS object model

---
+++ src/main/scala/fr/proline/core/om/provider/sql/lcms/RunLoader.scala
@@ -0,0 +1,104 @@
+package fr.proline.core.om.provider.sql.lcms
+
+import fr.proline.core.LcmsDb
+
+class RunLoader( val lcmsDb: LcmsDb )  {
+  
+  import scala.collection.mutable.ArrayBuffer
+  import fr.proline.core.om.helper.SqlUtils._
+  import fr.proline.core.om.lcms.RunClasses._
+  
+  def getRuns( runIds: Seq[Int] ): Array[LcmsRun] = {
+    
+    val scans = this.getScans( runIds )
+    // Group scans by run id
+    val scansByRunId = scans.groupBy(_.runId)
+    
+    // TODO: load related raw file and instrument
+    
+    val runs = new Array[LcmsRun](runIds.length)    
+    var colNames: Seq[String] = null
+    var runIdx = 0
+    
+    // Load runs
+    val lcmsDbTx = lcmsDb.getOrCreateTransaction()
+    lcmsDbTx.select( "SELECT * FROM run WHERE id IN (" + runIds.mkString(",") + ")" ) { r =>
+        
+        if( colNames == null ) { colNames = r.columnNames }
+        
+        // Build the run record
+        val runRecord = colNames.map( colName => ( colName -> r.nextObject.get ) ).toMap
+        val runId = runRecord("id").asInstanceOf[Int]
+        val runScans = scansByRunId(runId)
+        
+        // Build the run
+        runs(runIdx) = buildRunMap(runRecord, runScans )
+        runIdx += 1
+        
+        ()
+      }
+    
+    runs
+  }
+  
+  def buildRunMap( runRecord: Map[String,Any], scans: Array[LcmsScan] ): LcmsRun = {
+    
+    import java.util.Date
+    
+    new LcmsRun( id = runRecord("id").asInstanceOf[Int],
+                 rawFileName = runRecord("raw_file_path").asInstanceOf[String],// raw_file_name
+                 minIntensity = runRecord.getOrElse("min_intensity",Double.NaN).asInstanceOf[Double],
+                 maxIntensity = runRecord.getOrElse("max_intensity",Double.NaN).asInstanceOf[Double],
+                 ms1ScanCount = runRecord.getOrElse("ms1_scan_count",0).asInstanceOf[Int],
+                 ms2ScanCount = runRecord.getOrElse("ms2_scan_count",0).asInstanceOf[Int],
+                 rawFile = null,
+                 scans = scans
+               )
+  }
+  
+  def getScans( runIds: Seq[Int] ): Array[LcmsScan] = {
+    
+    val lcmsDbTx = lcmsDb.getOrCreateTransaction()
+    val nbScans: Int = lcmsDbTx.selectInt( "SELECT count(*) FROM scan WHERE run_id IN (" + runIds.mkString(",") + ")" )
+    val scans = new Array[LcmsScan](nbScans)    
+    var colNames: Seq[String] = null
+    var lcmsScanIdx = 0
+    
+    // Load scans
+    lcmsDbTx.select( "SELECT * FROM scan WHERE run_id IN (" + runIds.mkString(",") +")" ) { r =>
+        
+        if( colNames == null ) { colNames = r.columnNames }
+        
+        // Build the scan record
+        val scanRecord = colNames.map( colName => ( colName -> r.nextObject.get ) ).toMap
+        
+        // Build the scan
+        scans(lcmsScanIdx) = buildLcmsScan( scanRecord )
+        lcmsScanIdx += 1
+        
+        ()
+      }
+    
+    scans
+  }
+  
+  def buildLcmsScan( scanRecord: Map[String,Any] ): LcmsScan = {
+    
+    val precursorMoz = scanRecord.getOrElse("precursor_moz",Double.NaN).asInstanceOf[Double]
+    val precursorCharge = scanRecord.getOrElse("precursor_charge",0).asInstanceOf[Int]
+    
+    new LcmsScan( id = scanRecord("id").asInstanceOf[Int],
+                  initialId = scanRecord("initial_id").asInstanceOf[Int],
+                  cycle = scanRecord("cycle").asInstanceOf[Int],
+                  time = scanRecord("time").asInstanceOf[Double].toFloat,
+                  msLevel = scanRecord("ms_level").asInstanceOf[Int],
+                  tic = scanRecord("tic").asInstanceOf[Double],
+                  basePeakMoz = scanRecord("base_peak_moz").asInstanceOf[Double],
+                  basePeakIntensity = scanRecord("base_peak_intensity").asInstanceOf[Double],
+                  runId = scanRecord("run_id").asInstanceOf[Int],
+                  precursorMoz = precursorMoz,
+                  precursorCharge = precursorCharge
+                 )
+  }
+
+}
---
+++ src/main/scala/fr/proline/core/om/provider/sql/lcms/RunMapLoader.scala
@@ -0,0 +1,145 @@
+package fr.proline.core.om.provider.sql.lcms
+
+import fr.proline.core.LcmsDb
+
+class RunMapLoader( val lcmsDb: LcmsDb,
+                    val loadPeaks: Boolean = false ) extends IMapLoader {
+  
+  import scala.collection.mutable.ArrayBuffer
+  import fr.proline.core.om.helper.SqlUtils._
+  import fr.proline.core.om.lcms.FeatureClasses._
+  import fr.proline.core.om.lcms.MapClasses._
+  
+  val peakPickingSoftwareById = lcmsDbHelper.getPeakPickingSoftwareById()
+  val peakelFittingModelById = lcmsDbHelper.getPeakelFittingModelById()
+  
+  /** Returns a list of features corresponding to a given list of run map ids */
+  def getMaps( runMapIds: Seq[Int] ): Array[RunMap] = {
+    
+    val features = this.getFeatures( runMapIds )
+    // Group features by map id
+    val featuresByMapId = features.groupBy(_.mapId)
+    
+    val runMaps = new Array[RunMap](runMapIds.length)    
+    var colNames: Seq[String] = null
+    var lcmsMapIdx = 0
+    
+    // Load processed map features
+    lcmsDbTx.selectAndProcess( 
+      "SELECT map.*, run_map.run_id, run_map.peak_picking_software_id, run_map.peakel_fitting_model_id FROM run_map, map " +
+      "WHERE run_map.id IN (" + runMapIds.mkString(",")+") " +
+      "AND map.id = run_map.id " ) { r =>
+        
+      if( colNames == null ) { colNames = r.columnNames }
+      
+      // Build the map record
+      val mapRecord = colNames.map( colName => ( colName -> r.nextObject.get ) ).toMap
+      
+      val mapId = mapRecord("id").asInstanceOf[Int]
+      val featureScoringId = mapRecord("feature_scoring_id").asInstanceOf[Int]
+      val peakPickingSoftwareId = mapRecord("peak_picking_software_id").asInstanceOf[Int]
+      val peakelFittingModelId = mapRecord("peakel_fitting_model_id").asInstanceOf[Int]
+      
+      val mapFeatures = featuresByMapId( mapId )
+      val featureScoring = featureScoringById.getOrElse(featureScoringId,null)
+      val peakPickingSoftware = peakPickingSoftwareById(peakPickingSoftwareId)
+      val peakelFittingModel = peakelFittingModelById.getOrElse(peakelFittingModelId,null)        
+      
+      // Build the map
+      runMaps(lcmsMapIdx) = buildRunMap(mapRecord, mapFeatures, featureScoring, peakPickingSoftware, peakelFittingModel )
+      lcmsMapIdx += 1
+      
+      ()
+    }
+    
+    runMaps
+    //val features = mapFeatureLoader.asInstanceOf[RunMapFeatureLoader].getFeatures( runMapIds )
+    
+    //super.getMaps( mapRecords, features)
+    
+  }
+  
+  def buildRunMap( mapRecord: Map[String,Any], features: Array[Feature], featureScoring: FeatureScoring,
+                   peakPickingSoftware: PeakPickingSoftware, peakelFittingModel: PeakelFittingModel ): RunMap = {
+    
+    import java.util.Date
+    
+    new RunMap(  id = mapRecord("id").asInstanceOf[Int],
+                 name = mapRecord("name").asInstanceOf[String],
+                 isProcessed = false,
+                 creationTimestamp = new Date(), // TODO: parse date
+                 features = features,
+                 runId = mapRecord("run_id").asInstanceOf[Int],
+                 peakPickingSoftware = peakPickingSoftware,
+                 description = mapRecord("description").asInstanceOf[String],
+                 featureScoring = featureScoring,
+                 peakelFittingModel = peakelFittingModel
+               )
+  }
+  
+  /** Returns a list of features corresponding to a given list of run map ids */
+  def getFeatures( mapIds: Seq[Int] ): Array[Feature] = {
+    
+    // Check that provided map ids correspond to run maps
+    val nbMaps = lcmsDbTx.selectInt( "SELECT count(*) FROM run_map WHERE id IN (" + mapIds.mkString(",") + ")" )  
+    if( nbMaps < mapIds.length ) {
+      throw new Exception("map ids must correspond to existing run maps");
+    }
+    
+    // Load run ids
+    val runIds = lcmsDbTx.select( "SELECT run_id FROM run_map WHERE id IN (" + mapIds.mkString(",") + ")" ) { _.nextInt.get }
+    
+    // Load mapping between scan ids and scan initial ids
+    val scanInitialIdById = lcmsDbHelper.getScanInitialIdById( runIds ) 
+    
+    // Retrieve mapping between features and MS2 scans
+    val ms2EventIdsByFtId = lcmsDbHelper.getMs2EventIdsByFtId( mapIds )
+    
+    // TODO: load isotopic patterns if needed
+    //val ipsByFtId = if( loadPeaks ) getIsotopicPatternsByFtId( mapIds ) else null
+    
+    // Retrieve mapping between overlapping features
+    val olpFtIdsByFtId = getOverlappingFtIdsByFtId( mapIds )
+
+    var olpFeatureById: Map[Int,Feature] = null
+    if( olpFtIdsByFtId.size > 0 ) {
+      olpFeatureById = getOverlappingFeatureById( mapIds, scanInitialIdById, ms2EventIdsByFtId )
+    }
+
+    val ftBuffer = new ArrayBuffer[Feature]
+    
+    eachFeatureRecord( mapIds, ftRecord => {
+      val ftId = ftRecord("id").asInstanceOf[Int]
+        
+      // Try to retrieve overlapping features
+      var olpFeatures: Array[Feature] = null
+      if( olpFtIdsByFtId.contains(ftId) ) {
+        olpFeatures = olpFtIdsByFtId(ftId) map { olpFtId => olpFeatureById(olpFtId) }
+      }
+      
+      // TODO: load isotopic patterns if needed
+      
+      val feature = buildRunMapFeature( ftRecord, scanInitialIdById, ms2EventIdsByFtId, null, olpFeatures )
+      
+      ftBuffer += feature
+    })
+
+    ftBuffer.toArray
+    
+  }
+  
+  /** Builds a feature object coming from a run map */
+  def buildRunMapFeature( featureRecord: Map[String,Any],
+                          scanInitialIdById: Map[Int,Int],
+                          ms2EventIdsByFtId: Map[Int,Array[Int]],
+                          isotopicPatterns: Option[Array[IsotopicPattern]],
+                          overlappingFeatures: Array[Feature]
+                          ): Feature = {
+    
+    val mapId = featureRecord("map_id").asInstanceOf[Int]
+    buildFeature( featureRecord, scanInitialIdById, ms2EventIdsByFtId, isotopicPatterns, overlappingFeatures, mapId )
+
+  }
+
+
+}
