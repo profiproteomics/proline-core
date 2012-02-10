a7030c867e44cac48184431218bbc36db0199b88
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-02-10 10:51:36 +0000

    #4079 OM Reorganization : loader / builder / ...

--- src/main/scala/fr/proline/core/om/builder/PtmDefinitionBuilder.scala
+++ src/main/scala/fr/proline/core/om/builder/PtmDefinitionBuilder.scala
@@ -1,12 +1,20 @@
-package fr.proline.core.om.provider.sql
+package fr.proline.core.om.builder
 
 import fr.proline.core.om.msi.PtmClasses.PtmDefinition
 import fr.proline.core.om.msi.PtmClasses.PtmEvidence
 import fr.proline.core.om.msi.PtmClasses.PtmNames
 import fr.proline.core.om.msi.PtmClasses.LocatedPtm
 
-object PtmDefinitionBuilder {
+object PtmDefinitionBuilder {  
 
+  /**
+   * Create a PtmDefinition using corresponding information
+   *  - ptmRecord : contains value for ptm properties "id" for ptm_id, "short_name", "full_name" for Ptm Names
+   *  - ptmSpecifRecord : contains value for ptmDefinition properties "residue", "id", "location"
+   *  - ptmEvidenceRecords :List of map:  contains value for properties "type", "composition", "mono_mass", "average_mass","is_required" for each ptmEvidence 
+   *  - ptmClassification : name of classification,
+   *  
+   */
   def buildPtmDefinition( ptmRecord: Map[String,Any],
                           ptmSpecifRecord: Map[String,Any],                          
                           ptmEvidenceRecords: Seq[Map[String,Any]],
@@ -31,7 +39,7 @@ object PtmDefinitionBuilder {
     
     val residueStr = ptmSpecifRecord("residue").asInstanceOf[String];
     val resChar = if( residueStr != null ) residueStr.charAt(0) else '\0'
-    
+        
     return new PtmDefinition(
                           id = ptmSpecifRecord("id").asInstanceOf[Int],
                           ptmId = ptmRecord("id").asInstanceOf[Int],
@@ -44,7 +52,7 @@ object PtmDefinitionBuilder {
                           )
   }
 
-  def buildLocatedPtm( pepSeq: String, ptmDef: PtmDefinition, seqPos: Int ): LocatedPtm = {
+  def buildLocatedPtm( ptmDef: PtmDefinition, seqPos: Int ): LocatedPtm = {
     
     var( isNTerm, isCTerm ) = ( false, false )
     
@@ -76,40 +84,4 @@ object PtmDefinitionBuilder {
                      isCTerm = isCTerm )
 
   }
-  
-/*
-##############################################################################
-# Method: build_located_ptm()
-# TODO: put this method in the peptide class
-#
-method build_located_ptm( Str $pep_sequence, Object $ptm_definition!, Int $seq_pos! ) {
-  
-  my $precursor_delta = $ptm_definition->precursor_delta;
-  my %located_ptm_attrs = ( definition => $ptm_definition,
-                            seq_position => $seq_pos,
-                            mono_mass => $precursor_delta->mono_mass,
-                            average_mass => $precursor_delta->average_mass,
-                            composition => $precursor_delta->composition,
-                          );
-  
-  ### N-term locations are: Any N-term or Protein N-term
-  if( $ptm_definition->location =~ /.+N-term$/ ) {
-    die "sequence position must be '0' because it's a N-Term PTM" if $seq_pos != 0;
-    
-    $located_ptm_attrs{is_nterm} = 1;    
-    }
-  ### C-term locations are: Any C-term, Protein C-term
-  elsif( $ptm_definition->location =~ /.+C-term$/ ) {
-    #my $nb_residues = length($pep_sequence);
-    #die "sequence postion must be '$nb_residues' because it's a C-Term PTM" if $seq_pos != $nb_residues;
-    die "sequence position must be '-1' because it's a C-Term PTM" if $seq_pos != -1;
-    
-    $located_ptm_attrs{is_cterm} = 1;
-    }
-  
-  ### Build a located ptm
-  my $located_ptm = new Pairs::Msi::Model::LocatedPtm( %located_ptm_attrs );
-  
-  return $located_ptm;
-  }*/
 }
