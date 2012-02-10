8e149771e546662e2abc429f457fbc9403a21a73
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-02-10 10:17:51 +0000

    - Add a service draft for feature clustering of LCMS maps

---
+++ src/main/scala/fr/proline/core/service/lcms/ClusterizeMapFeatures.scala
@@ -0,0 +1,96 @@
+package fr.proline.core.service.lcms
+
+/*import java.sql.Connection
+*/
+
+import fr.proline.core.om.helper.SqlUtils._
+import fr.proline.core.LcmsDb
+import fr.proline.core.service.IService
+import fr.proline.core.om.lcms.MapClasses._
+import fr.proline.core.om.lcms.FeatureClasses._
+import fr.proline.core.om.provider.sql.lcms.RunLoader
+import fr.proline.core.om.storer.ProcessedMapStorer
+import fr.proline.core.algo.lcms.FeatureClusterer
+import fr.proline.core.algo.lcms.ClusteringParams
+
+class ClusterizeMapFeatures( lcmsDb: LcmsDb, lcmsMap: ProcessedMap, params: ClusteringParams ) extends IService {
+  
+  val boolStrAsInt = lcmsDb.boolStrAsInt
+  val maxNbIters = lcmsDb.maxVariableNumber
+  var featuresWithClusters: Array[Feature] = null
+  
+  def getFeaturesWithClusters = featuresWithClusters
+  
+  def runService(): Boolean = {
+    
+    // Retrieve database transaction
+    val lcmsDbTx = lcmsDb.getOrCreateTransaction()
+    
+    if( ! lcmsMap.isProcessed ) throw new Exception( "the map must be a processed map" )
+    
+    val runMapIds = lcmsMap.runMapIds
+    if( runMapIds.length > 1 ) throw new Exception( "the processed map must correspond to a unique run map" )
+    
+    val processedMapId = lcmsMap.id
+    val runMapId = runMapIds(0)
+    
+    // Retrieve run id corresponding to run map id
+    val runId = lcmsDbTx.selectInt( "SELECT run_id FROM run_map WHERE id = " + runMapId )
+    
+    // Retrieve corresponding scans
+    val runLoader = new RunLoader( lcmsDb )
+    val scans = runLoader.getScans( Array(runId) )
+    
+    // Remove existing cluster from the processed map
+    val lcmsMapWithoutClusters = lcmsMap.copyWithoutClusters()
+    
+    // Perform the feature clustering
+    val ftClusterer = new FeatureClusterer()
+    val lcmsMapWithClusters = ftClusterer.clusterizeFeatures( lcmsMapWithoutClusters, scans, params )
+    
+    // Retrieve feature cluster ids if they exist
+    val existingFtClusterIds = lcmsDbTx.select("SELECT cluster_feature_id FROM feature_cluster_item " +
+                                               "WHERE processed_map_id="+ processedMapId +" " +
+                                               "GROUP BY cluster_feature_id") { _.nextInt.get }
+    
+    if( existingFtClusterIds.length > 0 ) {
+      
+      // Delete existing clusters from this processed map
+      println( "delete existing feature clusters..." )
+      existingFtClusterIds.grouped(maxNbIters).foreach( tmpFtIds => {    
+        lcmsDbTx.execute("DELETE FROM feature WHERE id IN (" + tmpFtIds.mkString(",") +")")
+        lcmsDbTx.execute("DELETE FROM processed_map_feature_item WHERE feature_id IN (" + tmpFtIds.mkString(",") +")")
+      })
+      
+      // Delete existing feature_cluster_items for this map
+      lcmsDbTx.execute( "DELETE FROM feature_cluster_item WHERE processed_map_id = " + processedMapId )
+      
+      // Set all sub-features of the map as not clusterized
+      lcmsDbTx.execute( "UPDATE processed_map_feature_item SET is_clusterized = " + BoolToSQLStr(false,boolStrAsInt) +
+                        " WHERE processed_map_id = " + processedMapId ) //+
+                        //" AND is_clusterized = " + BoolToSQLStr(true,boolStrAsInt) )
+    
+    } else println( "no feature cluster detected..." )
+    
+    // Store the feature clusters
+    val processedMapStorer = ProcessedMapStorer( lcmsDb )
+    processedMapStorer.storeFeatureClusters( lcmsMapWithClusters.features )
+    
+    /*
+    // TODO: Update map modification time
+    Pairs::Lcms::RDBO::Map::Manager.updateMaps(
+      set = { modification_timestamp = time },
+      where = ( id = processedMapId ),
+      db = lcmsRdb
+      )*/
+    
+    // Update service results
+    featuresWithClusters = lcmsMapWithClusters.features
+    
+    // Exit the service with a success status
+    true
+    
+  }
+
+  
+}
