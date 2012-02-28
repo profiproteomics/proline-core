32f3c2365b12cd0962c6f53e52dec6d336f1e76c
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-02-28 09:33:44 +0000

    - Add a trait which abstracts a ResultFile

---
+++ src/main/scala/fr/proline/core/om/msi/IResultFile.scala
@@ -0,0 +1,33 @@
+package fr.proline.core.om.msi
+
+import fr.proline.core.om.msi.MsQueryClasses._
+import fr.proline.core.om.msi.ResultSetClasses._
+  
+class Spectrum() // TO define somewhere else
+
+trait IResultFile {
+  
+  var msLevel: Int
+  var msQueryById: Map[Int,MsQuery]
+  var hasDecoyResultSet: Boolean
+  var hasMs2Peaklist: Boolean
+  
+  def getResultSet( wantDecoy: Boolean ): Object
+  def eachSpectrum( onEachSpectrum: Spectrum => Unit ): Unit
+
+}
+
+class ResultFileImplFake extends IResultFile {
+  
+  var msLevel: Int = 2
+  var msQueryById: Map[Int,MsQuery] = null
+  var hasDecoyResultSet: Boolean = true
+  var hasMs2Peaklist: Boolean = true
+  
+  def getResultSet( wantDecoy: Boolean ): Object = { new Object }
+  
+  def eachSpectrum( onEachSpectrum: Spectrum => Unit ): Unit = {
+    val spec = new Spectrum
+    onEachSpectrum(spec)
+  }
+}
