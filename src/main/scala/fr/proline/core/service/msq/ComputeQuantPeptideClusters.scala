a3f6d0d559ea263ccbc472a1d45fca9e75bc0aff
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-02-29 18:37:48 +0000

    - Prototype services relative to quantitation

---
+++ src/main/scala/fr/proline/core/service/msq/ComputeQuantPeptideClusters.scala
@@ -0,0 +1,11 @@
+package fr.proline.core.service.msq
+
+import fr.proline.core.service.IService
+
+class ComputeQuantPeptideClusters extends IService {
+  
+  def runService() = {
+    false
+  }
+
+}
---
+++ src/main/scala/fr/proline/core/service/msq/QuantifyFraction.scala
@@ -0,0 +1,11 @@
+package fr.proline.core.service.msq
+
+import fr.proline.core.service.IService
+
+class QuantifyFraction extends IService {
+  
+  def runService() = {
+    false
+  }
+
+}
