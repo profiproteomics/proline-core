44a5676146a2bb25cb95650c90ef5d66be444ae6
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-03-12 12:52:24 +0000

    #4531 : Definition du service pour le parse

--- src/main/scala/fr/proline/core/om/model/msi/IResultFile.scala
+++ src/main/scala/fr/proline/core/om/model/msi/IResultFile.scala
@@ -1,5 +1,5 @@
 package fr.proline.core.om.model.msi
-
+import java.io.File
   
 class Spectrum() // TO define somewhere else
 
@@ -9,23 +9,34 @@ trait IResultFile {
   var msQueryById: Map[Int,MsQuery]
   var hasDecoyResultSet: Boolean
   var hasMs2Peaklist: Boolean
+  var providerKey : String
   
-  def getResultSet( wantDecoy: Boolean ): Object
+  def getResultSet( wantDecoy: Boolean ): ResultSet
   def eachSpectrum( onEachSpectrum: Spectrum => Unit ): Unit
+  def parseFile(identFile : File) : Boolean
 
 }
+ 
+ object ResultFileFactory {   
 
-class ResultFileImplFake extends IResultFile {
-  
-  var msLevel: Int = 2
-  var msQueryById: Map[Int,MsQuery] = null
-  var hasDecoyResultSet: Boolean = true
-  var hasMs2Peaklist: Boolean = true
-  
-  def getResultSet( wantDecoy: Boolean ): Object = { new Object }
-  
-  def eachSpectrum( onEachSpectrum: Spectrum => Unit ): Unit = {
-    val spec = new Spectrum
-    onEachSpectrum(spec)
-  }
+   import scala.collection.mutable.HashMap
+ 
+   private val resultFilePerFormat:HashMap[String, IResultFile]= new HashMap[String , IResultFile]()
+   
+   /**
+    * Register specified ResultFile for specified format. 
+    * return an Option containing previous ResultFile associated to format if exist None otherwise
+    * 
+    */
+   def registerResultFile(format: String, resultFile:IResultFile) : Option[IResultFile] = {     
+		  resultFilePerFormat.put(format, resultFile)
+   }
+   
+   /**
+    * Return an Option containing the ResultFile for specified format if exist, None otherwise
+    */
+   def getResultFile(identificationFormat: String) :Option[IResultFile]= {    
+       resultFilePerFormat.get(identificationFormat)
+   }
 }
+ 
