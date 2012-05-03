57e2aeca65650c048694733ae812e35a461f3b52
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-05-03 14:37:42 +0000

    Move Provider implem from Proline-ORM to Proline-DAL

---
+++ Proline-DAL/src/test/scala/fr/proline/core/om/provider/msi/impl/ORMPeptideProviderTest.scala
@@ -0,0 +1,95 @@
+package fr.proline.core.om.provider.msi.impl
+
+import org.hamcrest.CoreMatchers
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+import org.junit.Before
+import org.junit.After
+import org.junit.Assert._
+import fr.proline.core.om.model.msi.Peptide
+import fr.proline.core.orm.utils.JPAUtil
+import fr.proline.core.om.utils.DatabaseTestCase
+import scala.collection.mutable.ArrayBuffer
+import fr.proline.core.om.model.msi.LocatedPtm
+import scala.collection.mutable.ArrayBuilder
+import fr.proline.core.om.model.msi.PtmEvidence
+import fr.proline.core.om.model.msi.IonTypes
+import fr.proline.core.om.model.msi.PtmDefinition
+import fr.proline.core.om.model.msi.PtmNames
+//import fr.proline.core.om.provider.msi.impl.ORMPeptideProvider
+
+@Test
+class ORMPeptideProviderTest extends DatabaseTestCase {
+
+  var ormPepProvider: ORMPeptideProvider = null
+  private val SEQ_TO_FOUND : String = "LTGMAFR"
+    
+  
+  override def getSQLScriptLocation() : String  ={
+	  return "/dbscripts/ps/h2";
+  }
+  
+  @Before 
+  @throws(classOf[Exception])
+  def setUp()  = {
+	  initDatabase()
+	  initEntityManager(JPAUtil.PersistenceUnitNames.PS_Key.getPersistenceUnitName())
+	  loadDataSet("/fr/proline/core/om/ps/Unimod_Dataset.xml")
+	  ormPepProvider = new ORMPeptideProvider(em);
+  }
+  
+  @After 
+  @throws(classOf[Exception]) 
+  override def tearDown() ={
+		super.tearDown();
+  }
+  
+  @Test
+  def getSinglePeptide()= {
+	  val pep : Option[Peptide] = ormPepProvider.getPeptide(4);
+  	  assertThat(pep, CoreMatchers.notNullValue());
+  	  assertNotSame(pep, None);		
+  	  assertThat(pep.get.calculatedMass, CoreMatchers.equalTo(810.405807));
+  }
+  
+  @Test
+  def getMultiplePeptides() = {
+	 val ids = new ArrayBuffer[Int]
+  	 ids += 0 
+  	 ids += 1
+  	 ids += 4
+  	 
+	 val peps : Array[Option[Peptide]] = ormPepProvider.getPeptides(ids)
+	 assertThat(peps, CoreMatchers.notNullValue())
+	 assertThat(peps.length, CoreMatchers.equalTo(3))
+	 assertThat(peps.apply(2).get.id, CoreMatchers.equalTo(4))
+	 assertThat(peps(2).get.calculatedMass, CoreMatchers.equalTo(810.405807))
+  }
+  
+  @Test
+  def getPeptideOnSeqAndNoPtms() =  {
+	 val ptms = new Array[LocatedPtm](0)
+	 val pep : Option[Peptide] = ormPepProvider.getPeptide(SEQ_TO_FOUND, ptms);
+	 assertThat(pep, CoreMatchers.notNullValue());
+	 assertNotSame(pep, None);	
+	 assertTrue(pep.get.ptms == null ||  pep.get.ptms.length==0  );
+  }
+	
+  @Test
+  def getPeptideOnSeqAndPtms() = {		
+	  var ptmsBuilder = Array.newBuilder[LocatedPtm]
+		
+	  val ptmEvi : PtmEvidence = new PtmEvidence(IonTypes.Precursor,"",  Double.MaxValue, Double.MaxValue, false)
+	  val ptmEvidences = Array[PtmEvidence](ptmEvi)
+	  
+	  val ptmDef = new PtmDefinition(0, "ANYWHERE", new PtmNames("Oxidation", "Oxidation or Hydroxylation"), ptmEvidences, 'M', null, 0);
+	  ptmsBuilder += new LocatedPtm(ptmDef, 3, Double.MaxValue, Double.MaxValue, "O", false, false)
+		
+	  val pep : Option[Peptide] = ormPepProvider.getPeptide(SEQ_TO_FOUND, ptmsBuilder.result());
+	  assertThat(pep, CoreMatchers.notNullValue());
+	  assertNotSame(pep, None$.MODULE$);	
+	  assertTrue(pep.get.ptms.length== 1);
+	  assertThat(pep.get.ptms(0).seqPosition, CoreMatchers.equalTo(3));
+	}
+  
+}
