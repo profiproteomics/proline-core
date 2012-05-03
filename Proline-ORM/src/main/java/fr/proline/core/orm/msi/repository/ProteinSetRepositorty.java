c1044710d1a456fd3e2b3778bde5a495415350bd
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-05-03 14:00:19 +0000

    ProteinSet Aggregat Repository

---
+++ Proline-ORM/src/main/java/fr/proline/core/orm/msi/repository/ProteinSetRepositorty.java
@@ -0,0 +1,27 @@
+package fr.proline.core.orm.msi.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import fr.proline.core.orm.msi.PeptideInstance;
+import fr.proline.core.orm.utils.JPARepository;
+
+public class ProteinSetRepositorty extends JPARepository {
+
+	public  ProteinSetRepositorty(EntityManager em) {
+		super(em);	
+	}
+	
+	public PeptideInstance findPeptideInstanceForPepMatch(int pepMatchID){
+		TypedQuery<PeptideInstance> query = getEntityManager().createNamedQuery("findPepInstByPepMatch", PeptideInstance.class);
+		query.setParameter("pmID", pepMatchID);
+		return query.getSingleResult();
+	}
+	
+	public PeptideInstance findPeptideInstanceForPeptide(int pepID){
+		TypedQuery<PeptideInstance> query = getEntityManager().createNamedQuery("findPepInstForPeptideId", PeptideInstance.class);
+		query.setParameter("pepID", pepID);
+		return query.getSingleResult();
+	}
+
+}
