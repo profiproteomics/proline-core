c3ec2d071b46808a05518a5a0fc1244dc8a9b80a
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-05-25 14:25:49 +0000

    Fixes : Come back to multiple queries to get nonexisting values (else missing Option.None) Implements NYI methods Use Ptm Location Enum as param Catch PersistenceException when executing query => return Option.None

--- Proline-DAL/src/main/scala/fr/proline/core/om/provider/msi/impl/ORMPTMProvider.scala
+++ Proline-DAL/src/main/scala/fr/proline/core/om/provider/msi/impl/ORMPTMProvider.scala
@@ -1,47 +1,83 @@
 package fr.proline.core.om.provider.msi.impl
 
-import fr.proline.core.om.provider.msi.IPTMProvider
-import scala.collection.JavaConversions.collectionAsScalaIterable
-import scala.collection.JavaConverters.asJavaCollectionConverter
 import fr.proline.core.om.model.msi.PtmDefinition
-import fr.proline.core.orm.ps.repository.PtmRepository
-import javax.persistence.EntityManager
+import fr.proline.core.om.model.msi.PtmLocation
+import fr.proline.core.om.provider.msi.IPTMProvider
 import fr.proline.core.om.utils.OMConverterUtil
+import fr.proline.core.orm.ps.repository.PtmRepository
+import fr.proline.core.orm.ps.PtmSpecificity
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceException
+import com.weiglewilczek.slf4s.Logging
 
-class ORMPTMProvider (val em:EntityManager )  extends IPTMProvider {
+
+
+class ORMPTMProvider (val em:EntityManager )  extends IPTMProvider with Logging {
 
   var ptmRepo = new PtmRepository(em) //Created by constructor
 
   def getPtmDefinitions(ptmDefIds: Seq[Int]): Array[Option[PtmDefinition]] = {
     
-	  val converter = new OMConverterUtil()
-    var foundPtmDefBuilder = Array.newBuilder[Option[PtmDefinition]]
+	val converter = new OMConverterUtil()
+	var foundPtmDefBuilder = Array.newBuilder[Option[PtmDefinition]]
 	  
-	  // TODO: find a better syntax
-	  val ptmSpecificityORMs = em.createQuery("FROM fr.proline.core.orm.ps.PtmSpecificity ptm_specificity WHERE id IN (:ids)",
-	                                          classOf[fr.proline.core.orm.ps.PtmSpecificity] )
-	                             .setParameter("ids", ptmDefIds.asJavaCollection ).getResultList().toList
-	  
-	  ptmSpecificityORMs.foreach ( ptmSpecificityORM => {
-      if(ptmSpecificityORM != null){
-        foundPtmDefBuilder += Some(converter.convertPtmSpecificityORM2OM(ptmSpecificityORM))
-      } else
-        foundPtmDefBuilder += None
-	  } )
-	  
-    /*ptmDefIds foreach (id => {
-      val ptmSpecificityORM : fr.proline.core.orm.ps.PtmSpecificity = em.find(classOf[fr.proline.core.orm.ps.PtmSpecificity], id)
-			if(ptmSpecificityORM != null){
-				founfPtmDefBuilder += Some(converter.convertPtmSpecificityORM2OM(ptmSpecificityORM))
-			} else
-				founfPtmDefBuilder += None		
-    })*/
+    ptmDefIds foreach (id => {
+    	val ptmSpecificityORM : fr.proline.core.orm.ps.PtmSpecificity = em.find(classOf[fr.proline.core.orm.ps.PtmSpecificity], id)
+		if(ptmSpecificityORM != null){
+			foundPtmDefBuilder += Some(converter.convertPtmSpecificityORM2OM(ptmSpecificityORM))
+		} else
+			foundPtmDefBuilder += None		
+    })
     
     foundPtmDefBuilder.result
   }
 
-  def getPtmDefinition(ptmName: String, ptmResidue: Char, ptmLocation: String): Option[PtmDefinition] = { throw new Exception("NYI")}
+  def getPtmDefinition(ptmName: String, ptmResidu: Char, ptmLocation: PtmLocation.Location ): Option[PtmDefinition] = {
+    val converter = new OMConverterUtil()
+    try {
+    	//VDS TEMP  TODO : Use same values as PtmLocation.Location in DB ! 
+    	val locationStr = ptmLocation match {
+    		case PtmLocation.ANYWHERE => "ANYWHERE"
+    		case PtmLocation.ANY_C_TERM => "ANY_CTERM"
+    		case PtmLocation.ANY_N_TERM => "ANY_NTERM"
+    		case PtmLocation.C_TERM => "CTERM"
+    		case PtmLocation.N_TERM => "NTERM"
+    		case PtmLocation.PROT_C_TERM => "PROTEIN_CTERM"
+    		case PtmLocation.PROT_N_TERM=> "PROTEIN_NTERM"
+    	}
+      
+    	var ptmSpecificity : PtmSpecificity = null 
+		if( ptmResidu.equals('\0'))
+			ptmSpecificity =ptmRepo.findPtmSpecificityWithNoResiduByNameLoc(ptmName, locationStr)
+		else 
+			ptmSpecificity =ptmRepo.findPtmSpecificityByNameLocResidu(ptmName,locationStr,ptmResidu)
+        
+		if(ptmSpecificity == null)
+			return None
+		else 
+			return Some(converter.convertPtmSpecificityORM2OM(ptmSpecificity))
+    } catch {
+	  case e:PersistenceException => {
+	    logger.warn(" Error while requiering PtmSpecificity (name: "+ptmName+" residu: "+ptmResidu+" location: "+ptmLocation.toString+") : "+e.getMessage)
+	    return None
+	  }  
+    }
+  }
 
-  def getPtmId( shortName: String):  Option[Int] = {  throw new Exception("NYI") }
+  def getPtmId( shortName: String):  Option[Int] = {
+    try {
+    	val ptm = em.createQuery("FROM Ptm ptm WHERE ptm.shortName = :shortName",  classOf[fr.proline.core.orm.ps.Ptm]).setParameter("shortName", shortName).getSingleResult
+    	if(ptm == null)
+			return None
+		else 
+		  return Some(ptm.getId) 
+    } catch {
+      case e:PersistenceException => {
+        logger.warn(" Error while requiering Ptm "+shortName+" : "+e.getMessage)
+        return None      
+      }
+    }
+    return None 
+  }
 
 }
