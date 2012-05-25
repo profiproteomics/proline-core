0067b58cbb6b0c9defc19005a01bb981c9e6f59d
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-05-25 05:52:28 +0000

    Ajout du PersistenceUnit PDI

--- Proline-ORM/src/main/java/fr/proline/core/orm/utils/JPAUtil.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/utils/JPAUtil.java
@@ -4,7 +4,7 @@ public class JPAUtil {
 
 	public enum PersistenceUnitNames{		
 		
-		UDS_Key("udsdb_production"), PS_Key("psdb_production"), MSI("msidb_production");
+		UDS_Key("udsdb_production"), PS_Key("psdb_production"), MSI_Key("msidb_production"),PDI_Key("pdidb_production");
 		String pu_name;
 		PersistenceUnitNames(String name){
 			pu_name = name;
