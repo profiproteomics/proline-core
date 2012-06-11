32eaf9885a1668c4804c54c3a9105904decb998c
Christophe Bruley <christophe.bruley@cea.fr>
2012-06-11 14:25:28 +0000

    - Enum added for controlled string values

--- Proline-ORM/src/main/java/fr/proline/core/orm/pdi/SequenceDbConfig.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/pdi/SequenceDbConfig.java
@@ -1,59 +1,75 @@
 package fr.proline.core.orm.pdi;
 
+import static javax.persistence.CascadeType.PERSIST;
+import static javax.persistence.CascadeType.REMOVE;
+
 import java.io.Serializable;
 import javax.persistence.*;
-
 
 /**
  * The persistent class for the seq_db_config database table.
  * 
  */
 @Entity
-@Table(name="seq_db_config")
+@Table(name = "seq_db_config")
 public class SequenceDbConfig implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 
+	public enum Alphabet { AA, DNA };
+	
+	public enum Format { SWISS, GENEBANK, GFF };
+
+	public enum Type { IPI, SPROT, TREMBL, NCBI }
+	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private String type;
+	@Enumerated(EnumType.STRING)
+	private Type type;
 
-	private String alphabet;
+	@Enumerated(EnumType.STRING)
+	private Alphabet alphabet;
 
-	@Column(name="ref_entry_format")
-	private String refEntryFormat;
+	@Column(name = "ref_entry_format")
+	@Enumerated(EnumType.STRING)
+	private Format refEntryFormat;
 
-	@Column(name="serialized_properties")
+	@Column(name = "serialized_properties")
 	private String serializedProperties;
 
-	//bi-directional many-to-one association to FastaParsingRule
-    @ManyToOne
-	@JoinColumn(name="fasta_parsing_rule_id")
+	// bi-directional many-to-one association to FastaParsingRule
+	@ManyToOne(cascade = { PERSIST })
+	@JoinColumn(name = "fasta_parsing_rule_id")
 	private FastaParsingRule fastaParsingRule;
 
-    public SequenceDbConfig() {
-    }
+	protected SequenceDbConfig() {
 
-	public String getType() {
+	}
+	
+	public SequenceDbConfig(Type type) {
+		setType(type);
+	}
+
+	public Type getType() {
 		return this.type;
 	}
 
-	public void setType(String type) {
+	public void setType(Type type) {
 		this.type = type;
 	}
 
-	public String getAlphabet() {
+	public Alphabet getAlphabet() {
 		return this.alphabet;
 	}
 
-	public void setAlphabet(String alphabet) {
+	public void setAlphabet(Alphabet alphabet) {
 		this.alphabet = alphabet;
 	}
 
-	public String getRefEntryFormat() {
+	public Format getRefEntryFormat() {
 		return this.refEntryFormat;
 	}
 
-	public void setRefEntryFormat(String refEntryFormat) {
+	public void setRefEntryFormat(Format refEntryFormat) {
 		this.refEntryFormat = refEntryFormat;
 	}
 
@@ -72,5 +88,5 @@ public class SequenceDbConfig implements Serializable {
 	public void setFastaParsingRule(FastaParsingRule fastaParsingRule) {
 		this.fastaParsingRule = fastaParsingRule;
 	}
-	
+
 }
