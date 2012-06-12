6d793fa4028b46fea5a3c039d74067c201081962
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-06-12 14:19:24 +0000

    Add a RsmStorer component

--- Proline-DAL/src/main/scala/fr/proline/core/om/storer/msi/RsmStorer.scala
+++ Proline-DAL/src/main/scala/fr/proline/core/om/storer/msi/RsmStorer.scala
@@ -1,5 +1,90 @@
 package fr.proline.core.om.storer.msi
 
-class RsmStorer {
+import com.weiglewilczek.slf4s.Logging
+import net.noerd.prequel.ReusableStatement
+import net.noerd.prequel.SQLFormatterImplicits._
 
+import fr.proline.core.dal.SQLFormatterImplicits._
+import fr.proline.core.dal.{MsiDb,MsiDbResultSummaryTable}
+import fr.proline.core.om.model.msi.ResultSummary
+import fr.proline.core.om.storer.msi.impl.SQLiteRsmStorer
+
+trait IRsmStorer extends Logging {
+  
+  val msiDb: MsiDb // Main MSI db connection
+  val scoringIdByType = new fr.proline.core.dal.helper.MsiDbHelper( msiDb ).getScoringIdByType
+  
+  def storeRsmPeptideInstances( rsm: ResultSummary ): Int
+  def storeRsmPeptideSets( rsm: ResultSummary ): Int
+  def storeRsmProteinSets( rsm: ResultSummary ): Int
+  
+}
+
+/** A factory object for implementations of the IRsmStorer trait */
+object RsmStorer {
+  
+  /*import fr.proline.core.om.storer.msi.impl.GenericRsStorer
+  import fr.proline.core.om.storer.msi.impl.PgRsStorer
+  import fr.proline.core.om.storer.msi.impl.SQLiteRsStorer*/
+  
+  def apply( msiDb: MsiDb ): RsmStorer = { msiDb.config.driver match {
+    //case "org.postgresql.Driver" => new RsStorer( new PgRsStorer( msiDb ) )
+    case "org.sqlite.JDBC" => new RsmStorer( new SQLiteRsmStorer( msiDb ) )
+    //case _ => new RsStorer( new GenericRsStorer( msiDb ) )
+    }
+  }
+}
+
+class RsmStorer( private val _storer: IRsmStorer ) extends Logging {
+  
+  val msiDb = _storer.msiDb
+  
+  def storeResultSummary( rsm: ResultSummary ): Unit = {
+    
+    this._insertResultSummary( rsm )
+    
+    // Store peptide instances
+    this._storer.storeRsmPeptideInstances( rsm )
+    logger.info( "peptide instances have been stored !" )
+    
+    // Store protein sets
+    this._storer.storeRsmProteinSets( rsm )
+    logger.info( "protein sets have been stored" )
+    
+    // Store peptides sets
+    this._storer.storeRsmPeptideSets( rsm )
+    logger.info( "peptides sets have been stored" )
+    
+
+    
+  }
+  
+  private def _insertResultSummary( rsm: ResultSummary ): Unit = {
+    
+    val msiDbConn = this.msiDb.getOrCreateConnection()
+    
+    /// Define some vars
+    val rsmDesc = if( rsm.name == null ) None else Some( rsm.description )
+    val modificationTimestamp = new java.util.Date() // msiDb.stringifyDate( new java.util.Date )        
+    var decoyRsmId = if( rsm.getDecoyResultSummaryId > 0 ) Some(rsm.getDecoyResultSummaryId) else None
+    val rsId = rsm.getResultSetId
+    
+    // Store RDB result summary
+    // TODO: use JPA instead    
+    val rsmInsertQuery = MsiDbResultSummaryTable.makeInsertQuery { t =>
+      List( t.description, t.modificationTimestamp, t.decoyResultSummaryId, t.resultSetId )
+    }
+    
+    val stmt = msiDbConn.prepareStatement( rsmInsertQuery, java.sql.Statement.RETURN_GENERATED_KEYS )     
+    new ReusableStatement( stmt, msiDb.config.sqlFormatter ) <<
+      rsmDesc <<
+      modificationTimestamp <<
+      decoyRsmId <<
+      rsId
+
+    stmt.execute()
+    rsm.id = this.msiDb.extractGeneratedInt( stmt )
+    
+  }
+  
 }
