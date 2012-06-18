1700f5a05baba66439fd77d7d8f17a7cb83dac6c
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-06-18 13:09:44 +0000

    add test for null password

--- Proline-Databases/src/test/java/fr/proline/repository/ConnectionPrototypeTest.java
+++ Proline-Databases/src/test/java/fr/proline/repository/ConnectionPrototypeTest.java
@@ -1,19 +1,25 @@
 package fr.proline.repository;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.sql.DatabaseMetaData;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import fr.proline.repository.ConnectionPrototype.DatabaseProtocol;
 import fr.proline.repository.ProlineRepository.Databases;
 import fr.proline.repository.ProlineRepository.DriverType;
 
 public class ConnectionPrototypeTest {
+	
+	public final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	String dbPropertiesFile = "/connection_proto_db.properties" ;
+
 	@Test
 	public void testH2ToConnector() throws Exception {
 		ConnectionPrototype proto = new ConnectionPrototype();
@@ -23,7 +29,18 @@ public class ConnectionPrototypeTest {
 		assertEquals("jdbc:h2:mem:test_uds", metaData.getURL());
 		assertEquals("sa", metaData.getUserName().toLowerCase());
 	}
-
+	
+	@Test
+	public void testH2NullPwdToConnector() throws Exception {
+		ConnectionPrototype proto = new ConnectionPrototype();
+		proto.driver(DriverType.H2).protocol(DatabaseProtocol.MEMORY).username("sa").namePattern("test");
+		proto.password(null);
+		DatabaseConnector connector = proto.toConnector(Databases.UDS);
+		DatabaseMetaData metaData = connector.getConnection().getMetaData();
+		assertEquals("jdbc:h2:mem:test_uds", metaData.getURL());
+		assertEquals("sa", metaData.getUserName().toLowerCase());
+	}
+	
 	@Test
 	public void testPGToConnector() throws Exception {
 		ConnectionPrototype proto = new ConnectionPrototype();
@@ -39,10 +56,10 @@ public class ConnectionPrototypeTest {
 		DatabaseConnector connector = proto.toConnector(Databases.UDS);
 		assertNotNull(connector);
 		assertEquals("jdbc:sqlite:./target/proline_test_uds", connector.getConnection().getMetaData().getURL());
-	}
+    }
 
 	@Test(expected=IOException.class)
 	public void testInvalidFilePrototypeConnector() throws Exception {
-		ConnectionPrototype proto = new ConnectionPrototype("Mytest"+dbPropertiesFile);
+		 new ConnectionPrototype("Mytest"+dbPropertiesFile);
 	}
 }
