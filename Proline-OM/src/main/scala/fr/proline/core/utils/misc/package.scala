4ad3b74c3809b834e92dd8d7130f801052941c03
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-07-20 16:24:32 +0000

    Enhance the RegexUtils : - add a new method =# which allows to capture matched groups (returns the scala match) - add a cache of already compiled regexes

--- Proline-OM/src/main/scala/fr/proline/core/utils/misc/package.scala
+++ Proline-OM/src/main/scala/fr/proline/core/utils/misc/package.scala
@@ -81,6 +81,20 @@ package object misc {
     
     import util.matching.Regex
     import java.util.regex.Pattern
+    
+    // Private cache of compiled regexes
+    val regexCache = new collection.mutable.HashMap[String,Regex]
+    
+    def getRegex( s: String, groupNames: String* ) = {
+      if( regexCache.contains(s) ) regexCache(s)
+      else {
+        val regex = new Regex(s, groupNames: _* )
+        regexCache += (s -> regex)
+        regex
+      }
+    }
+    
+    def getPattern( s: String ) = this.getRegex( s ).pattern
 
     /** Use a Regex object to match a given string */
     class RichRegex(self: Regex) {
@@ -91,11 +105,16 @@ package object misc {
     /** Match a string object to a given regular expression */
     class RichString(self: String) {
       /** The regular expression is provided as a scala Regex object */
-      def =~(r: util.matching.Regex) = r.pattern.matcher(self).matches
+      def =~(r: util.matching.Regex) = r.pattern.matcher(self).matches      
       /** The regular expression is provided as a String  */
-      def =~(s: String) = self.matches(s)      
+      //def =~(s: String) = self.matches(s) 
+      def =~(s: String) = RegexUtils.getPattern(s).matcher(self).matches   
       /** The regular expression is provided as a scala String */
-      def ~~(s: String) = Pattern.compile(s).matcher(self).find
+      //def ~~(s: String) = Pattern.compile(s).matcher(self).find
+      def ~~(s: String) = RegexUtils.getPattern(s).matcher(self).find
+      
+      /** The regular expression is provided as a String  */
+      def =#(s: String, groupNames: String* ) = RegexUtils.getRegex(s, groupNames: _* ).findFirstMatchIn(self)
     }
     implicit def strToRichStr(str: String) = new RichString(str)
   }
