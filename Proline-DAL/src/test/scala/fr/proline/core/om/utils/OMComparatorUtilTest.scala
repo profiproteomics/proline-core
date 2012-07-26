7aefc0ba11802cb376e81e900b6318dad72dba26
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-07-26 06:35:54 +0000

    Traduit les tests OMConverter de java a scala

---
+++ Proline-DAL/src/test/scala/fr/proline/core/om/utils/OMComparatorUtilTest.scala
@@ -0,0 +1,68 @@
+package fr.proline.core.om.utils
+
+import org.junit.Before
+import fr.proline.repository.utils.DatabaseTestCase
+import fr.proline.core.orm.utils.JPAUtil
+import org.junit.After
+import org.junit.Assert._
+import fr.proline.repository.utils.DatabaseUtils
+import fr.proline.core.orm.ps.PeptidePtm
+import fr.proline.core.om.model.msi.PtmDefinition
+import fr.proline.core.om.model.msi.LocatedPtm
+import org.junit.Test
+import fr.proline.core.om.model.msi.PtmEvidence
+import fr.proline.core.om.model.msi.IonTypes
+import fr.proline.core.om.model.msi.PtmNames
+
+class OMComparatorUtilTest extends DatabaseTestCase  {
+  
+  @Before
+  @throws(classOf[Exception])
+  def initialize() = {
+     initDatabase();
+     initEntityManager(JPAUtil.PersistenceUnitNames.PS_Key.getPersistenceUnitName());
+     loadDataSet("/fr/proline/core/om/ps/Unimod_Dataset.xml");   
+  }
+  
+  @After 
+  @throws(classOf[Exception]) 
+  override def tearDown() ={
+		super.tearDown();
+  }
+  
+   override def getSQLScriptLocation() : String  ={
+	  return DatabaseUtils.H2_DATABASE_PS_SCRIPT_LOCATION;
+  }
+   
+   	@Test
+	def comparePepPtm() =  {
+		val pepPtm : PeptidePtm = em.find(classOf[PeptidePtm],1)
+		
+		val ptmEvi = new PtmEvidence(ionType = IonTypes.Precursor,
+							composition= "Post-translational", 
+							monoMass= 15.994915, 
+							averageMass = 15.9994, 
+							isRequired = true)
+		
+		var ptmEvidences = new Array[PtmEvidence](1)
+		ptmEvidences.update(0, ptmEvi)
+		
+		val  ptmDef = new PtmDefinition(id = -100, 
+		    				location = "ANYWHERE",
+		    				names = new PtmNames(shortName= "Oxidation", fullName= "Oxidation or Hydroxylation"), 
+		    				ptmEvidences = ptmEvidences, 
+		    				residue= 'M', 
+		    				classification = "Post-translational",ptmId= 0)
+		
+		val lPtm : LocatedPtm = new LocatedPtm(definition = ptmDef, 
+									seqPosition = 3,
+									monoMass= Double.MaxValue, 
+									averageMass= Double.MaxValue,
+									composition = "O",
+									isNTerm= false,
+									isCTerm= false)
+		
+		assertTrue(OMComparatorUtil.comparePeptidePtm(lPtm, pepPtm));
+	
+	}
+}
---
+++ Proline-DAL/src/test/scala/fr/proline/core/om/utils/OMConverterUtilTest.scala
@@ -0,0 +1,72 @@
+package fr.proline.core.om.utils
+
+import org.junit.Before
+import fr.proline.repository.utils.DatabaseTestCase
+import fr.proline.core.orm.utils.JPAUtil
+import org.junit.After
+import org.junit.Assert._
+import fr.proline.repository.utils.DatabaseUtils
+import fr.proline.core.orm.ps.PeptidePtm
+import fr.proline.core.om.model.msi.PtmDefinition
+import fr.proline.core.om.model.msi.LocatedPtm
+import org.junit.Test
+import fr.proline.core.om.model.msi.PtmEvidence
+import fr.proline.core.om.model.msi.IonTypes
+import fr.proline.core.om.model.msi.PtmNames
+import scala.collection.mutable.ArrayBuilder
+
+class OMConverterUtilTest extends DatabaseTestCase  {  
+  
+  var converter: PeptidesOMConverterUtil = null
+  
+  @Before
+  @throws(classOf[Exception])
+  def initialize() = {
+     initDatabase();
+     initEntityManager(JPAUtil.PersistenceUnitNames.PS_Key.getPersistenceUnitName());
+     loadDataSet("/fr/proline/core/om/ps/Unimod_Dataset.xml");
+     converter = new PeptidesOMConverterUtil(true );
+  }
+  
+  @After 
+  @throws(classOf[Exception]) 
+  override def tearDown() ={
+		super.tearDown();
+  }
+  
+   override def getSQLScriptLocation() : String  ={
+	  return DatabaseUtils.H2_DATABASE_PS_SCRIPT_LOCATION;
+  }
+   
+   	@Test
+	def testConvertPeptides() =  {
+	   	val ormPep : fr.proline.core.orm.ps.Peptide  = em.find(classOf[fr.proline.core.orm.ps.Peptide], 4)
+		val omPep : fr.proline.core.om.model.msi.Peptide = converter.convertPeptidePsORM2OM(ormPep)
+		assertNotNull(omPep);
+		assertEquals(omPep.calculatedMass, ormPep.getCalculatedMass(),0.01d)
+		assertEquals(omPep.sequence, ormPep.getSequence())
+		
+		//Test PTMs
+		val pepPTMsIT : java.util.Iterator[PeptidePtm]  = ormPep.getPtms().iterator()		
+		var pepPtmsNamesBuilder = Array.newBuilder[String]
+		var pepPtmsLocationsBuilder = Array.newBuilder[Int]
+		while(pepPTMsIT.hasNext()){
+			val  nextORMPtm : PeptidePtm = pepPTMsIT.next();
+			pepPtmsNamesBuilder += nextORMPtm.getSpecificity().getPtm().getFullName()
+			pepPtmsLocationsBuilder += nextORMPtm.getSeqPosition()
+		}
+		val pepPtmsNames : Array[String] = pepPtmsNamesBuilder.result
+		val pepPtmsLocations : Array[Int] = pepPtmsLocationsBuilder.result
+		
+		val omPtms  = omPep.ptms
+		
+		omPtms  foreach( nextOMPtm => {		  
+			val indexPtm = pepPtmsNames.indexOf(nextOMPtm.definition.names.fullName);
+			if(indexPtm == -1)
+				fail("Ptm not found in OM Peptide");
+			else 
+				assertEquals(Integer.valueOf(nextOMPtm.seqPosition), pepPtmsLocations.apply(indexPtm));  
+		})
+		
+	}
+}
