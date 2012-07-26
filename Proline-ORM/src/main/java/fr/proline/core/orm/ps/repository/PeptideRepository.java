5c0b8e68c1bbff0164f3b008085cb22bb1d1ef30
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-07-26 08:36:37 +0000

    FindPeptide : return null if none exist

--- Proline-ORM/src/main/java/fr/proline/core/orm/ps/repository/PeptideRepository.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/ps/repository/PeptideRepository.java
@@ -3,12 +3,18 @@ package fr.proline.core.orm.ps.repository;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import fr.proline.core.orm.ps.Peptide;
 import fr.proline.core.orm.utils.JPARepository;
 
-public class PeptideRepository extends JPARepository { 
+public class PeptideRepository extends JPARepository {
+	
+	protected static Logger logger = LoggerFactory.getLogger(PeptideRepository.class);
 	
 	public PeptideRepository(EntityManager em) {
 		super(em);
@@ -21,14 +27,24 @@ public class PeptideRepository extends JPARepository {
 	}
 	
 	public Peptide findPeptidesBySequenceAndPtmStr(String seq, String ptmStr) {
-		TypedQuery<Peptide> query = getEntityManager().createNamedQuery("findPepsBySeqPtmStr", Peptide.class);
-		query.setParameter("seq", seq.toUpperCase()).setParameter("ptmStr", ptmStr);
-		return query.getSingleResult();
+		try {
+			TypedQuery<Peptide> query = getEntityManager().createNamedQuery("findPepsBySeqPtmStr", Peptide.class);
+			query.setParameter("seq", seq.toUpperCase()).setParameter("ptmStr", ptmStr);
+			return query.getSingleResult();
+		} catch (NoResultException nre) {
+			logger.info("No Peptides for sequence {} and PTM String {} ",seq, ptmStr);
+			return null;
+		}
 	}
 	
 	public Peptide findPeptidesBySeqWoPtm(String seq) {
-		TypedQuery<Peptide> query = getEntityManager().createNamedQuery("findPepsBySeqWoPtm", Peptide.class);
-		query.setParameter("seq", seq.toUpperCase());
-		return query.getSingleResult();
+		try {			
+			TypedQuery<Peptide> query = getEntityManager().createNamedQuery("findPepsBySeqWoPtm", Peptide.class);
+			query.setParameter("seq", seq.toUpperCase());
+			return query.getSingleResult();
+		} catch (NoResultException nre) {
+			logger.info("No Peptides for sequence {} and no PTMs",seq);
+			return null;
+		}		
 	}
 }
