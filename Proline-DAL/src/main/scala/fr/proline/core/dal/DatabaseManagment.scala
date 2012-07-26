ab55e9443a274654305e73dfd045f7f11b0331ec
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-07-26 06:24:37 +0000

    Utilisation d'un cache pour les DBConnector des MSI

--- Proline-DAL/src/main/scala/fr/proline/core/dal/DatabaseManagment.scala
+++ Proline-DAL/src/main/scala/fr/proline/core/dal/DatabaseManagment.scala
@@ -3,20 +3,22 @@ package fr.proline.core.dal
 import fr.proline.core.orm.utils.JPAUtil
 import fr.proline.repository.DatabaseConnector
 import fr.proline.repository.ProlineRepository
-import javax.persistence.EntityManager
 import javax.persistence.EntityManagerFactory
 import javax.persistence.Persistence
 import javax.persistence.TypedQuery
 import fr.proline.core.orm.uds.ExternalDb
 import scala.collection.mutable.Map
-import fr.proline.repository.ConnectionPrototype
 import scala.collection.JavaConversions
 import fr.proline.core.orm.uds.Project
 import javax.persistence.NoResultException
 import fr.proline.repository.ProlineRepository.DriverType
+import com.weiglewilczek.slf4s.Logging
+import scala.collection.mutable.HashMap
 
-class DatabaseManagment (val udsDBConnector : DatabaseConnector ){
-
+class DatabaseManagment (val udsDBConnector : DatabaseConnector ) extends Logging {
+   
+	private var externalDbIdToDBConnector : Map[Int, DatabaseConnector] = new HashMap[Int, DatabaseConnector] 
+  
 	private lazy val udsEMF : EntityManagerFactory  = {
 		//Create Link to UDSDb
 		val emf : EntityManagerFactory = Persistence.createEntityManagerFactory(JPAUtil.PersistenceUnitNames.getPersistenceUnitNameForDB(ProlineRepository.Databases.UDS), udsDBConnector.getEntityManagerSettings)
@@ -59,31 +61,41 @@ class DatabaseManagment (val udsDBConnector : DatabaseConnector ){
 		pdiConn
 	}
 	
-	def getMSIDatabaseConnector(projectID : Int) : DatabaseConnector = {
+	def getMSIDatabaseConnector(projectID : Int, createNew : Boolean = false) : DatabaseConnector = {	  
 	  var msiDB: ExternalDb = null
 	  try {
 		val udsEM = udsEMF.createEntityManager()
 		val query : TypedQuery[Project] = udsEM.createQuery("Select prj from Project prj where prj.id =  :id", classOf[Project])
-		query.setParameter("id", "projectID")		
+		query.setParameter("id", projectID)		
 		val project = query.getSingleResult
-		udsEM.close
 		val assocMSI = JavaConversions.asScalaSet(project.getExternalDatabases).filter(p => {p.getType.equals("msi")})
+		udsEM.close
 		if(assocMSI.size>1)
 		  throw new javax.persistence.NonUniqueResultException("Multiple MSI databases associated to this project")
 		msiDB = assocMSI.iterator.next
 	  } catch {
-	    case nre:NoResultException  =>  return null
+	    case nre:NoResultException  =>  {
+	      logger.warn("NoResultException  "+ nre.getMessage)
+	      return null 
+	    }
 	  }
+	  
+	  if(createNew)
+	    externalDbIdToDBConnector -= msiDB.getId
+	    
+	  if(!externalDbIdToDBConnector.contains(msiDB.getId)) {
 	  
 		var propBuilder = Map.newBuilder[String, String]
 		propBuilder += DatabaseConnector.PROPERTY_USERNAME ->msiDB.getDbUser
 		propBuilder += DatabaseConnector.PROPERTY_PASSWORD->msiDB.getDbPassword
 		propBuilder += DatabaseConnector.PROPERTY_DRIVERCLASSNAME ->udsDBConnector.getDriverType.getDriverClassName()
 		propBuilder += DatabaseConnector.PROPERTY_URL ->createURL(msiDB)
-		
-		 
-		val msiConn = new DatabaseConnector(JavaConversions.mutableMapAsJavaMap(propBuilder.result))
-		msiConn
+				 
+		externalDbIdToDBConnector.put(msiDB.getId,new DatabaseConnector(JavaConversions.mutableMapAsJavaMap(propBuilder.result)))
+	  }
+	  
+	  externalDbIdToDBConnector(msiDB.getId)
+	  
 	}
 	
 	private def createURL(externalDB: ExternalDb) : String = {
