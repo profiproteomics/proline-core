911ae7111e09a3f7eb25894d8d49d9068769cad3
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-07-26 06:19:24 +0000

    Ajout d'une méthode permettant de rechercher les bioSequence par l'accession et la SeqDB

--- Proline-ORM/src/main/java/fr/proline/core/orm/pdi/repository/BioSequenceRepository.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/pdi/repository/BioSequenceRepository.java
@@ -2,13 +2,19 @@ package fr.proline.core.orm.pdi.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.TypedQuery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import fr.proline.core.orm.pdi.BioSequence;
 import fr.proline.core.orm.utils.JPARepository;
 
 public class BioSequenceRepository extends JPARepository {
 
+	protected static Logger logger = LoggerFactory.getLogger(BioSequenceRepository.class);
+	
 	public BioSequenceRepository(EntityManager em) {
 		super(em);
 	}
@@ -24,4 +30,27 @@ public class BioSequenceRepository extends JPARepository {
 			return null;
 		}
 	}
+	
+	public BioSequence findBioSequencePerAccessionAndSeqDB(String accession, Integer seqDbInstanceId) {
+		TypedQuery<BioSequence> query = getEntityManager()
+				.createQuery("Select bs from fr.proline.core.orm.pdi.BioSequence bs, fr.proline.core.orm.pdi.ProteinIdentifier pi, fr.proline.core.orm.pdi.SequenceDbEntry se, fr.proline.core.orm.pdi.SequenceDbInstance si, fr.proline.core.orm.pdi.DbEntryProteinIdentifierMap dbepi"
+								+ " where dbepi.proteinIdentifier = pi  and  dbepi.dbEntry = se and se.sequenceDbInstance = si and se.bioSequence = bs and pi.value = :acc and si.id = :seqDbInstId ",
+						BioSequence.class);
+		query.setParameter("acc", accession).setParameter("seqDbInstId",seqDbInstanceId);		
+		
+		try {
+			return query.getSingleResult();
+		} catch (NoResultException nre) {
+			logger.info(
+					" No BioSequence for accession {} and SequenceDbInstance {} ",
+					accession, seqDbInstanceId);
+			return null;
+		} catch (NonUniqueResultException nure) {
+			logger.warn(
+					" More than one BioSequence for accession {} and SequenceDbInstance {} ",
+					accession, seqDbInstanceId);
+			return null;
+		}
+	}
+	
 }
