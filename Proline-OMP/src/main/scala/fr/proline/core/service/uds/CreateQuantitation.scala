ce365e2d512c8590f7bc67b5a03434827cc45b71
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-08-02 06:11:20 +0000

    FIX : databaseManagement.udsEMF n'est pas accessible

--- Proline-OMP/src/main/scala/fr/proline/core/service/uds/CreateQuantitation.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/service/uds/CreateQuantitation.scala
@@ -17,6 +17,9 @@ import fr.proline.core.orm.uds.{ BiologicalGroup => UdsBiologicalGroup,
                                  QuantitationFraction => UdsQuantitationFraction,
                                  RatioDefinition => UdsRatioDefinition,
                                  SampleAnalysisReplicate => UdsSampleAnalysisReplicate }
+import javax.persistence.Persistence
+import fr.proline.core.orm.utils.JPAUtil
+import fr.proline.repository.ProlineRepository
 
 class CreateQuantitation(
         dbManager: DatabaseManagement,
@@ -32,7 +35,8 @@ class CreateQuantitation(
   def runService() = {
     
     // Open UDSdb connection
-    val udsEM = dbManager.udsEMF.createEntityManager()    
+    val udsEMF = Persistence.createEntityManagerFactory(JPAUtil.PersistenceUnitNames.getPersistenceUnitNameForDB(ProlineRepository.Databases.UDS),dbManager.udsDBConnector.getEntityManagerSettings())
+    val udsEM = udsEMF.createEntityManager()    
     
     // Retrieve some vars
     val biologicalSamples = experimentalDesign.biologicalSamples
