86f69fa79f3e37970f56a666331ce455b56ea0a4
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-08-31 16:47:06 +0000

    Minor fix: change getRunMap method signature to be conform with the ILcmsMapFileParser trait

--- Proline-OMP/src/main/scala/fr/proline/core/parser/lcms/impl/mzTSVParser.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/parser/lcms/impl/mzTSVParser.scala
@@ -2,11 +2,12 @@ package fr.proline.core.parser.lcms.impl
 
 import fr.proline.core.parser.lcms.ILcmsMapFileParser
 import fr.proline.core.om.model.lcms.LcmsRun
+import fr.proline.core.om.model.lcms.RunMap
 
 class mzTSVParser extends ILcmsMapFileParser {
   
-  def getRunMap( filePath: String, lcmsRun: LcmsRun, extraParams: Map [String,Any] ) = {
-    
+  def getRunMap( filePath: String, lcmsRun: LcmsRun, extraParams: Map [String,Any] ): Option[RunMap] = {
+    null
   }
 
 }
