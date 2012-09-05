cd29f8f9a4f94f4fab121027b933546ee503b5d5
Marco Dubois <cram@hotmail.fr>
2012-09-05 07:31:01 +0000

    add PropertiesClasses.scala

---
+++ Proline-OM/src/main/scala/fr/proline/core/om/model/lcms/PropertiesClasses.scala
@@ -0,0 +1,16 @@
+package fr.proline.core.om.model.lcms
+
+import scala.reflect.BeanProperty
+import com.codahale.jerkson.JsonSnakeCase
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+
+@JsonSnakeCase
+@JsonInclude( Include.NON_NULL )
+case class FeatureProperties (
+  @BeanProperty var peakelsCount: Option[Int] = None,
+  @BeanProperty var peakelsRatios: Option[Array[Float]] = None,
+  @BeanProperty var overlapCorrelation: Option[Float] = None,
+  @BeanProperty var overlapFactor: Option[Float] = None
+  //@BeanProperty var maPRopDuJour2: Option[Float] = None
+)
