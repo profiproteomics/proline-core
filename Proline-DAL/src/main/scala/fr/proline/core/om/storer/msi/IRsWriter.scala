131b9f8735b69cd3c0ab808e509c069ba54b5b80
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-09-18 14:26:35 +0000

    Storer Refactoring : OnGoing ! See wiki pages

---
+++ Proline-DAL/src/main/scala/fr/proline/core/om/storer/msi/IRsWriter.scala
@@ -0,0 +1,39 @@
+package fr.proline.core.om.storer.msi
+
+import fr.proline.core.om.model.msi.Protein
+import fr.proline.core.dal.MsiDb
+import com.weiglewilczek.slf4s.Logging
+import fr.proline.core.om.model.msi.Peptide
+import fr.proline.core.om.model.msi.ResultSet
+import scala.collection.mutable.HashMap
+
+trait IRsWriter extends Logging {
+  
+  val msiDb1: MsiDb // Main MSI db connection
+  lazy val msiDb2: MsiDb = new MsiDb( msiDb1.config, maxVariableNumber = 10000 ) // Secondary MSI db connection
+  
+  val scoringIdByType = new fr.proline.core.dal.helper.MsiDbHelper( msiDb1 ).getScoringIdByType
+  
+  // TODO: implement as InMemoryProvider
+  val peptideByUniqueKey = new HashMap[String,Peptide]()
+  val proteinBySequence = new HashMap[String,Protein]()
+  
+  /**
+   * Store specified new ResultSet and all associated data into dbs. 
+   * Protein and peptides referenced by the result set will be created as well
+   * if necessary.
+   */
+  def fetchExistingPeptidesIdByUniqueKey( pepSequences: Seq[String] ): Map[String,Int]
+  def storeNewPeptides( peptides: Seq[Peptide] ): Array[Peptide]
+  
+  def fetchProteinIdentifiers( accessions: Seq[String] ): Array[Any]// TODO: use JPA
+  
+  def fetchExistingProteins( protCRCs: Seq[String] ): Array[Protein]
+  def storeNewProteins( proteins: Seq[Protein] ): Array[Protein]
+  
+  def storeRsPeptideMatches( rs: ResultSet ): Int
+  def storeRsProteinMatches( rs: ResultSet ): Int 
+  def storeRsSequenceMatches( rs: ResultSet ): Int
+  
+}
+
