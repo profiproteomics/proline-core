1dbb0732aee16230070746a02e6752b87aa1359b
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-09-20 11:57:14 +0000

    Add comment + return ID when single object is stored (instead of list)

--- Proline-DAL/src/main/scala/fr/proline/core/om/storer/msi/RsStorer.scala
+++ Proline-DAL/src/main/scala/fr/proline/core/om/storer/msi/RsStorer.scala
@@ -13,6 +13,7 @@ trait IRsStorer extends Logging {
   /**
    * Store in persistence repository specified ResultSet and associated data.
    * Specified data should have already been updated : temporary id on associated data means should be saved.
+   * Transaction are not managed by this method, should be done by user.
    * 
    * @param resultSet : ResultSet to store
    * @param context: StorerContext containing mapping and context information (EntityManager, IDs mapping...)
@@ -24,7 +25,8 @@ trait IRsStorer extends Logging {
   /**
    * Store, in persistence repository, specified ResultSet and associated data.
    * Specified data should have already been updated : temporary id on associated data means should be saved.
-   * 
+   * Transaction are not managed by this method, should be done by user.
+   *  
    * @param resultSet : ResultSet to store
    * @return ID of stored ResultSet 
    * @exception Exception if an error occur while saving data
@@ -34,16 +36,20 @@ trait IRsStorer extends Logging {
   /**
    * Store, in persistence repository, specified ResultSet and associated data.
    * Specified data should have already been updated : temporary id on associated data means should be saved.
+   * Transaction are not managed by this method, should be done by user.
+   *  
    * @param resultSet : ResultSet to store
    * @param msQueries Queries to store and to associate to MSISearch referenced by specified ResultSet
    * @param peaklistContainer IPeaklistContainer fro which Spectrum will be retrieve
-   * @param context: StorerContext containing mapping and context information (EntityManager, IDs mapping...)
+   * @param context: StorerContext containing mapping and context information (EntityManager, IDs mapping...). This object will be updated by created entity cache
+   * @return Int : ID of created entity  
    * @exception Exception if an error occur while saving data 
    */
-  def storeResultSet(resultSet : ResultSet, msQueries : Seq [MsQuery], peakListContainer : IPeaklistContainer, context : StorerContext) : StorerContext
+  def storeResultSet(resultSet : ResultSet, msQueries : Seq [MsQuery], peakListContainer : IPeaklistContainer, context : StorerContext) : Int
   
   /**
    * Store PeakList and associated software, if necessary,  in repository 
+   * Transaction are not managed by this method, should be done by user.
    * 
    * @param peaklist : Peaklist to save 
    * @return Id of the saved PeakList
@@ -54,10 +60,11 @@ trait IRsStorer extends Logging {
    * Store Spectra retrieve by specified IPeaklistContainer in repository. 
    * Created spectra will be associated to specified PeakList ID, Spectra title mapped to their Id 
    * will be stored in specified StorerContext.  
-   * 
+   * Transaction are not managed by this method, should be done by user.
+   *    
    * @param peaklistId Peaklist to associate created spectrum to
    * @param peaklistContainer IPeaklistContainer fro which Spectrum will be retrieve
-   * @param context StorerContext containing mapping and context information (EntityManager, IDs mapping...)
+   * @param context StorerContext containing mapping and context information (EntityManager, IDs mapping...). . This object will be updated by created entity cache
    * @return StorerContext with updated references
    */
   def storeSpectra( peaklistId: Int, peaklistContainer: IPeaklistContainer, context : StorerContext ): StorerContext
@@ -65,16 +72,21 @@ trait IRsStorer extends Logging {
   /**
    * Store specified MsiSearch and associated data if necessary (SearchSettings, Peaklist,...) in repository
    * Map between created data and temporary ones will be stores in StorerContext
+   * Transaction are not managed by this method, should be done by user.
    *  
    *  @param msiSearch MsiSearch to store
-   *  @param context StorerContext where mapping will be saved and/or retrieve as well as repository connexion information
+   *  @param context StorerContext where mapping will be saved and/or retrieve as well as repository connection information. This object will be updated by created entity cache
+   *  
+   *  @return Int : Id of created MsiSearch 
    */
-  def storeMsiSearch(msiSearch : MSISearch, context : StorerContext) : StorerContext
+  def storeMsiSearch(msiSearch : MSISearch, context : StorerContext) : Int
   
   
   /**
    * Store specified queries in repository and associated them with specified MSISearch.
    * Map between created queries and temporary ones will be stores in StorerContext
+   * 
+   * Transaction are not managed by this method, should be done by user.
    * 
    * @param msiSearchID MSISearch to associate MSQuery to
    * @param msQueries Queries to store
@@ -87,6 +99,7 @@ trait IRsStorer extends Logging {
   
   /**
    * Insert definition of InstrumentConfig (which should exist in uds) in current MSI db if not already defined
+   * Transaction are not managed by this method, should be done by user.
    */
   def insertInstrumentConfig(instrumCfg : InstrumentConfig, context : StorerContext) 
   
