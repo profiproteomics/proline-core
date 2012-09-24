72dab07594acad9fc50049f1127f2da061d2efbf
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2012-09-24 10:30:57 +0000

    Added transient data in ORM objects, to be able to set attributes which are not saved in database. These attributes are used by the IHM #5938

--- Proline-ORM/src/main/java/fr/proline/core/orm/msi/ProteinMatch.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/msi/ProteinMatch.java
@@ -14,6 +14,7 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 /**
  * The persistent class for the protein_match database table.
