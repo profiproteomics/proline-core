fc3bbba5b3bd7cb2ba423eecad5bec45b98c4da1
Laurent Martin <laurent.martin2@cea.fr>
2012-11-06 08:59:28 +0000

    Calculate SeqDbInstance residueCount with a JPA query on distinct BioSequences instead of Context counting.
    #6217

--- Proline-ORM/src/main/java/fr/proline/core/orm/pdi/repository/PdiSeqDatabaseRepository.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/pdi/repository/PdiSeqDatabaseRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import fr.proline.core.orm.pdi.SequenceDbConfig;
@@ -12,6 +13,10 @@ import fr.proline.core.orm.utils.JPARepository;
 import fr.proline.core.orm.utils.StringUtils;
 
 public class PdiSeqDatabaseRepository extends JPARepository {
+
+    private static final String RESIDUE_COUNT_QUERY = "select sum (bs1.length) from fr.proline.core.orm.pdi.BioSequence bs1 where bs1.id in"
+	    + " (select distinct bs2.id from fr.proline.core.orm.pdi.BioSequence bs2, fr.proline.core.orm.pdi.SequenceDbEntry sde"
+	    + " where (sde.bioSequence = bs2) and (sde.sequenceDbInstance = :seqDbInstance))";
 
     public PdiSeqDatabaseRepository(final EntityManager pdiEm) {
 	super(pdiEm);
@@ -58,6 +63,38 @@ public class PdiSeqDatabaseRepository extends JPARepository {
 		result = seqDbConfigs.get(0);
 	    } else {
 		throw new RuntimeException("There are more than one SequenceDbConfig for given name");
+	    }
+
+	}
+
+	return result;
+    }
+
+    /**
+     * Calculate the total residue count (on distinct BioSequences) for a given seqDbInstance.
+     * 
+     * @param seqDbInstance
+     *            The <code>SequenceDbInstance</code> associated with BioSequences via SequenceDbEntries.
+     * @return The calculated residue count or <code>-1L</code> if it cannot be calculated.
+     */
+    public long calculateResidueCount(final SequenceDbInstance seqDbInstance) {
+
+	if (seqDbInstance == null) {
+	    throw new IllegalArgumentException("SeqDbInstance is null");
+	}
+
+	long result = -1L;
+
+	final Query query = getEntityManager().createQuery(RESIDUE_COUNT_QUERY);
+	query.setParameter("seqDbInstance", seqDbInstance);
+
+	final List<?> results = query.getResultList();
+
+	if ((results != null) && !results.isEmpty()) {
+	    final Object obj = results.get(0);
+
+	    if (obj instanceof Long) {
+		result = ((Long) obj).longValue();
 	    }
 
 	}
