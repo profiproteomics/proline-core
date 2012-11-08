cfad406faa4c56e8f64dcad94ee7d9436a019398
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-11-08 12:49:46 +0000

    Some work on Proline-Core related to instrument configurations : - minor refactoring of instrument and fragmentation classes => change some attribute definitions, add some enumerations and new classes - add an utils.io package to help for reading text files block by block => define a BufferedSource.eachLine method which can take a line separator as parameter
    #6509

--- Proline-OM/src/main/scala/fr/proline/core/om/model/msi/FragmentationClasses.scala
+++ Proline-OM/src/main/scala/fr/proline/core/om/model/msi/FragmentationClasses.scala
@@ -1,93 +1,127 @@
 package fr.proline.core.om.model.msi
 
-object FragmentIonType {
+import scala.collection.mutable.ArrayBuffer
   
-  import scala.collection.mutable.ArrayBuffer
+object Fragmentation {
   
   lazy val defaultIonTypes: Array[FragmentIonType] = {
-
+    
     // Create a map of theoretical fragments
-    val ionTypesAsStr = "a a-NH3 a-H2O b b-NH3 b-H2O c d v w x y y-NH3 y-H2O z z+1 z+2 ya yb immonium precursor".split(" ")
+    val ionTypesAsStr = "a a-NH3 a-H2O b b-NH3 b-H2O c d v w x y y-NH3 y-H2O z z+1 z+2 ya yb immonium".split(" ")
     
     val ionTypes = new ArrayBuffer[FragmentIonType](ionTypesAsStr.length)   
     for( val ionTypeAsStr <- ionTypesAsStr ) {
       
-      var( fragmentIonSeries, neutralLoss ) = ("", Option.empty[String] )
+      var( ionSeries, nl ) = ("", Option.empty[NeutralLoss.Value] )
       if( ionTypeAsStr matches ".*-.*"  ) {
         val ionTypeAttrs = ionTypeAsStr.split("-")
-        fragmentIonSeries = ionTypeAttrs(0)
-        neutralLoss = Some(ionTypeAttrs(1))
+        ionSeries = ionTypeAttrs(0)
+        nl = Some(NeutralLoss.withName(ionTypeAttrs(1)))
       }
-      else { fragmentIonSeries = ionTypeAsStr }
+      else { ionSeries = ionTypeAsStr }
       
       // Create new fragment ion type
       ionTypes += new FragmentIonType(
-                                      ionSeries = fragmentIonSeries,
-                                      neutralLoss = neutralLoss
+                                      ionSeries = FragmentIonSeries.withName(ionSeries),
+                                      neutralLoss = nl
                                     )
     }
-      
+    
     ionTypes.toArray
-  }
-  
+  } 
+
+}
+
+object FragmentIonSeries extends Enumeration {
+  //type FragmentIonSeries = Value
+  val a = Value("a")
+  val b = Value("b")
+  val c = Value("c")
+  val d = Value("d")
+  val v = Value("v")
+  val w = Value("w")
+  val x = Value("x")
+  val y = Value("y")
+  val ya = Value("ya")
+  val yb = Value("yb")
+  val z = Value("z")
+  val z1 = Value("z+1")
+  val z2 = Value("z+2")
+  val immonium = Value("immonium")
+}
+
+object NeutralLoss extends Enumeration {
+  val H2O = Value("H2O")
+  val NH3 = Value("NH3")
 }
 
 case class FragmentIonType(   
-               // Required fields
-               val ionSeries: String,
-               
-               // Immutable optional fields
-               val neutralLoss: Option[String] = None
-               ) {
+  // Required fields
+  val ionSeries: FragmentIonSeries.Value,
+ 
+  // Immutable optional fields
+  val neutralLoss: Option[NeutralLoss.Value] = None
+) {
   
-  override def toString():String = { 
-    if( neutralLoss != None && neutralLoss.get.length() > 0 ) { 
-      return ionSeries + "-" + neutralLoss
-    }
-    else { return ionSeries }
-  
+  override def toString():String = {
+    if( neutralLoss != None ) ionSeries + "-" + neutralLoss.get   
+    else ionSeries.toString
   }
 }
   
 trait FragmentationRule{
   // Required fields
   val description: String
+  
+  require( description != null )
 }
   
 case class ChargeConstraint(
   
   // Required fields
   val description: String,
-  val fragmentCharge: Int
+  val fragmentCharge: Int,
+  val precursorMinCharge: Option[Int] = None
   
 ) extends FragmentationRule
+
+trait FragmentationSeriesRequirement {
   
-case class RequiredSerie(
+  val requiredSeries: FragmentIonSeries.Value
+  val requiredSeriesQualityLevel: String
+  
+  // Requirements
+  if( requiredSeriesQualityLevel != null ) {
+    require( requiredSeriesQualityLevel == "significant" || 
+             requiredSeriesQualityLevel == "highest_scoring" )
+  }
+}
+
+case class RequiredSeries (
   
   // Required fields
   val description: String,
-  val requiredSerie: FragmentIonType,
-  val requiredSerieQualityLevel: String
+  val requiredSeries: FragmentIonSeries.Value,
+  val requiredSeriesQualityLevel: String
   
-) extends FragmentationRule {
+) extends FragmentationRule with FragmentationSeriesRequirement {
   
- // Requirements
-  require( requiredSerieQualityLevel == "significant" || 
-           requiredSerieQualityLevel == "highest_scoring" )
+  require( requiredSeries != null )
+  require( requiredSeriesQualityLevel != null )
 
 }
 
 case class TheoreticalFragmentIon(
     
   // Required fields
-  override val description: String,
-  override val requiredSerie: FragmentIonType,
-  override val requiredSerieQualityLevel: String,
+  val description: String,
+  val ionType: FragmentIonType,
+  val requiredSeries: FragmentIonSeries.Value = null,
+  val requiredSeriesQualityLevel: String = null,
  
   // Immutable optional fields
   val fragmentMaxMoz: Double = 0.0,
-  val residueConstraint: String = null,
-  val ionType: FragmentIonType = null
+  val residueConstraint: Option[String] = None  
   
-) extends RequiredSerie( description, requiredSerie, requiredSerieQualityLevel )
+) extends FragmentationRule with FragmentationSeriesRequirement
 
--- Proline-OM/src/main/scala/fr/proline/core/om/model/msi/InstrumentClasses.scala
+++ Proline-OM/src/main/scala/fr/proline/core/om/model/msi/InstrumentClasses.scala
@@ -2,6 +2,15 @@ package fr.proline.core.om.model.msi
 
 import fr.proline.core.utils.misc.InMemoryIdGen
 
+object Activation extends Enumeration {
+  type Type = Value
+  val CID = Value("CID")
+  val ECD = Value("ECD")
+  val ETD = Value("ETD")
+  val HCD = Value("HCD")
+  val PSD = Value("PSD")
+}
+
 object Instrument extends InMemoryIdGen
 case class Instrument(
                    // Required fields
@@ -14,16 +23,34 @@ case class Instrument(
       
 }
 
-object InstrumentConfig extends InMemoryIdGen
-case class InstrumentConfig(   
+object InstrumentConfig extends InMemoryIdGen {
+  
+  def buildName( instrumentName: String, activationType: String,
+                 ms1Analyzer: String, msnAnalyzer: String
+               ): String = {
+    "%s (A1=%s F=%s A2=%s)".format( instrumentName, ms1Analyzer, activationType, msnAnalyzer )
+  }
+  
+}
+case class InstrumentConfig(
                    // Required fields
                    val id: Int,
                    val name: String,
                    val instrument: Instrument,
                    val ms1Analyzer: String,
                    val msnAnalyzer: String,
-                   val activationType: String
+                   val activationType: String,
+                   val fragmentationRules: Option[Array[FragmentationRule]] = None
+                   
+                   // TODO: add properties
                    ) {
+  
+  // Secondary constructor were the name is automatically built
+  def this( id: Int, instrument: Instrument, ms1Analyzer: String, msnAnalyzer: String,
+            activationType: String, fragmentationRules: Option[Array[FragmentationRule]] = None ) {
+    this( id,InstrumentConfig.buildName(instrument.name,activationType,ms1Analyzer,msnAnalyzer),
+          instrument,ms1Analyzer,msnAnalyzer,activationType,fragmentationRules)
+  }
       
 }
   
