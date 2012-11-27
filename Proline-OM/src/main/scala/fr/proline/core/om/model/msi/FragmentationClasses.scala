9df9361b17c458ad6c2b36d0ae02c2e6a21e0aa3
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-11-27 13:29:34 +0000

    Refactoring of fragmentation related classes

--- Proline-OM/src/main/scala/fr/proline/core/om/model/msi/FragmentationClasses.scala
+++ Proline-OM/src/main/scala/fr/proline/core/om/model/msi/FragmentationClasses.scala
@@ -10,44 +10,44 @@ import fr.proline.core.utils.misc.InMemoryIdGen
   
 object Fragmentation {
   
+  val ionSeriesNames = "a a-NH3 a-H2O b b-NH3 b-H2O c d v w x y y-NH3 y-H2O z z+1 z+2 ya yb immonium".split(" ")
+  val mascotIonSeriesNames = "a a* a0 b b* b0 c d v w x y y* y0 z z+1 z+2 ya yb immonium".split(" ")
+  
   val defaultIonTypes: Array[FragmentIonType] = {
     
     // Create a map of theoretical fragments
-    val ionTypesAsStr = "a a-NH3 a-H2O b b-NH3 b-H2O c d v w x y y-NH3 y-H2O z z+1 z+2 ya yb immonium".split(" ")
-    
-    val ionTypes = new ArrayBuffer[FragmentIonType](ionTypesAsStr.length)   
-    for( val ionTypeAsStr <- ionTypesAsStr ) {
-      
-      var( ionSeries, nl ) = ("", Option.empty[NeutralLoss.Value] )
-      if( ionTypeAsStr matches ".*-.*"  ) {
-        val ionTypeAttrs = ionTypeAsStr.split("-")
-        ionSeries = ionTypeAttrs(0)
-        nl = Some(NeutralLoss.withName(ionTypeAttrs(1)))
-      }
-      else { ionSeries = ionTypeAsStr }
-      
+    val ionTypes = new ArrayBuffer[FragmentIonType](ionSeriesNames.length)   
+    for( val ionSeriesName <- ionSeriesNames ) {
       // Create new fragment ion type
-      ionTypes += new FragmentIonType(
-                                      ionSeries = FragmentIonSeries.withName(ionSeries),
-                                      neutralLoss = nl
-                                    )
+      ionTypes += new FragmentIonType( ionSeriesName )
     }
     
     ionTypes.toArray
-  } 
+  }
+  
+  lazy val defaultIonTypeByMascotSeriesName = mascotIonSeriesNames.zip(defaultIonTypes).toMap
+  
+  def isReverseSeries( seriesName: String ): Boolean = {
+    seriesName.startsWith("x") || seriesName.startsWith("y") || seriesName.startsWith("z")
+  }
 
 }
 
 object FragmentIonSeries extends Enumeration {
-  //type FragmentIonSeries = Value
   val a = Value("a")
+  val a_NH3 = Value("a-NH3")
+  val a_H2O = Value("a-H2O")
   val b = Value("b")
+  val b_NH3 = Value("b-NH3")
+  val b_H2O = Value("b-H2O")
   val c = Value("c")
   val d = Value("d")
   val v = Value("v")
   val w = Value("w")
   val x = Value("x")
   val y = Value("y")
+  val y_NH3 = Value("y-NH3")
+  val y_H2O = Value("y-H2O")
   val ya = Value("ya")
   val yb = Value("yb")
   val z = Value("z")
@@ -62,23 +62,41 @@ object NeutralLoss extends Enumeration {
   val NH3 = Value("NH3")
 }
 
-object FragmentIonType extends InMemoryIdGen
-case class FragmentIonType(
+object FragmentIonType extends InMemoryIdGen {
+  
+  def parseNeutralLoss(ionSeriesName: String): Option[NeutralLoss.Value] = {
+    if( ionSeriesName matches ".*-.*"  ) Some(NeutralLoss.withName( ionSeriesName.split("-")(1) ))
+    else None
+  }
+  
+}
+case class FragmentIonType private (
   
   var id: Int = FragmentIonType.generateNewId(),
   
   // Required fields
   val ionSeries: FragmentIonSeries.Value,
- 
+  
   // Immutable optional fields
   val neutralLoss: Option[NeutralLoss.Value] = None,
   
   var properties: Option[FragmentIonTypeProperties] = None
+  
 ) {
   
+  def this(ionSeriesName: String, properties: Option[FragmentIonTypeProperties] = None ) = {
+    this( FragmentIonType.generateNewId(),
+          FragmentIonSeries.withName(ionSeriesName),
+          FragmentIonType.parseNeutralLoss(ionSeriesName),
+          properties )
+  }  
+  
+  val isReverseSeries = Fragmentation.isReverseSeries(ionSeries.toString)
+  
   override def toString():String = {
-    if( neutralLoss != None ) ionSeries + "-" + neutralLoss.get   
-    else ionSeries.toString
+    this.ionSeries.toString
+    //if( neutralLoss != None ) ionSeries + "-" + neutralLoss.get   
+    //else ionSeries.toString
   }
 }
 
@@ -137,7 +155,7 @@ case class RequiredSeries (
 
 }
 
-case class TheoreticalFragmentIon(
+case class FragmentIonRequirement(
     
   // Required fields
   val description: String,
--- Proline-OM/src/main/scala/fr/proline/core/om/model/msi/ObjectTreeClasses.scala
+++ Proline-OM/src/main/scala/fr/proline/core/om/model/msi/ObjectTreeClasses.scala
@@ -6,17 +6,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 
 /** 
-* @param fragmentMatches 
-**/
-@JsonSnakeCase
-@JsonInclude( Include.NON_NULL )
-case class SpectrumMatch (
-@BeanProperty var fragmentMatches: Array[FragmentMatch]
-)
-
-
-/** 
-* @param ionSerie 
+* @param label 
 * @param aaPosition 
 * @param type 
 * @param charge 
@@ -28,15 +18,86 @@ case class SpectrumMatch (
 **/
 @JsonSnakeCase
 @JsonInclude( Include.NON_NULL )
-case class FragmentMatch ( 
-  @BeanProperty var ionSerie: String,
-  @BeanProperty var aaPosition: Int,
-  @BeanProperty var `type`: String = "regular",
-  @BeanProperty var charge: Int,
-  @BeanProperty var moz: Double,
-  @BeanProperty var calculatedMoz: Double,
-  @BeanProperty var intensity: Float,
-  @BeanProperty var neutralLossMass: Option[Double] = None,
-  @BeanProperty var theoreticalFragmentId: Int
+case class FragmentMatch (  
+  var label: String,
+  //val ionSeries: String,
+  //val aaPosition: Int,
+  var `type`: String = FragmentMatchType.REGULAR.toString,
+  //var charge: Int,
+  var moz: Double,
+  var calculatedMoz: Double,  
+  var intensity: Float,
+  var neutralLossMass: Option[Double] = None
+) {
+  
+  @transient private var _ionSeries: String = null
+  @transient private var _aaPosition: Int = 0
+  @transient private var _charge: Int = 1
+  
+  private def _parseLabelIfNotDone() {
+    if( _ionSeries == null ) {
+      val FullLabelRegex = """(\w+)\((\d+)\)(\w*)""".r      
+      val FullLabelRegex(ionSeries, aaPositionStr, chargeStr) = label
+      
+      this._ionSeries = ionSeries
+      this._aaPosition = aaPositionStr.toInt
+      
+      if( chargeStr.length > 0 )
+        this._charge = chargeStr.length
+    }
+  }
+  
+  def ionSeries: String = {_parseLabelIfNotDone(); this._ionSeries}
+  def aaPosition: Int = {_parseLabelIfNotDone(); this._aaPosition}
+  def charge: Int = {_parseLabelIfNotDone(); this._charge}
+  
+}
+
+object FragmentMatchType extends Enumeration {
+  val REGULAR = Value("REGULAR")
+  val INTERNAL = Value("INTERNAL")
+  val IMMONIUM = Value("IMMONIUM")
+}
+
+/** 
+* @param fragmentationTable 
+* @param fragmentMatches 
+**/
+@JsonSnakeCase
+@JsonInclude( Include.NON_NULL )
+case class SpectrumMatch (
+  val fragmentationTable: Array[TheoreticalFragmentSeries],
+  val fragmentMatches: Array[FragmentMatch]
 )
+
+@JsonSnakeCase
+@JsonInclude( Include.NON_NULL )
+case class TheoreticalFragmentSeries (
+  val fragSeries: String, // begins by an ionSeries name and may be followed by a "++" for doubly charge state
+  val masses: Array[Double]
+) {
+  @transient lazy val isReverse = Fragmentation.isReverseSeries(fragSeries)
+  
+  @transient private var _ionSeries: String = null
+  @transient private var _charge: Int = 1
+  
+  private def _parseFragSeriesIfNotDone() {
+    if( _ionSeries == null ) {
+      val FragSeriesRegex = """(\w+)([+]*).*""".r      
+      val FragSeriesRegex(ionSeries, chargeStr) = fragSeries
+      
+      this._ionSeries = ionSeries.replace("0","-H2O").replace("*","-NH3")
+      
+      if( chargeStr.length > 0 )
+        this._charge = chargeStr.length
+    }
+  }
+  
+  def ionSeries: String = {_parseFragSeriesIfNotDone(); this._ionSeries}
+  def charge: Int = {_parseFragSeriesIfNotDone(); this._charge}
+}
+
+
+
+
 
