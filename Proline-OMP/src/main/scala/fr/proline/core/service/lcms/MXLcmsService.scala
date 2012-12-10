f867eebe83bce21c9b6365e083ed46c6a9eb153a
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-12-10 14:34:56 +0000

    Add missing file

---
+++ Proline-OMP/src/main/scala/fr/proline/core/service/lcms/MXLcmsService.scala
@@ -0,0 +1,14 @@
+package fr.proline.core.service.lcms
+
+import fr.proline.api.service.IService
+import fr.proline.core.dal.SQLQueryHelper
+import fr.proline.repository.IDatabaseConnector
+
+trait MXLcmsService extends IService {
+
+  val lcmsDbConnector: IDatabaseConnector
+  val lcmsQueryHelper = new SQLQueryHelper(lcmsDbConnector)
+  val ezDBC = lcmsQueryHelper.ezDBC
+  val inExprLimit = ezDBC.getInExpressionCountLimit
+  
+}
