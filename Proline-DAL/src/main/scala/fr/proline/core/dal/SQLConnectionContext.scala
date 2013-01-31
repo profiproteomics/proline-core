dfc4a962f3713c595a79ea4ffa802d5af69cdeed
Laurent Martin <laurent.martin2@cea.fr>
2013-01-31 09:58:31 +0000

    Use Scala lazy val initializater instead of cached var and synchronized block.
    #7026

--- Proline-DAL/src/main/scala/fr/proline/core/dal/SQLConnectionContext.scala
+++ Proline-DAL/src/main/scala/fr/proline/core/dal/SQLConnectionContext.scala
@@ -2,18 +2,11 @@ package fr.proline.core.dal
 
 import java.sql.Connection
 
-import fr.profi.jdbc.easy.EasyDBC
 import fr.proline.context.DatabaseConnectionContext
 import fr.proline.repository.DriverType
 import javax.persistence.EntityManager
 
 class SQLConnectionContext private (entityManager: EntityManager, connection: Connection, driverType: DriverType) extends DatabaseConnectionContext(entityManager, connection, driverType) {
-
-  val m_sqlContextLock = new AnyRef()
-
-  /* All mutable fields are @GuardedBy("m_sqlContextLock") */
-
-  private var m_ezDBC: EasyDBC = null
 
   /* SQL Constructor */
   def this(connection: Connection, driverType: DriverType) = this(null, connection, driverType)
@@ -21,23 +14,14 @@ class SQLConnectionContext private (entityManager: EntityManager, connection: Co
   /* Copy constructor from another dbContext */
   def this(dbContext: DatabaseConnectionContext) = this(dbContext.getEntityManager, dbContext.getConnection, dbContext.getDriverType)
 
-  def ezDBC(): EasyDBC = {
+  lazy val ezDBC = {
+    val sqlConnection = getConnection
 
-    m_sqlContextLock.synchronized {
+    if (sqlConnection == null) {
+      throw new IllegalArgumentException("SQLConnectionContext has no a valid SQL Connection")
+    }
 
-      if (m_ezDBC == null) {
-        val sqlConnection = getConnection
-
-        if (sqlConnection == null) {
-          throw new IllegalArgumentException("SQLConnectionContext has no a valid SQL Connection")
-        }
-
-        m_ezDBC = ProlineEzDBC(sqlConnection, getDriverType)
-      }
-
-      m_ezDBC
-    } // End of synchronized block on m_sqlContextLock
-
+    ProlineEzDBC(sqlConnection, getDriverType)
   }
 
 }
