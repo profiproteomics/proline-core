5ceb0d144513f13103cda590045f8d41e1201893
Veronique Dupierris <veronique.dupierris@cea.fr>
2013-02-07 09:54:18 +0000

    comment

--- Proline-OMP/src/main/scala/fr/proline/core/algo/msi/inference/IProteinSetInferer.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msi/inference/IProteinSetInferer.scala
@@ -5,6 +5,12 @@ import fr.proline.core.om.model.msi.ResultSummary
 
 trait IProteinSetInferer {
 
+  /**
+   * Create a ResultSummary for specified resultSet.
+   * All data will be considered while inferring Protein Sets
+   * 
+   * TODO : Specify a set of peptideMatch to consider   
+   */
   def computeResultSummary( resultSet: ResultSet ): ResultSummary
   
 }
