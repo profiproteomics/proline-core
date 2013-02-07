2d6f26bfb635bbf699b2fb32770b7f643679aaf5
Veronique Dupierris <veronique.dupierris@cea.fr>
2013-02-07 10:05:49 +0000

    set Dataset Type on IdentificationDataset creation

--- Proline-ORM/src/main/java/fr/proline/core/orm/uds/IdentificationDataset.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/uds/IdentificationDataset.java
@@ -4,9 +4,6 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
@@ -36,6 +33,7 @@ public class IdentificationDataset extends Dataset implements Serializable {
     private Run run;
 
     public IdentificationDataset() {
+	super.setType(DatasetType.IDENTIFICATION);
     }
 
 
