3b7769c1ddceed56d4cd5f73ac918f87d7e9dd7f
Veronique Dupierris <veronique.dupierris@cea.fr>
2013-02-07 09:56:11 +0000

    validation refactoring: Use of Filter classes

--- Proline-OMP/src/main/scala/fr/proline/core/algo/msi/validation/ValidationClasses.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msi/validation/ValidationClasses.scala
@@ -1,29 +1,12 @@
 package fr.proline.core.algo.msi.validation
 
 import scala.collection.mutable.HashMap
-import com.codahale.jerkson.JsonSnakeCase
-
-trait ValidationParams {
-  val minPepSeqLength: Int
-  val properties: Option[Map[String,Any]]
-}
-
-@JsonSnakeCase
-case class ComputerValidationParams( expectedFdr: Float, minPepSeqLength: Int = 0, properties: Option[Map[String,Any]] = None ) extends ValidationParams
-
-@JsonSnakeCase
-case class UserValidationParams( pValue: Float, minPepSeqLength: Int = 0, properties: Option[Map[String,Any]] = None ) extends ValidationParams
-
-object TargetDecoyModes extends Enumeration {
-  type Mode = Value
-  val separated = Value("separated")
-  val concatenated = Value("concatenated")
-}
 
 case class ValidationResult( nbTargetMatches: Int,
                              nbDecoyMatches: Option[Int] = None,
                              fdr: Option[Float] = None,
                              properties: Option[HashMap[String,Any]] = None
                             )
+                            
 case class ValidationResults( expectedResult: ValidationResult, computedResults: Option[Seq[ValidationResult]] )
   
