7d2493265012d49917c411a20a3547ebaa2ecd71
Christophe Bruley <christophe.bruley@cea.fr>
2013-03-22 10:59:11 +0000

    - variant of ParsimoniousProteinSetInferer added

--- Proline-OMP/src/main/scala/fr/proline/core/algo/msi/ProteinSetInferer.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msi/ProteinSetInferer.scala
@@ -3,14 +3,17 @@ package fr.proline.core.algo.msi
 import inference._
 
 object InferenceMethods extends Enumeration {
-  type InferenceMethod = Value
+  type InferenceMethods = Value
   val parsimonious = Value("parsimonious")
+  val communist = Value("communist")
+  
 }
 
 object ProteinSetInferer {
   
-  def apply( methodName: InferenceMethods.InferenceMethod ): IProteinSetInferer = { methodName match {
+  def apply( methodName: InferenceMethods.InferenceMethods ): IProteinSetInferer = { methodName match {
     case InferenceMethods.parsimonious => new ParsimoniousProteinSetInferer()
+    case InferenceMethods.communist => new CommunistProteinSetInferer()
     }
   }
 
