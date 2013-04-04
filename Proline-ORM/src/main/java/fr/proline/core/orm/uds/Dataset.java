077bad7a960e38f6af751d33ec3441e4e52c6118
Laurent Martin <laurent.martin2@cea.fr>
2013-04-04 10:00:52 +0000

    Add _TRASH_ as @DataSet.type@ enum.

--- Proline-ORM/src/main/java/fr/proline/core/orm/uds/Dataset.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/uds/Dataset.java
@@ -23,384 +23,371 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-import fr.proline.core.orm.msi.ResultSummary;
 import fr.proline.core.orm.msi.ResultSet;
-
-
-
+import fr.proline.core.orm.msi.ResultSummary;
 
 /**
  * The persistent class for the dataset database table.
  * 
  */
 @Entity
-@Table(name="data_set")
-@Inheritance(strategy=InheritanceType.JOINED)
+@Table(name = "data_set")
+@Inheritance(strategy = InheritanceType.JOINED)
 @NamedQueries({
-    @NamedQuery(name="findDatasetByProject",
-		query="Select ds from Dataset ds where ds.project.id =:id"),
-    @NamedQuery(name="findRootDatasetByProject",
-		query="Select ds from Dataset ds where ds.project.id =:id and ds.parentDataset is null"),
-    @NamedQuery(name="findDatasetNamesByProject",
-    		query="Select ds.name from Dataset ds where ds.project.id =:id"),
-    @NamedQuery(name="findRootDatasetNamesByProject",
-    		query="Select ds.name from Dataset ds where ds.project.id =:id and ds.parentDataset is null"),
-    @NamedQuery(name="findDatasetByNameAndProject",
-    	query="Select ds from Dataset ds where ds.project.id =:id and ds.name=:name"),
-    @NamedQuery(name="findRootDatasetByNameAndProject",
-	query="Select ds from Dataset ds where ds.project.id =:id and ds.name=:name and ds.parentDataset is null")
-})
-public class Dataset implements Serializable{
-    
-	private static final long serialVersionUID = 1L;
+	@NamedQuery(name = "findDatasetByProject", query = "Select ds from Dataset ds where ds.project.id =:id"),
+	@NamedQuery(name = "findRootDatasetByProject", query = "Select ds from Dataset ds where ds.project.id =:id and ds.parentDataset is null"),
+	@NamedQuery(name = "findDatasetNamesByProject", query = "Select ds.name from Dataset ds where ds.project.id =:id"),
+	@NamedQuery(name = "findRootDatasetNamesByProject", query = "Select ds.name from Dataset ds where ds.project.id =:id and ds.parentDataset is null"),
+	@NamedQuery(name = "findDatasetByNameAndProject", query = "Select ds from Dataset ds where ds.project.id =:id and ds.name=:name"),
+	@NamedQuery(name = "findRootDatasetByNameAndProject", query = "Select ds from Dataset ds where ds.project.id =:id and ds.name=:name and ds.parentDataset is null") })
+public class Dataset implements Serializable {
 
+    private static final long serialVersionUID = 1L;
 
-	public enum DatasetType {
-		IDENTIFICATION, QUANTITATION, AGGREGATE
-	};
-	    
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
+    public enum DatasetType {
+	IDENTIFICATION, QUANTITATION, AGGREGATE, TRASH
+    };
 
-	@Column(name="creation_timestamp")
-	private Timestamp creationTimestamp  = new Timestamp(new Date().getTime());
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
-	private String description;
+    @Column(name = "creation_timestamp")
+    private Timestamp creationTimestamp = new Timestamp(new Date().getTime());
 
-	@Column(name="fraction_count")
-	private Integer fractionCount;
+    private String description;
 
-	private String keywords;
+    @Column(name = "fraction_count")
+    private Integer fractionCount;
 
-	@Column(name="modification_log")
-	private String modificationLog;
+    private String keywords;
 
-	private String name;
+    @Column(name = "modification_log")
+    private String modificationLog;
 
-	@Enumerated(value = EnumType.STRING)    
-	private DatasetType type;
-	
-	private Integer number;
+    private String name;
 
-	@ManyToOne
-	@JoinColumn(name="fractionation_id")
-	private Fractionation fractionation;
-	
-	@ManyToOne
-	@JoinColumn(name="aggregation_id")
-	private Aggregation aggregation;
-	
-	
-	@ManyToOne
-	@JoinColumn(name="project_id")
-	private Project project;
+    @Enumerated(value = EnumType.STRING)
+    private DatasetType type;
 
-	@Column(name="serialized_properties")
-	private String serializedProperties;
+    private Integer number;
 
-	//bi-directional many-to-one association to DataSet
-	@ManyToOne
-	@JoinColumn(name="parent_dataset_id")
-	private Dataset parentDataset;
-	
-	//bi-directional many-to-one association to DataSet
-	@OneToMany(mappedBy="parentDataset")
-	private Set<Dataset> children;	
-
-	
-	@Column(name="result_set_id")
-	private Integer resultSetId;
-	
-	@Column(name="result_summary_id")
-	private Integer resultSummaryId;
-	
-	//bi-directional many-to-one association to BiologicalSample
-	@OneToMany(mappedBy="dataset")
-	private Set<BiologicalSample> biologicalSamples;
-
-	//bi-directional many-to-one association to GroupSetup
-	@OneToMany(mappedBy="dataset")
-	private Set<GroupSetup> groupSetups;
-
-	//bi-directional many-to-one association to QuantChannel
-	@OneToMany(mappedBy="dataset")
-	private Set<QuantitationChannel> quantitationChannels;
-
-	//uni-directional many-to-one association to QuantMethod
     @ManyToOne
-	@JoinColumn(name="quant_method_id")
-	private QuantitationMethod method;
+    @JoinColumn(name = "fractionation_id")
+    private Fractionation fractionation;
 
-	//bi-directional many-to-one association to MasterQuantitationChannel
-	@OneToMany(mappedBy="dataset")
-	private Set<MasterQuantitationChannel> masterQuantitationChannels;
+    @ManyToOne
+    @JoinColumn(name = "aggregation_id")
+    private Aggregation aggregation;
 
-	//bi-directional many-to-one association to SampleAnalysis
-	@OneToMany(mappedBy="dataset")
-	private Set<SampleAnalysis> sampleReplicates;
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
 
-	// Transient Variables not saved in database
-    @Transient private TransientData transientData = null;
-	
+    @Column(name = "serialized_properties")
+    private String serializedProperties;
+
+    // bi-directional many-to-one association to DataSet
+    @ManyToOne
+    @JoinColumn(name = "parent_dataset_id")
+    private Dataset parentDataset;
+
+    // bi-directional many-to-one association to DataSet
+    @OneToMany(mappedBy = "parentDataset")
+    private Set<Dataset> children;
+
+    @Column(name = "result_set_id")
+    private Integer resultSetId;
+
+    @Column(name = "result_summary_id")
+    private Integer resultSummaryId;
+
+    // bi-directional many-to-one association to BiologicalSample
+    @OneToMany(mappedBy = "dataset")
+    private Set<BiologicalSample> biologicalSamples;
+
+    // bi-directional many-to-one association to GroupSetup
+    @OneToMany(mappedBy = "dataset")
+    private Set<GroupSetup> groupSetups;
+
+    // bi-directional many-to-one association to QuantChannel
+    @OneToMany(mappedBy = "dataset")
+    private Set<QuantitationChannel> quantitationChannels;
+
+    // uni-directional many-to-one association to QuantMethod
+    @ManyToOne
+    @JoinColumn(name = "quant_method_id")
+    private QuantitationMethod method;
+
+    // bi-directional many-to-one association to MasterQuantitationChannel
+    @OneToMany(mappedBy = "dataset")
+    private Set<MasterQuantitationChannel> masterQuantitationChannels;
+
+    // bi-directional many-to-one association to SampleAnalysis
+    @OneToMany(mappedBy = "dataset")
+    private Set<SampleAnalysis> sampleReplicates;
+
+    // Transient Variables not saved in database
+    @Transient
+    private TransientData transientData = null;
+
     protected Dataset() {
     }
 
     public Dataset(Project project) {
-    	this.project = project;
+	this.project = project;
     }
 
-    
-	public Integer getId() {
-		return this.id;
+    public Integer getId() {
+	return this.id;
+    }
+
+    public void setId(Integer id) {
+	this.id = id;
+    }
+
+    public Timestamp getCreationTimestamp() {
+	return this.creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Timestamp creationTimestamp) {
+	this.creationTimestamp = creationTimestamp;
+    }
+
+    public String getDescription() {
+	return this.description;
+    }
+
+    public void setDescription(String description) {
+	this.description = description;
+    }
+
+    public Integer getFractionCount() {
+	return this.fractionCount;
+    }
+
+    public void setFractionCount(Integer fractionCount) {
+	this.fractionCount = fractionCount;
+    }
+
+    public DatasetType getType() {
+	return type;
+    }
+
+    public void setType(DatasetType type) {
+	this.type = type;
+    }
+
+    public Dataset getParentDataset() {
+	return parentDataset;
+    }
+
+    public void setParentDataset(Dataset parentDataset) {
+	this.parentDataset = parentDataset;
+    }
+
+    public Integer getResultSetId() {
+	return resultSetId;
+    }
+
+    public void setResultSetId(Integer resultSetId) {
+	this.resultSetId = resultSetId;
+    }
+
+    public Integer getResultSummaryId() {
+	return resultSummaryId;
+    }
+
+    public void setResultSummaryId(Integer resultSummaryId) {
+	this.resultSummaryId = resultSummaryId;
+    }
+
+    public String getKeywords() {
+	return this.keywords;
+    }
+
+    public void setKeywords(String keywords) {
+	this.keywords = keywords;
+    }
+
+    public String getModificationLog() {
+	return this.modificationLog;
+    }
+
+    public void setModificationLog(String modificationLog) {
+	this.modificationLog = modificationLog;
+    }
+
+    public String getName() {
+	return this.name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public Integer getNumber() {
+	return this.number;
+    }
+
+    public void setNumber(Integer number) {
+	this.number = number;
+    }
+
+    public Project getProject() {
+	return this.project;
+    }
+
+    public void setProject(Project project) {
+	this.project = project;
+    }
+
+    public String getSerializedProperties() {
+	return this.serializedProperties;
+    }
+
+    public void setSerializedProperties(String serializedProperties) {
+	this.serializedProperties = serializedProperties;
+    }
+
+    public Set<BiologicalSample> getBiologicalSamples() {
+	return this.biologicalSamples;
+    }
+
+    public void setBiologicalSamples(Set<BiologicalSample> biologicalSamples) {
+	this.biologicalSamples = biologicalSamples;
+    }
+
+    public Set<GroupSetup> getGroupSetups() {
+	return this.groupSetups;
+    }
+
+    public void setGroupSetups(Set<GroupSetup> groupSetups) {
+	this.groupSetups = groupSetups;
+    }
+
+    public Set<QuantitationChannel> getQuantitationChannels() {
+	return this.quantitationChannels;
+    }
+
+    public void setQuantitationChannels(Set<QuantitationChannel> quantitationChannels) {
+	this.quantitationChannels = quantitationChannels;
+    }
+
+    public QuantitationMethod getMethod() {
+	return this.method;
+    }
+
+    public void setMethod(QuantitationMethod method) {
+	this.method = method;
+    }
+
+    public Set<MasterQuantitationChannel> getMasterQuantitationChannels() {
+	return this.masterQuantitationChannels;
+    }
+
+    public void setMasterQuantitationChannels(Set<MasterQuantitationChannel> masterQuantitationChannels) {
+	this.masterQuantitationChannels = masterQuantitationChannels;
+    }
+
+    public Set<SampleAnalysis> getSampleReplicates() {
+	return this.sampleReplicates;
+    }
+
+    public void setSampleReplicates(Set<SampleAnalysis> sampleReplicates) {
+	this.sampleReplicates = sampleReplicates;
+    }
+
+    public Fractionation getFractionation() {
+	return fractionation;
+    }
+
+    public void setFractionation(Fractionation fractionation) {
+	this.fractionation = fractionation;
+    }
+
+    public Aggregation getAggregation() {
+	return aggregation;
+    }
+
+    public void setAggregation(Aggregation aggregation) {
+	this.aggregation = aggregation;
+    }
+
+    public Set<Dataset> getChildren() {
+	return children;
+    }
+
+    public void setChildren(Set<Dataset> children) {
+	this.children = children;
+    }
+
+    public Set<IdentificationDataset> getIdentificationDataset() {
+	Set<IdentificationDataset> idfDS = new HashSet<IdentificationDataset>();
+	if ((getChildren() == null || getChildren().isEmpty())
+		&& (getType().equals(DatasetType.IDENTIFICATION))) {
+	    idfDS.add((IdentificationDataset) this);
 	}
 
-	public void setId(Integer id) {
-		this.id = id;
+	for (Dataset ds : getChildren()) {
+	    idfDS.addAll(ds.getIdentificationDataset());
+	}
+	return idfDS;
+
+    }
+
+    // FIXME: (IdentificationDataset)this in getIdentificationDataset throws a ClassCastException
+    // this method is just a workaround but a better solution has to be implemented
+    public Set<Dataset> getIdentificationDatasets() {
+	Set<Dataset> idfDS = new HashSet<Dataset>();
+	if ((getChildren() == null || getChildren().isEmpty())
+		&& (getType().equals(DatasetType.IDENTIFICATION))) {
+	    idfDS.add((Dataset) this);
 	}
 
-	public Timestamp getCreationTimestamp() {
-		return this.creationTimestamp;
+	for (Dataset ds : getChildren()) {
+	    idfDS.addAll(ds.getIdentificationDatasets());
 	}
+	return idfDS;
 
-	public void setCreationTimestamp(Timestamp creationTimestamp) {
-		this.creationTimestamp = creationTimestamp;
-	}
+    }
 
-	public String getDescription() {
-		return this.description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Integer getFractionCount() {
-		return this.fractionCount;
-	}
-
-	public void setFractionCount(Integer fractionCount) {
-		this.fractionCount = fractionCount;
-	}
-
-
-	public DatasetType getType() {
-	    return type;
-	}
-
-	public void setType(DatasetType type) {
-	    this.type = type;
-	}
-
-	public Dataset getParentDataset() {
-	    return parentDataset;
-	}
-
-	public void setParentDataset(Dataset parentDataset) {
-	    this.parentDataset = parentDataset;
-	}
-
-	public Integer getResultSetId() {
-	    return resultSetId;
-	}
-
-	public void setResultSetId(Integer resultSetId) {
-	    this.resultSetId = resultSetId;
-	}
-
-	public Integer getResultSummaryId() {
-	    return resultSummaryId;
-	}
-
-	public void setResultSummaryId(Integer resultSummaryId) {
-	    this.resultSummaryId = resultSummaryId;
-	}
-
-	public String getKeywords() {
-		return this.keywords;
-	}
-
-	public void setKeywords(String keywords) {
-		this.keywords = keywords;
-	}
-
-	public String getModificationLog() {
-		return this.modificationLog;
-	}
-
-	public void setModificationLog(String modificationLog) {
-		this.modificationLog = modificationLog;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getNumber() {
-		return this.number;
-	}
-
-	public void setNumber(Integer number) {
-		this.number = number;
-	}
-
-	public Project getProject() {
-		return this.project;
-	}
-
-	public void setProject(Project project) {
-		this.project = project;
-	}
-
-	public String getSerializedProperties() {
-		return this.serializedProperties;
-	}
-
-	public void setSerializedProperties(String serializedProperties) {
-		this.serializedProperties = serializedProperties;
-	}
-
-	public Set<BiologicalSample> getBiologicalSamples() {
-		return this.biologicalSamples;
-	}
-
-	public void setBiologicalSamples(Set<BiologicalSample> biologicalSamples) {
-		this.biologicalSamples = biologicalSamples;
-	}
-	
-	public Set<GroupSetup> getGroupSetups() {
-		return this.groupSetups;
-	}
-
-	public void setGroupSetups(Set<GroupSetup> groupSetups) {
-		this.groupSetups = groupSetups;
-	}
-	
-	public Set<QuantitationChannel> getQuantitationChannels() {
-		return this.quantitationChannels;
-	}
-
-	public void setQuantitationChannels(Set<QuantitationChannel> quantitationChannels) {
-		this.quantitationChannels = quantitationChannels;
-	}
-	
-	public QuantitationMethod getMethod() {
-		return this.method;
-	}
-
-	public void setMethod(QuantitationMethod method) {
-		this.method = method;
-	}
-	
-	public Set<MasterQuantitationChannel> getMasterQuantitationChannels() {
-		return this.masterQuantitationChannels;
-	}
-
-	public void setMasterQuantitationChannels(Set<MasterQuantitationChannel> masterQuantitationChannels) {
-		this.masterQuantitationChannels = masterQuantitationChannels;
-	}
-	
-	public Set<SampleAnalysis> getSampleReplicates() {
-		return this.sampleReplicates;
-	}
-
-	public void setSampleReplicates(Set<SampleAnalysis> sampleReplicates) {
-		this.sampleReplicates = sampleReplicates;
-	}
-
-	public Fractionation getFractionation() {
-	    return fractionation;
-	}
-
-	public void setFractionation(Fractionation fractionation) {
-	    this.fractionation = fractionation;
-	}
-
-	public Aggregation getAggregation() {
-	    return aggregation;
-	}
-
-	public void setAggregation(Aggregation aggregation) {
-	    this.aggregation = aggregation;
-	}
-
-	public Set<Dataset> getChildren() {
-	    return children;
-	}
-
-	public void setChildren(Set<Dataset> children) {
-	    this.children = children;
-	}
-	
-	public Set<IdentificationDataset> getIdentificationDataset(){
-	    Set<IdentificationDataset> idfDS = new HashSet<IdentificationDataset>();
-	    if ( (getChildren() == null || getChildren().isEmpty())&& (getType().equals(DatasetType.IDENTIFICATION)) ) {		    	    	  	    		  	    		
-		idfDS.add((IdentificationDataset)this);       	    	  
-	    }
-				
-	    for (Dataset ds : getChildren()) {							
-		idfDS.addAll(ds.getIdentificationDataset());
-	    }
-	    return idfDS;
-	    
-	}
-	
-	// FIXME: (IdentificationDataset)this in getIdentificationDataset throws a ClassCastException
-	// this method is just a workaround but a better solution has to be implemented
-    public Set<Dataset> getIdentificationDatasets(){
-      Set<Dataset> idfDS = new HashSet<Dataset>();
-      if ( (getChildren() == null || getChildren().isEmpty())&& (getType().equals(DatasetType.IDENTIFICATION)) ) {
-      idfDS.add((Dataset)this);
-      }
-      
-      for (Dataset ds : getChildren()) {
-      idfDS.addAll(ds.getIdentificationDatasets());
-      }
-      return idfDS;
-      
-  }
-	
-	 
     public TransientData getTransientData() {
-    	if (transientData == null) {
-    		transientData = new TransientData();
-    	}
-    	return transientData;
+	if (transientData == null) {
+	    transientData = new TransientData();
+	}
+	return transientData;
     }
-    
+
     /**
-     * Transient Data which will be not saved in database Used by the Proline
-     * Studio IHM
-     *
+     * Transient Data which will be not saved in database Used by the Proline Studio IHM
+     * 
      * @author JM235353
      */
     public static class TransientData implements Serializable {
 
-        private static final long serialVersionUID = 1L;
-        private ResultSummary resultSummary = null; //JPM.WART : uds package has no access to msi package
-        private ResultSet resultSet = null;
+	private static final long serialVersionUID = 1L;
+	private ResultSummary resultSummary = null; // JPM.WART : uds package has no access to msi package
+	private ResultSet resultSet = null;
 
+	protected TransientData() {
+	}
 
-        protected TransientData() {
-        }
+	public ResultSummary getResultSummary() {
+	    return resultSummary;
+	}
 
-        public ResultSummary getResultSummary() {  
-            return resultSummary;
-        }
-        public void setResultSummary(ResultSummary resultSummary) {  
-            this.resultSummary = resultSummary;
-        }
-        
-        public ResultSet getResultSet() {  
-            return resultSet;
-        }
-        public void setResultSet(ResultSet resultSet) {  
-            this.resultSet = resultSet;
-        }
+	public void setResultSummary(ResultSummary resultSummary) {
+	    this.resultSummary = resultSummary;
+	}
+
+	public ResultSet getResultSet() {
+	    return resultSet;
+	}
+
+	public void setResultSet(ResultSet resultSet) {
+	    this.resultSet = resultSet;
+	}
     }
-	
+
 }
