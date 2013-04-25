0bdd5c84e73f71ce0c789c1c9e8f02f0a91c706f
Veronique Dupierris <veronique.dupierris@cea.fr>
2013-04-25 11:48:42 +0000

    Retrieve proteinSet if not defined in peptideSet

--- Proline-OMP/src/main/scala/fr/proline/core/algo/msi/scoring/MascotStandardScoreUpdater.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msi/scoring/MascotStandardScoreUpdater.scala
@@ -14,9 +14,10 @@ class MascotStandardScoreUpdater extends IProtSetAndPepSetScoreUpdater {
 	   val pepMatches = entry._2.sortWith((a,b) => a.score > b.score)
            pepSet.score += pepMatches(0).score
        })
-       if(pepSet.proteinSet!=null && pepSet.proteinSet.isDefined ){
-         pepSet.proteinSet.get.score=pepSet.score
-         pepSet.proteinSet.get.scoreType = ProtSetScoring.MASCOT_STANDARD_SCORE.toString
+       if(pepSet.getProteinSetId !=0 ){
+         val protSet =  if(pepSet.proteinSet !=null && pepSet.proteinSet.isDefined) pepSet.proteinSet.get else rsm.proteinSetById(pepSet.getProteinSetId)
+         protSet.score=pepSet.score
+         protSet.scoreType = ProtSetScoring.MASCOT_STANDARD_SCORE.toString
        }
     }
     
