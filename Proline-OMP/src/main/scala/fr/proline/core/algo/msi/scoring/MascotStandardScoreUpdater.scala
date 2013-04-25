f0ece8b6e255996d570822ca6255927c7dae51c4
Veronique Dupierris <veronique.dupierris@cea.fr>
2013-04-25 07:57:45 +0000

    fixes #7469 : getBestPepMatchesByProtSetId incorrect (see #7533) . Use other algo to go through peptideSet & proteinSet

--- Proline-OMP/src/main/scala/fr/proline/core/algo/msi/scoring/MascotStandardScoreUpdater.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msi/scoring/MascotStandardScoreUpdater.scala
@@ -7,27 +7,17 @@ class MascotStandardScoreUpdater extends IProtSetAndPepSetScoreUpdater {
 
   def updateScoreOfProteinSets( rsm: ResultSummary, params: Any* ) {
     
-    val bestPepMatchesByProtSetId = rsm.getBestPepMatchesByProtSetId
-    
-    for( proteinSet <- rsm.proteinSets ) {
-      
-      val proteinSetId = proteinSet.id
-      val bestPepMatches = bestPepMatchesByProtSetId(proteinSetId)
-      proteinSet.score = 0
-      for (bestPepMatch <- bestPepMatches) {
-      	proteinSet.score += bestPepMatch.score
-      }
-      proteinSet.scoreType = ProtSetScoring.MASCOT_STANDARD_SCORE.toString
-      proteinSet.peptideSet.score = proteinSet.score
-    }
-    
-    for(orphanPepSet <- rsm.peptideSets.filterNot(_.getProteinSetId>0)){
-       orphanPepSet.score = 0
-       val pepMatchesByPepInsID = orphanPepSet.getPeptideInstances.map( pi => pi.id -> pi.peptideMatches) 
+    for(pepSet <- rsm.peptideSets){
+       pepSet.score = 0
+       val pepMatchesByPepInsID = pepSet.getPeptideInstances.map( pi => pi.id -> pi.peptideMatches) 
        pepMatchesByPepInsID.foreach( entry => {
 	   val pepMatches = entry._2.sortWith((a,b) => a.score > b.score)
-           orphanPepSet.score += pepMatches(0).score
+           pepSet.score += pepMatches(0).score
        })
+       if(pepSet.proteinSet!=null && pepSet.proteinSet.isDefined ){
+         pepSet.proteinSet.get.score=pepSet.score
+         pepSet.proteinSet.get.scoreType = ProtSetScoring.MASCOT_STANDARD_SCORE.toString
+       }
     }
     
     ()
