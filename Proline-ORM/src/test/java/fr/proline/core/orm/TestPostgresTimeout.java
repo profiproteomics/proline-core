0c1c2a20cacd11e1b9c35a7f0986678f8141f5a5
Laurent Martin <laurent.martin2@cea.fr>
2013-05-17 15:58:31 +0000

    Add some threads to check PostgreSQL connection timeouts.
    refs #7607

--- Proline-ORM/src/test/java/fr/proline/core/orm/TestPostgresTimeout.java
+++ Proline-ORM/src/test/java/fr/proline/core/orm/TestPostgresTimeout.java
@@ -2,6 +2,9 @@ package fr.proline.core.orm;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
@@ -26,7 +29,9 @@ public class TestPostgresTimeout extends DatabaseTestCase {
 
     private static final long BASE_TIMEOUT = 3 * 60 * 60 * 1000L; // 3 hour
 
-    private static final long MAX_OFFSET = 60 * 60 * 1000L; // 1 hour
+    private static final long MAX_TIMEOUT_OFFSET = 60 * 60 * 1000L; // 1 hour
+
+    private static final int N_CONCURRENT_THREADS = 20;
 
     @Override
     public ProlineDatabaseType getProlineDatabaseType() {
@@ -39,43 +44,21 @@ public class TestPostgresTimeout extends DatabaseTestCase {
     }
 
     public void testPostgresqlTimeout() {
-	final IDatabaseConnector connector = getConnector();
+	final ExecutorService executor = Executors.newCachedThreadPool();
+
+	final IDatabaseConnector psDbConnector = getConnector();
 
 	boolean goOn = true;
 
 	while (goOn) {
 
-	    EntityManager em = null;
+	    for (int i = 0; i < N_CONCURRENT_THREADS; ++i) {
+		final Runnable queryTask = new QueryRunner(psDbConnector);
 
-	    try {
-		em = connector.getEntityManagerFactory().createEntityManager();
-
-		Query countQuery = em
-			.createQuery("select count (pep) from fr.proline.core.orm.ps.Peptide pep");
-
-		final Object obj = countQuery.getSingleResult();
-
-		if (obj instanceof Long) {
-		    LOG.info("There are {} peptides in PS Db", (Long) obj);
-		}
-
-	    } catch (Exception ex) {
-		LOG.error("Error running PostgreSQL quuery", ex);
-
-		fail(ex.getMessage());
-	    } finally {
-
-		if (em != null) {
-		    try {
-			em.close();
-		    } catch (Exception exClose) {
-			LOG.error("Error closing EntityManager", exClose);
-		    }
-		}
-
+		executor.execute(queryTask);
 	    }
 
-	    final long timeout = BASE_TIMEOUT + (long) (Math.random() * MAX_OFFSET);
+	    final long timeout = BASE_TIMEOUT + (long) (Math.random() * MAX_TIMEOUT_OFFSET);
 	    LOG.info("Waiting {} ms ...", timeout);
 
 	    try {
@@ -88,7 +71,9 @@ public class TestPostgresTimeout extends DatabaseTestCase {
 
 	}
 
-	connector.close();
+	psDbConnector.close();
+
+	executor.shutdown();
     }
 
     public static void main(final String[] args) {
@@ -98,6 +83,59 @@ public class TestPostgresTimeout extends DatabaseTestCase {
 	    test.testPostgresqlTimeout();
 	} finally {
 	    test.tearDown();
+	}
+
+    }
+
+}
+
+class QueryRunner implements Runnable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(QueryRunner.class);
+
+    private static final long QUERY_PAUSE = 60 * 1000L; // 1 minute
+
+    private final IDatabaseConnector m_psDbConnector;
+
+    public QueryRunner(final IDatabaseConnector psDbConnector) {
+
+	if (psDbConnector == null) {
+	    throw new IllegalArgumentException("PsDbConnector is null");
+	}
+
+	m_psDbConnector = psDbConnector;
+    }
+
+    public void run() {
+	EntityManager psEm = null;
+
+	try {
+	    psEm = m_psDbConnector.getEntityManagerFactory().createEntityManager();
+
+	    final Query countQuery = psEm
+		    .createQuery("select count (pep) from fr.proline.core.orm.ps.Peptide pep");
+
+	    final Object obj = countQuery.getSingleResult();
+
+	    if (obj instanceof Long) {
+		LOG.info("There are {} peptides in PS Db", (Long) obj);
+	    }
+
+	    Thread.sleep(QUERY_PAUSE); // Sleep 1 minute keeping psEm open ...
+	} catch (Exception ex) {
+	    LOG.error("Error running PostgreSQL query on PS Db", ex);
+
+	    fail(ex.getMessage());
+	} finally {
+
+	    if (psEm != null) {
+		try {
+		    psEm.close();
+		} catch (Exception exClose) {
+		    LOG.error("Error closing PS EntityManager", exClose);
+		}
+	    }
+
 	}
 
     }
