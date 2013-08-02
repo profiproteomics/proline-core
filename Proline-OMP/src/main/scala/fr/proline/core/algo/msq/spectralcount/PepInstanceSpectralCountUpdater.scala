d47e3fbd2f45600feff37479af3ba12e4000a506
Veronique Dupierris <veronique.dupierris@cea.fr>
2013-08-02 06:55:45 +0000

    Dataset hierarchy could be done on RSM or RS ! Change updatepeptideInstanceSC behaviour

--- Proline-OMP/src/main/scala/fr/proline/core/algo/msq/spectralcount/PepInstanceSpectralCountUpdater.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msq/spectralcount/PepInstanceSpectralCountUpdater.scala
@@ -15,172 +15,308 @@ import scala.collection.JavaConversions.collectionAsScalaIterable
 import java.util.HashSet
 import fr.proline.core.om.provider.ProviderDecoratedExecutionContext
 import fr.proline.core.om.provider.msi.IResultSetProvider
+import fr.proline.core.om.provider.msi.impl.SQLResultSummaryProvider
+import fr.proline.core.om.provider.msi.IResultSummaryProvider
 //import scala.collection.JavaConverters.asJavaCollectionConverter
 
 trait IPepInstanceSpectralCountUpdater {
-  
-  def updatePepInstanceSC( rsm: ResultSummary, execContext: IExecutionContext ): Unit
-  
+  /**
+   * *
+   * Update Peptide Instances basic SC using filtering information.
+   * In case of leave RSM, basic SC = PSM count
+   * In case of RSM (RSM.X) validated from a merged RS the following computation will be done :
+   *  - Get all leaves RS from RSM.Rs
+   *  - For each leave RS apply RSM.X PSM filters and create a Map : Peptide ID-> Basic Spectral Count
+   *  - Update RSM.X peptide instance SC using the sum of leave RS BSC.
+   * In case of RSM (RSM.Y) issued from a mergeRSM service, the following computation will be done :
+   *  - Get the leave RSM and compute SC (  basic SC = valid PSM count)
+   *  - update RSM.Y peptide instance SC using the sum of leave RSM peptide instance SC
+   */
+  def updatePepInstanceSC(rsm: ResultSummary, execContext: IExecutionContext): Unit
+
 }
 
 object PepInstanceFilteringLeafSCUpdater extends IPepInstanceSpectralCountUpdater with Logging {
 
-  private def getIsLeave(rsm: ResultSummary, execContext: IExecutionContext): Boolean = {
+  private def getIsLeaveRSM(rsmID: Long, execContext: IExecutionContext): Boolean = {
     var rsType: String = null
+    val rsId = getRSIdForRsmID(rsmID, execContext)
+
     val jdbcWork = new JDBCWork() {
       override def execute(con: Connection) {
+
         val getPMQuery = "SELECT type from result_set WHERE id = ?"
         val pStmt = con.prepareStatement(getPMQuery)
-        pStmt.setLong(1, rsm.getResultSetId)
+        pStmt.setLong(1, rsId)
         val sqlResultSet = pStmt.executeQuery()
         if (sqlResultSet.next)
           rsType = sqlResultSet.getString(1)
         pStmt.close()
       }
 
-    } // End of jdbcWork anonymous inner class    	  
+    } // End of jdbcWork anonymous inner class    	 
+
     execContext.getMSIDbConnectionContext().doWork(jdbcWork, false)
     if ((rsType matches "SEARCH") || (rsType matches "DECOY_SEARCH"))
       return true
     else
       return false
   }
-  
-  
-  /***
+
+  private def getRSIdForRsmID(rsmID: Long, execContext: IExecutionContext): Long = {
+    var rsID: Long = -1
+
+    val jdbcWork = new JDBCWork() {
+
+      override def execute(con: Connection) {
+        val getRSIdQuery = "SELECT result_set_id from result_summary WHERE id = ?"
+        val pStmt = con.prepareStatement(getRSIdQuery)
+        pStmt.setLong(1, rsmID)
+        val sqlResultSet = pStmt.executeQuery()
+        if (sqlResultSet.next) {
+          rsID = sqlResultSet.getLong(1)
+          pStmt.close()
+        }
+      }
+    } // End of jdbcWork anonymous inner class    	 
+
+    execContext.getMSIDbConnectionContext().doWork(jdbcWork, false)
+    if (rsID > 0)
+      return rsID
+    else
+      throw new IllegalArgumentException("Unable to get Result Set ID for ResultSummary ID " + rsmID)
+  }
+
+  /**
+   * *
    * Update Peptide Instances basic SC using filtering information.
    * In case of leave RSM, basic SC = PSM count
-   * In case of RSM (RSM.X) validated from a merged RS the following computation will be done : 
+   * In case of RSM (RSM.X) validated from a merged RS the following computation will be done :
    *  - Get all leaves RS from RSM.Rs
    *  - For each leave RS apply RSM.X PSM filters and create a Map : Peptide ID-> Basic Spectral Count
-   *  - Update RSM.X peptide instance SC using the sum of leave RS BSC. 
+   *  - Update RSM.X peptide instance SC using the sum of leave RS BSC.
+   * In case of RSM (RSM.Y) issued from a mergeRSM service, the following computation will be done :
+   *  - Get the leave RSM and compute SC (  basic SC = valid PSM count)
+   *  - update RSM.Y peptide instance SC using the sum of leave RSM peptide instance SC
    */
-  def updatePepInstanceSC( rsm: ResultSummary, execContext: IExecutionContext ): Unit = {    
-  
+  def updatePepInstanceSC(rsm: ResultSummary, execContext: IExecutionContext): Unit = {
+
     var spectralCountByPepId = new HashMap[Long, Int]()
-	val startTime =System.currentTimeMillis()
+    val startTime = System.currentTimeMillis()
 
-	val validPeptideMatchesB  = Seq.newBuilder[PeptideMatch]
-	rsm.getAllPeptideMatchesByPeptideSetId.values.foreach(a => {validPeptideMatchesB ++= a})
-	val validPeptideMatches : Seq[PeptideMatch] = validPeptideMatchesB.result
+    val validPeptideMatchesB = Seq.newBuilder[PeptideMatch]
+    rsm.getAllPeptideMatchesByPeptideSetId.values.foreach(a => { validPeptideMatchesB ++= a })
+    val validPeptideMatches: Seq[PeptideMatch] = validPeptideMatchesB.result
 
-	val isNativeRS = getIsLeave(rsm, execContext)
-	
-	if(isNativeRS){
-	  
-	   //VDS comment :Could get info from PeptideInstance.peptideMatches.filter on isValidated... but should be sure
-	  // PeptideInstance.peptideMatches != null ...
-	  
-	   // Peptide Instance SC = PepMatch Count
-	   for (psm <- validPeptideMatches) {
-		   var pepSC = spectralCountByPepId.getOrElse(psm.peptideId, 0)
-		   pepSC += 1		
-		   spectralCountByPepId.put(psm.peptideId, pepSC)
+    spectralCountByPepId = getRSMSpectralCountByPepId(rsm, rsm.id, rsm.peptideInstances.map(_.peptideId),  execContext)
+
+
+    val endTime = System.currentTimeMillis()
+    logger.debug(" Needed Time to calculate " + validPeptideMatches.length + " = " + (endTime - startTime) + " ms")
+
+    val tmpPepInstByPepId = Map() ++ rsm.peptideInstances.map { pepInst => (pepInst.peptideId -> pepInst) }
+    spectralCountByPepId.foreach(pepSc => {
+      if (tmpPepInstByPepId.get(pepSc._1).isDefined) {
+        tmpPepInstByPepId.get(pepSc._1).get.totalLeavesMatchCount = pepSc._2
+      } else {
+        throw new Exception("PeptideInstance associated to validated PeptideMatch not found for peptide id=" + pepSc._1)
       }
-		
-	} else { 
-		//Get leaves RS
-		val leavesRSs = getLeavesRS(rsm, execContext)
-		val appliedPSMFilters = ResultSummaryFilterBuilder.buildPeptideMatchFilters(rsm)
-		
-		// Get PepID->SC for each RS
-		leavesRSs.foreach(rs =>{
-			val rsPepInstSC : HashMap[Long, Int] = countValidPSMInResultSet(rs,appliedPSMFilters)
-			// Merge result in global result 
-			rsPepInstSC.foreach(entry => {
-			  var pepSC = spectralCountByPepId.getOrElse(entry._1, 0)
-			  pepSC += entry._2		
-			  spectralCountByPepId.put(entry._1, pepSC)
-			})			
-		})			 
-	}
-	
-    val endTime =System.currentTimeMillis()
-    logger.debug(" Needed Time to calculate "+validPeptideMatches.length+" = "+(endTime - startTime)+" ms")
-    
-    val tmpPepInstByPepId = Map() ++ rsm.peptideInstances.map { pepInst => ( pepInst.peptideId -> pepInst ) }     
-    spectralCountByPepId.foreach(pepSc => { 
-    	if(tmpPepInstByPepId.get(pepSc._1).isDefined){
-    	  tmpPepInstByPepId.get(pepSc._1).get.totalLeavesMatchCount = pepSc._2
-    	}else{
-    		throw new Exception("PeptideInstance associated to validated PeptideMatch not found for peptide id=" + pepSc._1 )
-    	}
     })
+  }
+
+  private def getRSMSpectralCountByPepId(rootRSM: ResultSummary, rsmID: Long, pepID: Seq[Long], execContext: IExecutionContext): HashMap[Long, Int] = {
+    val scByPepID = new HashMap[Long, Int]()
+
+    //***** RSM is leave : COUNT Valid PSM
+    if (getIsLeaveRSM(rsmID, execContext)) {
+      var rsm: ResultSummary = null
+      if (rootRSM.id == rsmID)
+       rsm = rootRSM
+      else {
+        val rsmOp = getResultSummaryProvider(execContext).getResultSummary(rsmID, true)
+        if (rsmOp.isEmpty)
+          throw new IllegalArgumentException("Unable to read resultaSummay with specified ID " + rsmID)
+        rsm = rsmOp.get
+      }
+
+      //VDS comment :Could get info from PeptideInstance.peptideMatches.filter on isValidated... but should be sure
+      // PeptideInstance.peptideMatches != null ...
+      val validPeptideMatchesB = Seq.newBuilder[PeptideMatch]
+      rsm.getAllPeptideMatchesByPeptideSetId.values.foreach(a => { validPeptideMatchesB ++= a })
+      val validPeptideMatches: Seq[PeptideMatch] = validPeptideMatchesB.result
+
+      // Peptide Instance SC = PepMatch Count
+      for (psm <- validPeptideMatches) {
+        if (pepID.contains(psm.peptideId)) {
+          var pepSC = scByPepID.getOrElse(psm.peptideId, 0)
+          pepSC += 1
+          scByPepID.put(psm.peptideId, pepSC)
+        }
+      }
+      return scByPepID
+
+    } else {
+
+      val childIDsOpt = getRSMChildsID(rsmID, execContext)
+
+      // ***** RSM is result of a Merge of ResultSet : count Leave RS "valid" PSM 
+      if (childIDsOpt.isEmpty) {
+
+        // Use RS hierarchy : If next level is merge RS, suppose all hierarchy is a RS merge          
+        //	Get leaves RS
+        val leavesRSs = getLeavesRS(rsmID, execContext)
+
+        //Get RSM root of RS Merge : May not be the "final" Root RSM !!!   if RSMs have then be merged ...
+        var rsm: ResultSummary = null
+        if (rootRSM.id == rsmID)
+          rootRSM
+        else {
+          val rsmOp = getResultSummaryProvider(execContext).getResultSummary(rsmID, true)
+          if (rsmOp.isEmpty)
+            throw new IllegalArgumentException("Unable to read resultaSummay with specified ID " + rsmID)
+          rsm = rsmOp.get
+        }
+
+        //Get filters used for RSM root of RS Merge
+        val appliedPSMFilters = ResultSummaryFilterBuilder.buildPeptideMatchFilters(rsm)
+
+        // Get PepID->SC for each RS
+        leavesRSs.foreach(rs => {
+          val rsPepInstSC: HashMap[Long, Int] = countValidPSMInResultSet(rs, appliedPSMFilters)
+          // Merge result in global result 
+          rsPepInstSC.foreach(entry => {
+            if (pepID.contains(entry._1)) {
+              var pepSC = scByPepID.getOrElse(entry._1, 0)
+              pepSC += entry._2
+              scByPepID.put(entry._1, pepSC)
+            }
+          })
+        })
+
+        // ***** RSM is result of a Merge of ResultSummary : continue to go into hierarchy : child RSM could be RS merge result  
+      } else {
+        //Use RSMs hierarchy.  rsm peptide Instance SC = sum child peptide Instance SC
+        childIDsOpt.get.foreach(childID => {
+          val childSCByPepID = getRSMSpectralCountByPepId(rootRSM, childID, pepID, execContext)
+          childSCByPepID.foreach(childSCEntry => {
+            var pepSC = scByPepID.getOrElse(childSCEntry._1, 0)
+            pepSC += childSCEntry._2
+            scByPepID.put(childSCEntry._1, pepSC)
+          })
+        })
+
+      }
+      return scByPepID
+    }
+
   }
 
   //A Optimiser: Prendre en entre la Map globale et l'incrementer...  
   private def countValidPSMInResultSet(rs: ResultSet, appliedPSMFilters: Array[IPeptideMatchFilter]): HashMap[Long, Int] = {
-    appliedPSMFilters.foreach( psmFilter =>{
-      var allPSMs = rs.peptideMatches.toSeq 
-      if(rs.decoyResultSet.isDefined){
+    appliedPSMFilters.foreach(psmFilter => {
+      var allPSMs = rs.peptideMatches.toSeq
+      if (rs.decoyResultSet.isDefined) {
         allPSMs ++= rs.decoyResultSet.get.peptideMatches
       }
       psmFilter.filterPeptideMatches(allPSMs, true, false)
     })
-    
-    val resultMap  =  new HashMap[Long, Int]()
-    rs.peptideMatches.filter(_.isValidated).foreach(psm =>{
-        var psmCount: Int = 0
-    	if(resultMap.contains(psm.peptideId)){
-    	  psmCount = resultMap.get(psm.peptideId).get
-    	}
-        psmCount += 1
-		resultMap.put(psm.peptideId, psmCount)
+
+    val resultMap = new HashMap[Long, Int]()
+    rs.peptideMatches.filter(_.isValidated).foreach(psm => {
+      var psmCount: Int = 0
+      if (resultMap.contains(psm.peptideId)) {
+        psmCount = resultMap.get(psm.peptideId).get
+      }
+      psmCount += 1
+      resultMap.put(psm.peptideId, psmCount)
     })
-    
-   resultMap
+
+    resultMap
   }
 
+  private def getResultSummaryProvider(execContext: IExecutionContext): IResultSummaryProvider = {
 
-  
-  private def getLeavesRS(rsm: ResultSummary, execContext: IExecutionContext): Seq[ResultSet] = {	  
+    new SQLResultSummaryProvider(msiDbCtx = execContext.getMSIDbConnectionContext,
+      psDbCtx = execContext.getPSDbConnectionContext,
+      udsDbCtx = execContext.getUDSDbConnectionContext)
+
+  }
+
+  private def getRSMChildsID(rsmID: Long, execContext: IExecutionContext): Option[Seq[Long]] = {
+
+    var childRSMIdsOp: Option[Seq[Long]] = null
+    val jdbcWork = new JDBCWork() {
+
+      override def execute(con: Connection) {
+        var childRSMIds = Seq.newBuilder[Long]
+        val stmt = con.prepareStatement("select child_result_summary_id from result_summary_relation where result_summary_relation.parent_result_summary_id = ?")
+        stmt.setLong(1, rsmID)
+        val sqlResultSet = stmt.executeQuery()
+        var childDefined = false
+        while (sqlResultSet.next) {
+          childDefined = true
+          childRSMIds += sqlResultSet.getLong(1)
+        }
+        if (!childDefined)
+          childRSMIdsOp = None
+        else
+          childRSMIdsOp = Some(childRSMIds.result)
+        stmt.close()
+      } // End of jdbcWork anonymous inner class
+    }
+
+    execContext.getMSIDbConnectionContext().doWork(jdbcWork, false)
+
+    return childRSMIdsOp
+  }
+
+  private def getLeavesRS(rsmID: Long, execContext: IExecutionContext): Seq[ResultSet] = {
     val providerContext = ProviderDecoratedExecutionContext(execContext) // Use Object factory
-      
-      
-    var leavesRsIds : Seq[Long] = getLeafChildsID(rsm.getResultSetId, providerContext)
-    var leavesRsBuilder  = Seq.newBuilder[ResultSet]
-   
-    val provider:IResultSetProvider=  providerContext.getProvider(classOf[IResultSetProvider])
-    
-    leavesRsIds.foreach(rsID =>{
+
+    val rsId = getRSIdForRsmID(rsmID, execContext)
+    var leavesRsIds: Seq[Long] = getLeafChildsID(rsId, providerContext)
+    var leavesRsBuilder = Seq.newBuilder[ResultSet]
+
+    val provider: IResultSetProvider = providerContext.getProvider(classOf[IResultSetProvider])
+
+    leavesRsIds.foreach(rsID => {
       val resultRS = provider.getResultSet(rsID)
-      if(resultRS.isDefined){
+      if (resultRS.isDefined) {
         leavesRsBuilder += resultRS.get
-      } else{
-        val msg = " !!! Unable to get leave search result with id "+rsID
+      } else {
+        val msg = " !!! Unable to get leave search result with id " + rsID
         logger.warn(msg)
         throw new Exception(msg)
-      }        
+      }
     })
-    
+
     leavesRsBuilder.result
-   
+
   }
 
-    
-  private def getLeafChildsID( rsId: Long, execContext: IExecutionContext) : Seq[Long] = {
+  private def getLeafChildsID(rsId: Long, execContext: IExecutionContext): Seq[Long] = {
     var allRSIds = Seq.newBuilder[Long]
-    
-	val jdbcWork = new JDBCWork() {
-    
-    	override def execute(con: Connection) {
-    	    
-    		val stmt = con.prepareStatement("select child_result_set_id from result_set_relation where result_set_relation.parent_result_set_id = :rsId")
-			stmt.setLong(1, rsId)
-			val sqlResultSet = stmt.executeQuery()
-			var childDefined = false 
-			while (sqlResultSet.next){
-			  childDefined = true
-			  allRSIds += sqlResultSet.getInt(1)
-			} 
-    		if(!childDefined)
-    		  allRSIds += rsId
-    		stmt.close()
-          } // End of jdbcWork anonymous inner class
+
+    val jdbcWork = new JDBCWork() {
+
+      override def execute(con: Connection) {
+
+        val stmt = con.prepareStatement("select child_result_set_id from result_set_relation where result_set_relation.parent_result_set_id = ?")
+        stmt.setLong(1, rsId)
+        val sqlResultSet = stmt.executeQuery()
+        var childDefined = false
+        while (sqlResultSet.next) {
+          childDefined = true
+          val nextChildId = sqlResultSet.getInt(1)
+          allRSIds ++= getLeafChildsID(nextChildId, execContext)
+        }
+        if (!childDefined)
+          allRSIds += rsId
+        stmt.close()
+      } // End of jdbcWork anonymous inner class
     }
     execContext.getMSIDbConnectionContext().doWork(jdbcWork, false)
-    
-    
+
     allRSIds.result
   }
 }
