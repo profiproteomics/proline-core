2de28ba09d93e1cbd30ce12f6481d00dcda4daef
Veronique Dupierris <veronique.dupierris@cea.fr>
2013-10-03 13:22:04 +0000

    simplied test. Specified value have been calculated using SQL Queries
    Former-commit-id: 4cd5a7bd9ce37116934c6b85a7ceed0cc518dc18

--- Proline-OMP/src/test/scala/fr/proline/core/algo/msi/TypicalProteinChooserTest.scala
+++ Proline-OMP/src/test/scala/fr/proline/core/algo/msi/TypicalProteinChooserTest.scala
@@ -25,9 +25,6 @@ class TypicalProteinChooserTest extends AbstractMultipleDBTestCase with Logging 
   val targetRSMId: Long = 2
 
   var executionContext: IExecutionContext = null
-  var rsmProvider: IResultSummaryProvider = null
-  var rsProvider: IResultSetProvider = null
-  protected var readRSM: ResultSummary = null
 
   @Before
   @throws(classOf[Exception])
@@ -44,22 +41,8 @@ class TypicalProteinChooserTest extends AbstractMultipleDBTestCase with Logging 
 
     logger.info("PDI, PS, MSI and UDS dbs succesfully initialized !")
 
-    val (execContext, rsmProv, rsProv) = buildSQLContext()
+    val execContext = buildJPAContext()
     executionContext = execContext
-    rsmProvider = rsmProv
-    rsProvider = rsProv
-    readRSM = this._loadRSM()
-    this._loadRSFor(readRSM)
-  }
-
-  private def _loadRSM(): ResultSummary = {
-    val rsm = rsmProvider.getResultSummary(targetRSMId,false).get
-	return rsm
-  }
-  
-  private def _loadRSFor(rsm: ResultSummary) = {
-    val rs = rsProvider.getResultSet(rsm.getResultSetId)
-    rsm.resultSet = rs	
   }
   
   @After
@@ -68,62 +51,26 @@ class TypicalProteinChooserTest extends AbstractMultipleDBTestCase with Logging 
     super.tearDown()
   }
 
-  def buildSQLContext() = {
-    val udsDbCtx = ContextFactory.buildDbConnectionContext(dsConnectorFactoryForTest.getUdsDbConnector, false).asInstanceOf[SQLConnectionContext]
-    val pdiDbCtx = ContextFactory.buildDbConnectionContext(dsConnectorFactoryForTest.getPdiDbConnector, true)
-    val psDbCtx = ContextFactory.buildDbConnectionContext(dsConnectorFactoryForTest.getPsDbConnector, false).asInstanceOf[SQLConnectionContext]
-    val msiDbCtx = ContextFactory.buildDbConnectionContext(dsConnectorFactoryForTest.getMsiDbConnector(1), false).asInstanceOf[SQLConnectionContext]
-    val executionContext = new BasicExecutionContext(udsDbCtx, pdiDbCtx, psDbCtx, msiDbCtx, null)
-
-    val rsmProvider = new SQLResultSummaryProvider(msiDbCtx, psDbCtx, udsDbCtx)
-    val rsProvider = new SQLResultSetProvider(msiDbCtx, psDbCtx, udsDbCtx)
-
-    (executionContext, rsmProvider, rsProvider)
+  
+  def buildJPAContext() = {
+    val executionContext = ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true) // Full JPA
+    executionContext
   }
-
   
   @Test
   def testChangeTypicalProt() = {
-	
-	val pmByIds = readRSM.resultSet.get.proteinMatchById
-	
-	//Load proteinMatches in all ProteinSet !!	
-	readRSM.proteinSets.foreach(protSet=> {
-	   var protSetProtMatches = Array.newBuilder[ProteinMatch]
-	   protSet.getProteinMatchIds.foreach(id =>{
-          protSetProtMatches += pmByIds(id) 
-	   })
-	   protSet.proteinMatches = Some(protSetProtMatches.result)
-	   protSet.setTypicalProteinMatch(pmByIds(protSet.getTypicalProteinMatchId))
-	})
 
 	//Verify which proteinSets should be modified by algo
-	var nbrTremblTPM:Int = 0    // Count proteinSet with Trembl typical protein match
-	var nbrTremblShouldChange : Int = 0 // Count proteinSet with Trembl typical protein match but which have "sp" proteinmatch in their sameset 
-	
-    readRSM.proteinSets.foreach(protSet=> {
-      
-      val sameSetPMIds =  protSet.peptideSet.proteinMatchIds //Change only with samese proteinMatch
-
-      if(pmByIds(protSet.getTypicalProteinMatchId).description.startsWith("tr")) {
-        nbrTremblTPM += 1
-       
-        if(protSet.proteinMatches.get.filter(pm=>{ (pm.description.startsWith("sp") && sameSetPMIds.contains(pm.id) )}).length>0) 
-          nbrTremblShouldChange += 1
-      }
-    })    
+	var nbrTremblTPM:Int = 964    // # proteinSet with Trembl typical protein match
+	var nbrTremblShouldChange : Int = 951 // # proteinSet with Trembl typical protein match but which have "sp" proteinmatch in their sameset 
 	
     val typicalChooser = new TypicalProteinChooser()
 	val ruleDesc = new TypicalProteinChooserRule(ruleName="Sp chooser", applyToAcc=false,rulePattern="^sp.*" )
-	typicalChooser.changeTypical(readRSM,ruleDesc,false)
-	
-	var newNbrTremblTPM:Int = 0    
-	readRSM.proteinSets.foreach(protSet=> {
-      if(protSet.getTypicalProteinMatch.get.description.startsWith("tr")) {	   
-        newNbrTremblTPM += 1        
-      }
-    })
-    Assert.assertEquals(nbrTremblTPM-nbrTremblShouldChange, newNbrTremblTPM)
+  	typicalChooser.changeTypical(targetRSMId,ruleDesc,executionContext.getMSIDbConnectionContext().getEntityManager())
+  	
+  	val nbrChangedTyp = typicalChooser.getChangedProteinSets.size
+
+    Assert.assertEquals(nbrTremblShouldChange, nbrChangedTyp)
 	
   }
   
