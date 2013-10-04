f8d9231dbf5da96f21813be166558cfa0fd40587
David Bouyssié <David.Bouyssie@ipbs.fr>
2013-10-04 13:05:37 +0000

    Update the PtmDefinition OM and format the class file
    Former-commit-id: e00ddffb35710bccd6a1d3c04e2a45f3724f13d3

--- Proline-OM/src/main/scala/fr/proline/core/om/model/msi/PtmClasses.scala
+++ Proline-OM/src/main/scala/fr/proline/core/om/model/msi/PtmClasses.scala
@@ -1,40 +1,42 @@
 package fr.proline.core.om.model.msi
 
-import org.apache.commons.lang3.StringUtils
 import com.codahale.jerkson.JsonSnakeCase
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 import fr.proline.util.misc.InMemoryIdGen
+import fr.proline.util.StringUtils
 
 object PtmNames extends InMemoryIdGen
 
 @JsonSnakeCase
-@JsonInclude( Include.NON_NULL )
-case class PtmNames( val shortName: String, val fullName: String ) {
-  
+@JsonInclude(Include.NON_NULL)
+case class PtmNames(val shortName: String, val fullName: String) {
+
   // Requirements
-  require( StringUtils.isNotEmpty( shortName ) )
-  
+  require(StringUtils.isNotEmpty(shortName))
+
 }
 
 @JsonSnakeCase
-@JsonInclude( Include.NON_NULL )
-case class UnimodEntry( // Required fields
-                   override val shortName: String, 
-                   override val fullName: String, 
-                   val specificities: Array[Any],
-                   
-                   // Immutable optional fields
-                   val unimodId: Long = 0, 
-                   val ptmEvidences: Array[PtmEvidence] = null
-                   )
-  extends PtmNames( shortName, fullName ) {
+@JsonInclude(Include.NON_NULL)
+case class UnimodEntry(
+  // Required fields
+  override val shortName: String,
+  override val fullName: String,
+  val specificities: Array[Any],
+
+  // Immutable optional fields
+  val unimodId: Long = 0,
+  val ptmEvidences: Array[PtmEvidence] = null
   
+) extends PtmNames(shortName, fullName) {
+
   // Requirements
-  require( specificities != null  )
-  
+  require(specificities != null)
+
 }
 
+// TODO: move Java enumerations like fr.proline.core.orm.ps.PtmEvidence.Type into Java-Commons-API
 object IonTypes extends Enumeration {
   type IonType = Value
   val Precursor = Value("Precursor")
@@ -44,96 +46,118 @@ object IonTypes extends Enumeration {
 }
 
 @JsonSnakeCase
-@JsonInclude( Include.NON_NULL )
-case class PtmEvidence( // Required fields
-                   val ionType: IonTypes.IonType,
-                   val composition: String,
-                   val monoMass: Double,
-                   val averageMass: Double,
-                   
-                   // Immutable optional fields
-                   val isRequired: Boolean = false
-                   ) {
-  // Requirements
-  require( ionType != null &&  composition != null )
-  
-  def ionType_ ( newIonType: IonTypes.IonType ) = { newIonType }
+@JsonInclude(Include.NON_NULL)
+case class PtmEvidence(
     
+  // Required fields
+  val ionType: IonTypes.IonType,
+  var composition: String,
+  val monoMass: Double,
+  val averageMass: Double,
+
+  // Immutable optional fields
+  val isRequired: Boolean = false
+) {
+  // Requirements
+  require(ionType != null && composition != null)
+
+  def ionType_(newIonType: IonTypes.IonType) = { newIonType }
+
 }
 
- 
 object PtmLocation extends Enumeration {
-	  type Location = Value
-	  val PROT_N_TERM = Value("Protein N-term")
-	  val PROT_C_TERM  = Value("Protein C-term")
-	  val N_TERM   = Value("N-term")
-	  val C_TERM   = Value("C-term")
-	  val ANY_N_TERM   = Value("Any N-term")
-	  val ANY_C_TERM   = Value("Any C-term")
-	  val ANYWHERE= Value("Anywhere")       
-} 
+  type Location = Value
+  val PROT_N_TERM = Value("Protein N-term")
+  val PROT_C_TERM = Value("Protein C-term")
+  val N_TERM = Value("N-term")
+  val C_TERM = Value("C-term")
+  val ANY_N_TERM = Value("Any N-term")
+  val ANY_C_TERM = Value("Any C-term")
+  val ANYWHERE = Value("Anywhere")
+}
+
+trait IPtmSpecificity {
+  def id: Long
+  val location: String
+  val residue: Char
+  val classification: String  
+  val ptmId: Long
+}
 
 @JsonSnakeCase
-@JsonInclude( Include.NON_NULL )
-case class PtmSpecificity( // Required fields
-                      val location: String, 
+@JsonInclude(Include.NON_NULL)
+case class PtmSpecificity(
     
-                      // Immutable optional fields
-                      val residue: Char = '\0',
-                      val classification: String = null,
-                      val id: Long = 0,
-                      val ptmId : Long = 0 ) {
-  
+  // Required fields
+  val location: String,
+
+  // Immutable optional fields
+  val residue: Char = '\0',
+  val classification: String = null,
+  val id: Long = 0,
+  val ptmId: Long = 0
+) extends IPtmSpecificity {
+
   // Requirements
-  require( StringUtils.isNotEmpty( location ) )  
-  
+  require(StringUtils.isNotEmpty(location))
+
 }
 
 object PtmDefinition extends InMemoryIdGen
 
 @JsonSnakeCase
-@JsonInclude( Include.NON_NULL )
-case class PtmDefinition( // Required fields
-                     override val id: Long,
-                     override val location: String,
-                     val names: PtmNames,
-                     val ptmEvidences: Array[PtmEvidence],
-                     
-                     // Immutable optional fields
-                     override val residue: Char = '\0',
-                     override val classification: String = null,
-                     override val ptmId: Long = 0
-                     )
-  extends PtmSpecificity( location, residue, classification, id, ptmId ) {
+@JsonInclude(Include.NON_NULL)
+case class PtmDefinition(
   
+  // Required fields
+  var id: Long,
+  val location: String,
+  val names: PtmNames,
+  val ptmEvidences: Array[PtmEvidence], // TODO: remove and replace by corresponding lazy attributes ???
+
+  // Immutable optional fields
+  val residue: Char = '\0',
+  val classification: String = null,
+  val ptmId: Long = 0,
+  val unimodId: Int = 0
+  
+) extends IPtmSpecificity {
+
   // Requirements
-  require(  names != null && ptmEvidences != null )
-  
+  require(names != null && ptmEvidences != null, "names and PTM evidences must not be null")
+
   // Lazy values
-  lazy val precursorDelta : PtmEvidence = {    
-    ptmEvidences.find( { _.ionType == IonTypes.Precursor } ).get;
+  lazy val precursorDelta: PtmEvidence = {
+    ptmEvidences.find( _.ionType == IonTypes.Precursor ).get
   }
+  
+  @transient lazy val neutralLosses = ptmEvidences.find( _.ionType == IonTypes.NeutralLoss )
+  @transient lazy val pepNeutralLosses = ptmEvidences.find( ev => ev.ionType == IonTypes.PepNeutralLoss )
+  @transient lazy val artefacts = ptmEvidences.find( ev => ev.ionType == IonTypes.Artefact )
+  
+  def isCompositionDefined = StringUtils.isNotEmpty(precursorDelta.composition)
   
 }
 
 @JsonSnakeCase
-@JsonInclude( Include.NON_NULL )
-case class LocatedPtm( // Required fields
-                  val definition: PtmDefinition, 
-                  val seqPosition: Int, 
-                  val monoMass: Double,
-                  val averageMass: Double,
-                  val composition : String,
-                  
-                  // Immutable optional fields
-                  val isNTerm : Boolean = false, 
-                  val isCTerm : Boolean = false
-                  ) {
-  
+@JsonInclude(Include.NON_NULL)
+case class LocatedPtm(
+  // Required fields
+  val definition: PtmDefinition,
+  val seqPosition: Int,
+  val monoMass: Double,
+  val averageMass: Double,
+  val composition: String,
+
+  // Immutable optional fields
+  val isNTerm: Boolean = false,
+  val isCTerm: Boolean = false
+) {
+
   // Requirements
-  require( definition != null && seqPosition >= -1 && monoMass > 0 && averageMass > 0 && StringUtils.isNotEmpty( composition ) )
-  if( isNTerm ) require( seqPosition == 0 )
-  if( isCTerm ) require( seqPosition == -1 )
+  require(definition != null && seqPosition >= -1 && monoMass > 0 && averageMass > 0 && StringUtils.isNotEmpty(composition))
+  if (isNTerm) require(seqPosition == 0)
+  if (isCTerm) require(seqPosition == -1)
 
 }
 
