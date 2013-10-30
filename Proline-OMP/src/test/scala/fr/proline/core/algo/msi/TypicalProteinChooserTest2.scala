d307f7f6dcd87fc8703d85bfc857824c4486f784
David Bouyssié <David.Bouyssie@ipbs.fr>
2013-10-30 15:43:19 +0000

    Add JUnit test which uses the newly submitted Merged RSMs dataset
    Former-commit-id: baedef09c3ba58fd98fca7678ab9ed949cbd4b04

---
+++ Proline-OMP/src/test/scala/fr/proline/core/algo/msi/TypicalProteinChooserTest2.scala
@@ -0,0 +1,76 @@
+package fr.proline.core.algo.msi
+
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import com.weiglewilczek.slf4s.Logging
+import fr.proline.context.BasicExecutionContext
+import fr.proline.context.IExecutionContext
+import fr.proline.core.dal.ContextFactory
+import fr.proline.core.dal.SQLConnectionContext
+import fr.proline.core.om.model.msi.ProteinMatch
+import fr.proline.core.om.model.msi.ResultSummary
+import fr.proline.core.om.provider.msi.IResultSummaryProvider
+import fr.proline.core.om.provider.msi.impl.SQLResultSetProvider
+import fr.proline.core.om.provider.msi.impl.SQLResultSummaryProvider
+import fr.proline.core.om.utils.AbstractMultipleDBTestCase
+import fr.proline.repository.DriverType
+import fr.proline.core.om.provider.msi.IResultSetProvider
+
+class TypicalProteinChooserTest2 extends AbstractMultipleDBTestCase with Logging {
+
+  // Define some vars
+  val driverType = DriverType.H2
+  val fileName = "STR_F063442_F122817_MergedRSMs"
+  val targetRSMId: Long = 33
+
+  var executionContext: IExecutionContext = null
+
+  @Before
+  @throws(classOf[Exception])
+  def setUp() = {
+
+    logger.info("Initializing DBs")
+    super.initDBsDBManagement(driverType)
+
+    //Load Data
+    pdiDBTestCase.loadDataSet("/dbunit/datasets/pdi/Proteins_Dataset.xml")
+    psDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/ps-db.xml")
+    msiDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/msi-db.xml")
+    udsDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/uds-db.xml")
+
+    logger.info("PDI, PS, MSI and UDS dbs succesfully initialized !")
+
+    val execContext = buildJPAContext()
+    executionContext = execContext
+  }
+
+  @After
+  override def tearDown() {
+    if (executionContext != null) executionContext.closeAll()
+    super.tearDown()
+  }
+
+  def buildJPAContext() = {
+    val executionContext = ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true) // Full JPA
+    executionContext
+  }
+
+  @Test
+  def testChangeTypicalProt() = {
+
+    // Check which proteinSets should be modified by algo
+    var nbrTremblShouldChange: Int = 4 // # proteinSet with an accession number containing 6 letters (AC instead of ID) 
+
+    val typicalChooser = new TypicalProteinChooser()
+    val ruleDesc = new TypicalProteinChooserRule(ruleName = "Sprot AC preferred", applyToAcc = true, rulePattern = "\\w{6,6}")
+    typicalChooser.changeTypical(targetRSMId, ruleDesc, executionContext.getMSIDbConnectionContext().getEntityManager())
+
+    val nbrChangedTyp = typicalChooser.getChangedProteinSets.size
+
+    Assert.assertEquals(nbrTremblShouldChange, nbrChangedTyp)
+
+  }
+
+}
