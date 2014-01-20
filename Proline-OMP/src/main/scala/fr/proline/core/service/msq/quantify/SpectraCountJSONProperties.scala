09785b218aa902f806af33e8fa3b60eff47ce6c8
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-01-20 15:43:37 +0000

    #9085 Regroupe plusieurs classe utilitaire pour le SC
    Former-commit-id: 9b34ba77cd6ffcaf0b529cbe8b1baa3bc64c76c0

---
+++ Proline-OMP/src/main/scala/fr/proline/core/service/msq/quantify/SpectraCountJSONProperties.scala
@@ -0,0 +1,40 @@
+package fr.proline.core.service.msq.quantify
+
+import fr.proline.core.om.model.msi.ProteinSet
+
+
+/*** 
+ * List properties String used in JSON SC result. The output result should be formatted as 
+ *     
+*	"{"SpectralCountResult":{[
+*	{"rsm_id":<id>,"proteins_spectral_counts":[{"protein_accession"=<protAcc>,"bsc"=<valBSC>,"ssc"=<valSSC>,"wsc"=<valWSC>}]},
+*	{"rsm_id":<id>,"proteins_spectral_counts":[{"protein_accession"=<protAcc>,"bsc"=<valBSC>,"ssc"=<valSSC>,"wsc"=<valWSC>}]}
+*	 ]}}"
+ */
+object SpectralCountsJSONProperties {
+  
+    final val rootPropName : String = "\"SpectralCountResult\""
+    final val rsmIDPropName : String = "\"rsm_id\""
+    final val protSCsListPropName : String = "\"proteins_spectral_counts\""
+    final val protACPropName : String = "\"protein_accession\""
+    final val bscPropName : String = "\"bsc\""
+    final val sscPropName : String = "\"ssc\""
+    final val wscPropName : String = "\"wsc\""
+           
+}
+
+/**
+ * Case clas to group the 3 different SC 
+ */
+case class SpectralCountsStruct( val basicSC : Float, val specificSC : Float, val weightedSC : Float)
+
+
+/**
+ * ProteinSet description for spectralCount calculation
+ */
+case class ProteinSetPeptidesDescription(
+			val proteinSet: ProteinSet, 
+			val typicalPMAcc: String, 
+			val nbrPepSpecific: Int, 
+			val weightByPeptideId: scala.collection.mutable.Map[Long, Float] = null)
+			
