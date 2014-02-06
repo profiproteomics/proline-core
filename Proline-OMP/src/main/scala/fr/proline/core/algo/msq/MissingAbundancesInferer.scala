594e42f068019fdae5bbcc194f8809caffceb9a6
Marco Dubois <cram@hotmail.fr>
2014-02-06 12:51:25 +0000

    -fix MissingAbundancesInferer.scala
    Former-commit-id: a9887230f571f919042c7a97fa01b8e0d3f3b457

--- Proline-OMP/src/main/scala/fr/proline/core/algo/msq/MissingAbundancesInferer.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msq/MissingAbundancesInferer.scala
@@ -2,27 +2,31 @@ package fr.proline.core.algo.msq
 
 import org.apache.commons.math.stat.StatUtils
 import org.apache.commons.math.stat.descriptive.rank.Percentile
+import fr.proline.util.primitives.isZeroOrNaN
 import fr.proline.util.random.randomGaussian
 
 object MissingAbundancesInferer {
   
   val percentileComputer = new Percentile()
-
-  protected def isZeroOrNaN( value: Float ) = value.isNaN || value == 0f
-  protected def isZeroOrNaN( value: Double ) = value.isNaN || value == 0.0
   
   def inferAbundances( abundanceMatrix: Array[Array[Float]], errorModel: AbsoluteErrorModel ): Array[Array[Float]] = {
+    require( abundanceMatrix.length > 10, "at least 10 abundance rows are required for missing abundance inference")
+    require( abundanceMatrix.flatten.count( isZeroOrNaN(_) == false ) > 0 ) // only for debug
     
     // Retrieve quartiles from flattened abundance matrix
-    val allDefinedAbundances = abundanceMatrix.flatten.filter( isZeroOrNaN(_) == false ).map(_.toDouble).sorted
+    val allDefinedAbundances = abundanceMatrix.flatten.withFilter( isZeroOrNaN(_) == false ).map(_.toDouble).sorted
+    //println( s"allDefinedAbundances.length: ${allDefinedAbundances.length}")
+    
     val q1 = percentileComputer.evaluate(allDefinedAbundances,25).toFloat
     val q3 = percentileComputer.evaluate(allDefinedAbundances,75).toFloat
+   // println( s"quartiles: ${q1} , ${q3}")
     
     // Convert quartiles into theoretical maximal bounds
     var(lb,ub) = ErrorModelComputer.quartilesToBounds(q1,q3)
+    //println( s"bounds: ${lb} , ${ub}")
     
     // Re-Compute Lower Bound using the first percentile if it is lower than the lowest observed abundance
-    if( lb < allDefinedAbundances(0) ) lb = {
+    if( lb < allDefinedAbundances.head ) lb = {
       val firstPercentileIdx = 1 + (allDefinedAbundances.length / 100).toInt
       allDefinedAbundances.take( firstPercentileIdx ).sum.toFloat / firstPercentileIdx
     }
