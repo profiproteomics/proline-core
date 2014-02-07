6cb767d065238a7c4ab92927dd6125c30f7869e0
Marco Dubois <cram@hotmail.fr>
2014-02-07 09:07:58 +0000

    -modify test case to fit the new specifications
    Former-commit-id: c6458f025950ed897aeb1ddf3e37ea09bcafa2da

--- Proline-OMP/src/test/scala/fr/proline/core/algo/msq/AbundanceNormalizerTest.scala
+++ Proline-OMP/src/test/scala/fr/proline/core/algo/msq/AbundanceNormalizerTest.scala
@@ -17,13 +17,14 @@ class AbundanceNormalizerTest {
       Array(1f,0,0.8f)
     )
     
+    println( AbundanceNormalizer.calcNormalizationFactors(abundanceMatrix).toList )
     val normalizedMatrix = AbundanceNormalizer.normalizeAbundances(abundanceMatrix)
     
     //import scala.runtime.ScalaRunTime.stringOf
     //println( stringOf(normalizedMatrix) )
-    
+  
     assertEquals( abundanceMatrix.length, normalizedMatrix.length )
-    assertArrayEquals( Array(1f,1f,Float.NaN), normalizedMatrix(3), 0.01f)
+    assertArrayEquals( Array(1f,0.9791666269302368f,Float.NaN), normalizedMatrix(3), 0.01f)
     assertArrayEquals( Array(1f,0f,1f), normalizedMatrix(4), 0.01f)
     
   }
