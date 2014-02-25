f74249053ae079c5fda9483d07b0cb92a76c7740
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-02-25 10:21:26 +0000

    Remove unused code
    Former-commit-id: 0d31413c1e9ecdaf0f3209781dc3e97d67229ebb

--- Proline-OMP/src/main/scala/fr/proline/core/algo/msi/scoring/ScoringClasses.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msi/scoring/ScoringClasses.scala
@@ -4,7 +4,7 @@ import fr.proline.core.om.model.msi.ResultSummary
 
 object PepSetScoring extends Enumeration {  
   val MASCOT_STANDARD_SCORE = Value("mascot:standard score")
-  val MASCOT_MUDPIT_SCORE = Value("mascot:mudpit score")
+//  val MASCOT_MUDPIT_SCORE = Value("mascot:mudpit score")
   val MASCOT_MODIFIED_MUDPIT_SCORE = Value("mascot:modified mudpit score")
 }
 
@@ -19,7 +19,6 @@ object PeptideSetScoreUpdater {
   def apply( methodName: PepSetScoring.Value ): IPeptideSetScoreUpdater = {
     methodName match {
       case PepSetScoring.MASCOT_MODIFIED_MUDPIT_SCORE => new MascotModifiedMudpitScoreUpdater()
-      case PepSetScoring.MASCOT_MUDPIT_SCORE => new MascotMudpitScoreUpdater()
       case PepSetScoring.MASCOT_STANDARD_SCORE => new MascotStandardScoreUpdater()          
     }
   }
