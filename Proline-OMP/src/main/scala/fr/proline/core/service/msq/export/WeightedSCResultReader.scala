00ee4a544d7da1fc3d25f13781e0d64d27b707fb
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-03-07 13:32:05 +0000

    #9617 : Ajout d'un service de relecture de résultat de SC
    Former-commit-id: a84c03b307a63bfbd48614e37f633be16d500843

---
+++ Proline-OMP/src/main/scala/fr/proline/core/service/msq/export/WeightedSCResultReader.scala
@@ -0,0 +1,228 @@
+package fr.proline.core.service.msq.export
+
+import fr.proline.api.service.IService
+import com.typesafe.scalalogging.slf4j.Logging
+import fr.proline.context.IExecutionContext
+import fr.proline.core.orm.uds.Dataset
+import fr.proline.core.service.msq.quantify.SpectralCountsJSONProperties
+import fr.proline.core.om.provider.msq.impl.SQLQuantResultSummaryProvider
+import fr.proline.repository.util.JDBCWork
+import java.sql.Connection
+import scala.collection.mutable.ArrayBuffer
+import fr.proline.core.om.model.msq.QuantResultSummary
+import fr.proline.core.om.model.msq.QuantProteinSet
+import fr.proline.core.om.model.msq.QuantProteinSet
+import scala.collection.mutable.HashMap
+
+class WeightedSCResultReader (
+    execCtx: IExecutionContext, 
+    datasetId : Long ) extends IService with Logging {
+
+  	/**
+ 	* "{"spectral_count_result":{[ 
+  	* { "rsm_id":Long, "proteins_spectral_counts":[ { "protein_accession"=Acc, "prot_match_id"=Long, "prot_set_id"=Long, "prot_status"=String,"pep_nbr"=Int,"bsc"=Float,"ssc"=Float,"wsc"=Float}, {...} ] },
+  	*	{ "rsm_id"... } 
+  	* ]}}"    
+  	*/
+
+  private var _resultAsJSON: String = null
+  def getResultAsJSON() = { _resultAsJSON }
+  
+  private var _refRSMId : Long = -1L
+  def getIdfRSMReferenceId() = { _refRSMId }
+  
+  def runService(): Boolean = {
+    logger.info("Retrive SpectralCount Result for DataSet "+datasetId)
+    
+    // Read Quantitation information from UDS
+    var mqcSerializedProp : String  = null
+    var mqchQuantRSMID : java.lang.Long = null
+    var quantChIdsList = new ArrayBuffer[Long]()
+    var rsmIdByQChId = Map.newBuilder[Long,Long]
+    
+    val jdbcWork = new JDBCWork() {
+      override def execute(con: Connection) {
+        val getMQChQuery = "SELECT id, quant_result_summary_id, serialized_properties from master_quant_channel WHERE quantitation_id = ?"
+        val pStmt = con.prepareStatement(getMQChQuery)
+        pStmt.setLong(1, datasetId)
+        val sqlResultSet = pStmt.executeQuery()
+        if (sqlResultSet.next){ //Should be One ! 
+          val mqchId = sqlResultSet.getLong("id")
+          mqcSerializedProp = sqlResultSet.getString("serialized_properties")
+          mqchQuantRSMID = sqlResultSet.getLong("quant_result_summary_id")
+          
+          
+          val getQChListQuery = "SELECT id, ident_result_summary_id from quant_channel WHERE quantitation_id = ? AND master_quant_channel_id = ?"
+          val pStmt2 = con.prepareStatement(getQChListQuery)
+          pStmt2.setLong(1, datasetId)
+          pStmt2.setLong(2, mqchId)  
+          val sqlResultSet2 = pStmt2.executeQuery()
+       	  while (sqlResultSet2.next){
+       	    val qChId = sqlResultSet2.getLong("id")
+       	    val idfRsmOd = sqlResultSet2.getLong("ident_result_summary_id")
+       	    quantChIdsList += qChId
+       	    rsmIdByQChId += qChId -> idfRsmOd
+       	  }
+        }
+        pStmt.close()               
+        
+      }      
+    } // End of jdbcWork anonymous inner class    
+    execCtx.getUDSDbConnectionContext().doWork(jdbcWork, false)
+          
+    require(mqchQuantRSMID != null && mqchQuantRSMID!=0, "undefined quantitation result summary for dataset id=" + datasetId)
+
+    // Read reference RSM idf ID from master quant channel    
+    try {
+	   
+	    if(mqcSerializedProp != null){
+	     
+	      val indexStart =  mqcSerializedProp.indexOf(SpectralCountsJSONProperties.refIdfRsmID)
+	      val indexEnd = if( mqcSerializedProp.indexOf(",", indexStart)>=0 )mqcSerializedProp.indexOf(",", indexStart) else mqcSerializedProp.indexOf("}", indexStart)
+	      
+	      _refRSMId = mqcSerializedProp.substring(indexStart+SpectralCountsJSONProperties.refIdfRsmID.length()+1,indexEnd).trim().toLong
+	    }
+    } catch {
+    	case e: Exception=>  _refRSMId = -1l
+    }
+    
+    
+    //Read quantitation RSM and associated QuantData 
+    val quantRsmProvider = new SQLQuantResultSummaryProvider(
+      execCtx.getMSIDbConnectionContext,
+      execCtx.getPSDbConnectionContext,
+      execCtx.getUDSDbConnectionContext
+    )
+    
+    val quantRSMOpt = quantRsmProvider.getQuantResultSummary(mqchQuantRSMID, quantChIdsList.toArray, true).get
+    _resultAsJSON = createJSonOutputResult(quantRSMOpt, rsmIdByQChId.result)
+    true
+  }
+
+   private def createJSonOutputResult(msiQuantRSM : QuantResultSummary, rsmIdByQChId : Map[Long,Long]): String = {
+          
+     val jsonBuilder: StringBuilder = new StringBuilder(" \"{")
+	 jsonBuilder.append(SpectralCountsJSONProperties.rootPropName).append(":{[")
+	 
+	  var firstQChOcc = true
+	  	 
+	 msiQuantRSM.quantChannelIds.foreach(qtChId =>{ // Go Through each QuantChannel (RSM)
+	   
+		 val rsmId = rsmIdByQChId(qtChId)
+		 if (!firstQChOcc) { jsonBuilder.append(",") } else { firstQChOcc = false }
+		 jsonBuilder.append("{").append(SpectralCountsJSONProperties.rsmIDPropName).append(":").append(rsmId).append(",") //save current RSM Id
+      
+		 // -- Save prots SC for current RSM          	
+		 var firstPACOcc = true
+		 jsonBuilder.append(SpectralCountsJSONProperties.protSCsListPropName).append(":[")
+
+		 msiQuantRSM.masterQuantProteinSets.foreach(mqProtSet => {
+		   //Go through All ProteinSets and extract only data for current QuantChanel
+		   
+		   //Get Typical Prot Accession
+		   var typicalAcc : String = null
+		   if( mqProtSet.proteinSet.getTypicalProteinMatch != null && mqProtSet.proteinSet.getTypicalProteinMatch.isDefined) 
+			   typicalAcc = mqProtSet.proteinSet.getTypicalProteinMatch.get.accession		         
+		   if(typicalAcc == null){
+			   // Read it from DBs
+			   val accJdbcWork = new JDBCWork() {
+				   override def execute(con: Connection) {
+					   val getTypAcc = "SELECT accession from protein_match WHERE id = ?"
+					   val pStmt = con.prepareStatement(getTypAcc)	
+					   pStmt.setLong(1, mqProtSet.proteinSet.getTypicalProteinMatchId)
+					   val sqlResultSet = pStmt.executeQuery()
+					   if (sqlResultSet.next){ //Should be One ! 
+						   typicalAcc =  sqlResultSet.getString("accession")
+					   }    		  
+					   pStmt.close()
+				   }            	         
+			   } // End of jdbcWork anonymous inner class    
+			   execCtx.getMSIDbConnectionContext().doWork(accJdbcWork, false)
+		   }
+		   
+		 //Get QuantProteinSet for current ProteinSet in current QuantChannel
+		 val protQuant = mqProtSet.quantProteinSetMap.get(qtChId)
+		 if (protQuant.isDefined) { 
+			 // This prot is quantified un current QuantChannel
+			 if (!firstPACOcc) { 
+				 jsonBuilder.append(",") 
+			 } else {
+				 firstPACOcc = false 
+			 }
+          
+			 //Properties to ge
+			 var protSetId : Long = -1
+			 var protMatchId: Long = -1
+			 var protMatchStatus : String = null
+			 var protMatchPepNbr = -1
+			 if (protQuant.get.proteinSetId.isDefined) {
+				 protSetId=protQuant.get.proteinSetId.get
+				 protMatchId = protQuant.get.proteinMatchId.getOrElse(-1)       
+				 
+				//Read Prot Status and Nbr Peptides from DBs
+			   val protStatJdbcWork = new JDBCWork() {
+				   override def execute(con: Connection) {
+				      //---- Read Prot Status
+					   val getProtStatus = "SELECT is_in_subset from protein_set_protein_match_item	 WHERE protein_match_id = ? AND protein_set_id = ?"
+					   val pStmt = con.prepareStatement(getProtStatus)	
+					   pStmt.setLong(1, protMatchId)
+					   pStmt.setLong(2, protSetId)
+					   val sqlResultSet = pStmt.executeQuery()
+					   if (sqlResultSet.next){ //Should be One ! 
+						   val isInSubset =  sqlResultSet.getBoolean("is_in_subset")
+						   if(isInSubset) {
+						     protMatchStatus = "Subset"
+						   } else {
+							   // Verify if SameSet or Typical 
+							   val getProtTypical = "SELECT typical_protein_match_id from protein_set WHERE id = ?"
+							   val pStmt2 = con.prepareStatement(getProtTypical)	
+							   pStmt2.setLong(1, protSetId)					   
+							   val sqlResultSet2 = pStmt2.executeQuery()
+							   if (sqlResultSet2.next){ 
+							       val protSetTypID = sqlResultSet2.getLong("typical_protein_match_id")
+							       if(protSetTypID.equals(protMatchId) )
+							    	   protMatchStatus = "Typical"
+						    	   else
+						    	     protMatchStatus = "Sameset"
+							   }
+							   pStmt2.close()
+						   }
+					   } else //NO LINK Between ProtSet and ProtMatch : Error !
+						   protMatchStatus ="NOT FOUND !"+protSetId						
+					   pStmt.close()		
+					   
+					      //---- Read Prot Status
+					   val getPepCount = "SELECT peptide_count from peptide_set_protein_match_map pspmm, peptide_set "+
+					   "WHERE pspmm.protein_match_id = ? and pspmm.result_summary_id = ?  and peptide_set.id = pspmm.peptide_set_id"
+					   val pStmt2 = con.prepareStatement(getPepCount)
+					   pStmt2.setLong(1, protMatchId)	
+					   pStmt2.setLong(2, rsmId)					   
+					   val sqlResultSet2 = pStmt2.executeQuery()
+					   if (sqlResultSet2.next){ 
+					     protMatchPepNbr = sqlResultSet2.getInt("peptide_count")
+					   }
+				   }            	         
+				 } // End of jdbcWork anonymous inner class    
+				 execCtx.getMSIDbConnectionContext().doWork(protStatJdbcWork, false)
+
+			 } // End protQuant.get.proteinSetId.isDefined
+			 jsonBuilder.append("{").append(SpectralCountsJSONProperties.protACPropName).append("=").append(typicalAcc).append(",")
+			 jsonBuilder.append(SpectralCountsJSONProperties.protMatchId).append("=").append(protMatchId).append(",")
+			 jsonBuilder.append(SpectralCountsJSONProperties.protSetId).append("=").append(protSetId).append(",")
+			 jsonBuilder.append(SpectralCountsJSONProperties.protMatchStatus).append("=").append(protMatchStatus).append(",")
+			 jsonBuilder.append(SpectralCountsJSONProperties.pepNbr).append("=").append(protMatchPepNbr).append(",")          
+			 jsonBuilder.append(SpectralCountsJSONProperties.bscPropName).append("=").append(protQuant.get.peptideMatchesCount).append(",")
+			 jsonBuilder.append(SpectralCountsJSONProperties.sscPropName).append("=").append(protQuant.get.rawAbundance).append(",")
+			 jsonBuilder.append(SpectralCountsJSONProperties.wscPropName).append("=").append(protQuant.get.abundance).append("}")
+		 } //End if Prot quantified in QCh
+		 }) //End Go through All ProteinSets
+		 
+		 jsonBuilder.append("]") //End protAC list for current RSM
+		 jsonBuilder.append("}") //End current RSM properties
+		 
+	 }) //End GoThrough RSMs/QChs
+	 jsonBuilder.append("]}}\"") //End SpectralCountResult array properties
+	 jsonBuilder.result
+
+   }
+}
---
+++ Proline-OMP/src/test/scala/fr/proline/core/service/msq/WeightedSCReaderTest.scala
@@ -0,0 +1,173 @@
+package fr.proline.core.service.msq
+
+import java.util.ArrayList
+import org.junit.After
+import org.junit.Assert._
+import org.junit.Before
+import org.junit.Test
+import com.typesafe.scalalogging.slf4j.Logging
+import fr.proline.context.IExecutionContext
+import fr.proline.core.algo.msq.SpectralCountConfig
+import fr.proline.core.dal.ContextFactory
+import fr.proline.core.om.provider.msi.IResultSetProvider
+import fr.proline.core.om.provider.msi.impl.SQLResultSetProvider
+import fr.proline.core.om.util.AbstractMultipleDBTestCase
+import fr.proline.core.orm.uds.Dataset
+import fr.proline.core.orm.uds.MasterQuantitationChannel
+import fr.proline.core.orm.uds.QuantitationChannel
+import fr.proline.core.service.msq.quantify.WeightedSpectralCountQuantifier
+import fr.proline.repository.DriverType
+import fr.proline.core.orm.uds.Project
+import fr.proline.core.orm.uds.SampleAnalysis
+import fr.proline.core.orm.uds.BiologicalSample
+import fr.proline.core.service.msq.export.WeightedSCResultReader
+
+@Test
+class WeightedSCReaderTest extends AbstractMultipleDBTestCase with Logging {
+
+  // Define the interface to be implemented
+  val driverType = DriverType.H2
+  val fileName = "STR_F063442_F122817_MergedRSMs"
+  val targetRSMId: Long = 33
+
+  var executionContext: IExecutionContext = null
+
+  @Before
+  @throws(classOf[Exception])
+  def setUp() = {
+
+    logger.info("Initializing DBs")
+    super.initDBsDBManagement(driverType)
+
+    //Load Data
+    pdiDBTestCase.loadDataSet("/dbunit/datasets/pdi/Proteins_Dataset.xml")
+    psDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/ps-db.xml")
+    msiDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/msi-db.xml")
+    udsDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/uds-db.xml")
+
+    logger.info("PDI, PS, MSI and UDS dbs succesfully initialized !")
+
+    val execContext = buildJPAContext()
+    executionContext = execContext
+  }
+
+  @After
+  override def tearDown() {
+    if (executionContext != null) executionContext.closeAll()
+    super.tearDown()
+  }
+
+  def buildJPAContext() = {
+    val executionContext = ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true) // Full JPA
+
+    executionContext
+  }
+
+  @Test
+  def readRSMSC() = {
+    //  Validate RS to generate RSM
+    val dsId = computeSC()
+    var wscReader = new WeightedSCResultReader(execCtx = executionContext, datasetId = dsId)
+    wscReader.run()
+    val result :String  = wscReader.getResultAsJSON
+    assertNotNull(result)
+    logger.info(result)
+  }
+  
+  private def computeSC() : Long = {
+  
+    val spCountCfg = new SpectralCountConfig(parentRSMId = Some(targetRSMId))
+
+    val udsEm = executionContext.getUDSDbConnectionContext.getEntityManager
+    udsEm.getTransaction().begin()
+
+    //Create Exp Design
+    val pj: Project = udsEm.find(classOf[Project], 1l)
+    val qtDS = new Dataset(pj)
+    qtDS.setNumber(2)
+    qtDS.setName("WSC test DS")
+    qtDS.setType(Dataset.DatasetType.QUANTITATION)
+    qtDS.setChildrenCount(2)
+
+    //Create Sample Analysis
+    val splAnalysis1 = new SampleAnalysis()
+    splAnalysis1.setNumber(1)
+    splAnalysis1.setDataset(qtDS)
+
+    val splAnalysis2 = new SampleAnalysis()
+    splAnalysis2.setNumber(2)
+    splAnalysis2.setDataset(qtDS)
+
+    //Create BiologicalSample
+    val bioSpl1 = new BiologicalSample()
+    bioSpl1.setName("WSC Test BioSpl")
+    bioSpl1.setNumber(1)
+    bioSpl1.setDataset(qtDS)
+
+    //Create link between SampleAnalysis  & BiologicalSample
+    val allSplAnalysis = new ArrayList[SampleAnalysis](2)
+    allSplAnalysis.add(splAnalysis1)
+    allSplAnalysis.add(splAnalysis2)
+    val bioSpls = new ArrayList[BiologicalSample](1)
+    bioSpls.add(bioSpl1)
+    bioSpl1.setSampleReplicates(allSplAnalysis)
+    splAnalysis1.setBiologicalSample(bioSpls)
+    splAnalysis2.setBiologicalSample(bioSpls)
+
+    //Create QuantitationChannel
+    val qCh1 = new QuantitationChannel()
+    qCh1.setIdentResultSummaryId(1)
+    qCh1.setContextKey("1.1")
+    qCh1.setQuantitationDataset(qtDS)
+
+    val qCh2 = new QuantitationChannel()
+    qCh2.setIdentResultSummaryId(2)
+    qCh2.setContextKey("1.2")
+    qCh2.setQuantitationDataset(qtDS)
+
+    //Create MasterQuantitationChannel
+    val mqCh = new MasterQuantitationChannel()
+    mqCh.setName("WSC Test")
+    mqCh.setNumber(1)
+
+    //Create link between QuantitationChannel  & BiologicalSample&SampleAnalysis
+    val qChs = new ArrayList[QuantitationChannel](2)
+    qChs.add(qCh1)
+    qChs.add(qCh2)
+    qCh1.setNumber(1)
+    qCh1.setSampleReplicate(splAnalysis1)
+    qCh1.setBiologicalSample(bioSpl1)
+    qCh2.setNumber(2)
+    qCh2.setSampleReplicate(splAnalysis2)
+    qCh2.setBiologicalSample(bioSpl1)
+    bioSpl1.setQuantitationChannels(qChs)
+    splAnalysis1.setQuantitationChannels(qChs)// a verifier vds
+
+    //Create link between MasterQuantitationChannel  & QuantitationChannels
+    mqCh.setQuantitationChannels(qChs)
+    qCh2.setMasterQuantitationChannel(mqCh)
+    qCh1.setMasterQuantitationChannel(mqCh)
+
+    //Create link between MasterQCh  & Dataset
+    mqCh.setDataset(qtDS)
+    val mqChs = new ArrayList[MasterQuantitationChannel](1)
+    mqChs.add(mqCh)
+    qtDS.setMasterQuantitationChannels(mqChs)
+    qtDS.setQuantitationChannels(qChs)
+
+    udsEm.persist(qtDS)
+    udsEm.persist(splAnalysis1)
+    udsEm.persist(splAnalysis2)
+    udsEm.persist(bioSpl1)
+
+    udsEm.getTransaction().commit()
+
+    var wsCalculator = new WeightedSpectralCountQuantifier(executionContext = executionContext, udsMasterQuantChannel = mqCh, scConfig = spCountCfg)
+    wsCalculator.quantify
+    assertNotNull(mqCh.getQuantResultSummaryId())
+    qtDS.getId()
+
+  }
+
+}
+
