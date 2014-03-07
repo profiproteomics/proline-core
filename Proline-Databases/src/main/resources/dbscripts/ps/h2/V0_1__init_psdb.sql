eebb1fa9a55d86e181505f759868d1e5e504a800
Alexandre Walter <alexandre.walter@unistra.fr>
2014-03-07 14:48:19 +0000

    * removed UNIQUE constraint on ptm.full_name column It should allow alias creation by only having ptm.short_name but why not same full name.
    Former-commit-id: 494661944fbf25367b90365f62507f374f1578e5

--- Proline-Databases/src/main/resources/dbscripts/ps/h2/V0_1__init_psdb.sql
+++ Proline-Databases/src/main/resources/dbscripts/ps/h2/V0_1__init_psdb.sql
@@ -88,18 +88,16 @@ CREATE TABLE public.ptm (
                 CONSTRAINT ptm_pk PRIMARY KEY (id)
 );
 COMMENT ON TABLE public.ptm IS 'Describes the names of the ptm definitions.
-UNIQUE(full_name)
 UNIQUE(short_name)';
 COMMENT ON COLUMN public.ptm.unimod_id IS 'The unimod record_id.';
-COMMENT ON COLUMN public.ptm.full_name IS 'A description of the PTM.
-MUST BE UNIQUE.';
+COMMENT ON COLUMN public.ptm.full_name IS 'A description of the PTM.;
 COMMENT ON COLUMN public.ptm.short_name IS 'Descriptive, one word name, suitable for use in software applications.
 This name must not include the specificity. For example, Carboxymethyl is the short name, not Carboxymethyl-Cys or Carboxymethyl (C).
 MUST BE UNIQUE.';
 COMMENT ON COLUMN public.ptm.serialized_properties IS 'A JSON string which stores optional properties (see corresponding JSON schema for more details).';
 
 
-CREATE UNIQUE INDEX public.ptm_full_name_idx
+CREATE INDEX public.ptm_full_name_idx
  ON public.ptm
  ( full_name );
 
