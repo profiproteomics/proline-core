4a0820daf22083b26e2cac5bd88ce7d259863578
David Bouyssié <David.Bouyssie@ipbs.fr>
2014-03-10 14:58:13 +0000

    Create specific migration script for the new ptm.full_name index definition
    Former-commit-id: cdaed78ca9c61f69d5a7be55fc75b5862e4dd1e8

--- Proline-Databases/src/main/resources/dbscripts/ps/postgresql/V0_2__core_0.1.3.sql
+++ Proline-Databases/src/main/resources/dbscripts/ps/postgresql/V0_2__core_0.1.3.sql
@@ -5,7 +5,3 @@ COMMENT ON COLUMN public.peptide_ptm.average_mass IS 'The average mass of the co
 TODO: convert to REAL (float) ?';
 
 ALTER TABLE ONLY public.peptide_ptm ALTER COLUMN mono_mass TYPE DOUBLE PRECISION, ALTER COLUMN mono_mass SET NOT NULL;
-
-DROP INDEX public.ptm_full_name_idx;
-
-CREATE INDEX public.ptm_full_name_idx ON public.ptm ( full_name );
