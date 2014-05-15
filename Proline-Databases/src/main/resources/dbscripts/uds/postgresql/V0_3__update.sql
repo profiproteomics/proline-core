2a599bd96f9b5fec4dc3e1e5ef68bce6286cd7aa
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-05-15 06:39:31 +0000

    fixes #10185 : utilise un mdp  fixe !
    Former-commit-id: c8feb012486b61bba1692250210dc8a34045a70e

--- Proline-Databases/src/main/resources/dbscripts/uds/postgresql/V0_3__update.sql
+++ Proline-Databases/src/main/resources/dbscripts/uds/postgresql/V0_3__update.sql
@@ -1,6 +1,6 @@
 ALTER TABLE user_account ADD COLUMN password_hash varchar;
 COMMENT ON COLUMN public.user_account.password_hash IS 'hash of user password, using sha-256.';
 
-UPDATE user_account SET password_hash = encode(digest(login,'SHA256'),'hex') WHERE password_hash is null;
+UPDATE user_account SET password_hash = '6692a40c9e151cb2917b1cdadfb25f98a4aca31f9b020b4ff320d04a99ccccf0' WHERE password_hash is null;
 
 ALTER TABLE user_account ALTER COLUMN password_hash set NOT NULL;
