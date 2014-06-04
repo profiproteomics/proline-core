25bff81eabf0a35a24350a3cf38b09464820191e
Christophe Bruley <christophe.bruley@cea.fr>
2014-06-04 08:03:17 +0000

    ionseries regexp usage modified to take into account * or 0 labelled series
    Former-commit-id: bf4aeefef5ea876adf6fe4f95f1d715e9647a123

--- Proline-OM/src/main/scala/fr/proline/core/om/model/msi/ObjectTreeClasses.scala
+++ Proline-OM/src/main/scala/fr/proline/core/om/model/msi/ObjectTreeClasses.scala
@@ -5,20 +5,20 @@ import scala.beans.BeanProperty
 //import com.fasterxml.jackson.annotation.JsonInclude.Include
 //import org.msgpack.annotation.Message
 
-/** 
-* @param label 
-* @param aaPosition 
-* @param type 
-* @param charge 
-* @param moz 
-* @param calculatedMoz 
-* @param intensity 
-* @param neutralLossMass 
-* @param theoreticalFragmentId 
-**/
+/**
+ * @param label
+ * @param aaPosition
+ * @param type
+ * @param charge
+ * @param moz
+ * @param calculatedMoz
+ * @param intensity
+ * @param neutralLossMass
+ * @param theoreticalFragmentId
+ */
 //@JsonInclude( Include.NON_NULL )
 //@Message
-case class FragmentMatch (  
+case class FragmentMatch(
   var label: String,
   //val ionSeries: String,
   //val aaPosition: Int,
@@ -27,85 +27,84 @@ case class FragmentMatch (
   var moz: Double,
   var calculatedMoz: Double,
   var intensity: Float,
-  var neutralLossMass: Option[Double] = None
-) {
-  
+  var neutralLossMass: Option[Double] = None) {
+
   // Plain constructor needed for MessagePack
-  def this() = this("",None,Double.NaN,Double.NaN,Float.NaN,None)
-  
+  def this() = this("", None, Double.NaN, Double.NaN, Float.NaN, None)
+
   @transient private var _ionSeries: String = null
   @transient private var _aaPosition: Int = 0
   @transient private var _charge: Int = 1
-  
+
   private def _parseLabelIfNotDone() {
-    if( _ionSeries == null ) {
-      val FullLabelRegex = """(\w+)\((\d+)\)([\w\+]*)""".r      
-      val FullLabelRegex(ionSeries, aaPositionStr, chargeStr) = label
-      
-      this._ionSeries = ionSeries
-      this._aaPosition = aaPositionStr.toInt
-      
-      if( chargeStr.length > 0 )
-        this._charge = chargeStr.length
+    if (_ionSeries == null) {
+      val FullLabelRegex = """(\w+\*?)\((\d+)\)([\w\+]*)""".r
+      FullLabelRegex.findAllIn(label).matchData.foreach {
+        m =>
+          this._ionSeries = m.group(1)
+          this._aaPosition = m.group(2).toInt
+
+          if (m.group(3).length > 0)
+            this._charge = m.group(3).length
+      }
     }
   }
-  
-  def ionSeries: String = {_parseLabelIfNotDone(); this._ionSeries}
-  def aaPosition: Int = {_parseLabelIfNotDone(); this._aaPosition}
-  def charge: Int = {_parseLabelIfNotDone(); this._charge}
-  
+
+  def ionSeries: String = { _parseLabelIfNotDone(); this._ionSeries }
+  def aaPosition: Int = { _parseLabelIfNotDone(); this._aaPosition }
+  def charge: Int = { _parseLabelIfNotDone(); this._charge }
+
 }
 
 object FragmentMatchType extends Enumeration {
+  type MatchType = Value
   val INTERNAL = Value("IN")
   val IMMONIUM = Value("IM")
 }
 
-/** 
-* @param fragmentationTable 
-* @param fragmentMatches 
-**/
+/**
+ * @param fragmentationTable
+ * @param fragmentMatches
+ */
 //@JsonInclude( Include.NON_NULL )
 //@Message
-case class SpectrumMatch (
+case class SpectrumMatch(
   @transient val msQueryInitialId: Int,
   @transient val peptideMatchRank: Int,
   var fragTable: Array[TheoreticalFragmentSeries],
-  var fragMatches: Array[FragmentMatch]
-) {
+  var fragMatches: Array[FragmentMatch]) {
   // Plain constructor needed for MessagePack
-  def this() = this(0,0,Array.empty[TheoreticalFragmentSeries],Array.empty[FragmentMatch])
+  def this() = this(0, 0, Array.empty[TheoreticalFragmentSeries], Array.empty[FragmentMatch])
 }
 
 //@JsonInclude( Include.NON_NULL )
 //@Message
-case class TheoreticalFragmentSeries (
+case class TheoreticalFragmentSeries(
   var fragSeries: String, // begins by an ionSeries name and may be followed by a "++" for doubly charge state
-  var masses: Array[Double]
-) {
-  
+  var masses: Array[Double]) {
+
   // Plain constructor needed for MessagePack
-  def this() = this("",Array.empty[Double])
-  
+  def this() = this("", Array.empty[Double])
+
   @transient lazy val isReverse = Fragmentation.isReverseSeries(fragSeries)
-  
+
   @transient private var _ionSeries: String = null
   @transient private var _charge: Int = 1
-  
+
   private def _parseFragSeriesIfNotDone() {
-    if( _ionSeries == null ) {
+    if (_ionSeries == null) {
       val FragSeriesRegex = """(\w+\*?)([+]*).*""".r
       val FragSeriesRegex(ionSeries, chargeStr) = fragSeries
-      
-      this._ionSeries = ionSeries.replace("0","-H2O").replace("*","-NH3")
-      
-      if( chargeStr.length > 0 )
+
+      this._ionSeries = ionSeries.replace("0", "-H2O").replace("*", "-NH3")
+
+      if (chargeStr.length > 0)
         this._charge = chargeStr.length
     }
   }
-  
-  def ionSeries: String = {_parseFragSeriesIfNotDone(); this._ionSeries}
-  def charge: Int = {_parseFragSeriesIfNotDone(); this._charge}
+
+  def ionSeries: String = { _parseFragSeriesIfNotDone(); this._ionSeries }
+  def charge: Int = { _parseFragSeriesIfNotDone(); this._charge }
 }
 
 
