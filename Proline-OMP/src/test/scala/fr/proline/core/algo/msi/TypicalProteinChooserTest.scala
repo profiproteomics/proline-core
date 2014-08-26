45fdb0ff7354db742fdc0cfb84e47c8f7d5fec52
David Bouyssié <David.Bouyssie@ipbs.fr>
2014-08-26 16:03:15 +0000

    Optimize the TypicalProteinChooserTest (using @BeforeClass)
    Former-commit-id: 4d1961f37241bd7c97235c4b81718ad243819f66

--- Proline-OMP/src/test/scala/fr/proline/core/algo/msi/TypicalProteinChooserTest.scala
+++ Proline-OMP/src/test/scala/fr/proline/core/algo/msi/TypicalProteinChooserTest.scala
@@ -1,32 +1,27 @@
 package fr.proline.core.algo.msi
 
-import org.junit.After
+import org.junit.AfterClass
 import org.junit.Assert
-import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 import com.typesafe.scalalogging.slf4j.Logging
-import fr.proline.context.BasicExecutionContext
 import fr.proline.context.IExecutionContext
 import fr.proline.core.dal.AbstractMultipleDBTestCase
 import fr.proline.core.dal.ContextFactory
-import fr.proline.core.om.model.msi.ProteinMatch
-import fr.proline.core.om.model.msi.ResultSummary
-import fr.proline.core.om.provider.msi.IResultSummaryProvider
-import fr.proline.core.om.provider.msi.impl.SQLResultSetProvider
-import fr.proline.core.om.provider.msi.impl.SQLResultSummaryProvider
 import fr.proline.repository.DriverType
-import fr.proline.core.om.provider.msi.IResultSetProvider
+import fr.proline.core.dbunit.STR_F063442_F122817_MergedRSMs
+import fr.proline.core.dbunit.DbUnitSampleDataset
 
-class TypicalProteinChooserTest extends AbstractMultipleDBTestCase with Logging {
+object TypicalProteinChooserTest extends AbstractMultipleDBTestCase with Logging {
 
   // Define some vars
   val driverType = DriverType.H2
-  val fileName = "STR_F063442_F122817_MergedRSMs"
-  val targetRSMId: Long = 33
+  val dbUnitResultFile = STR_F063442_F122817_MergedRSMs
+  val targetRSMId: Long = 33L
 
   var executionContext: IExecutionContext = null
 
-  @Before
+  @BeforeClass
   @throws(classOf[Exception])
   def setUp() = {
 
@@ -34,43 +29,50 @@ class TypicalProteinChooserTest extends AbstractMultipleDBTestCase with Logging 
     super.initDBsDBManagement(driverType)
 
     //Load Data
-    pdiDBTestCase.loadDataSet("/dbunit/datasets/pdi/Proteins_Dataset.xml")
-    psDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/ps-db.xml")
-    msiDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/msi-db.xml")
-    udsDBTestCase.loadDataSet("/dbunit_samples/" + fileName + "/uds-db.xml")
-
-       //loadDataSet("/fr/proline/core/om/ps/Unimod_Dataset.xml")
-    msiDBTestCase.loadCompositeDataSet(Array("/dbunit_samples/" + fileName + "/msi-db.xml","/fr/proline/core/algo/msi/Prot_ChangeTypical.xml"))
+    pdiDBTestCase.loadDataSet(DbUnitSampleDataset.PROTEINS.getResourcePath)
+    psDBTestCase.loadDataSet(dbUnitResultFile.psDbDatasetPath)
+    msiDBTestCase.loadCompositeDataSet(
+      Array(
+        dbUnitResultFile.msiDbDatasetPath,
+        "/fr/proline/core/algo/msi/Prot_ChangeTypical.xml"
+      )
+    )
+    udsDBTestCase.loadDataSet(dbUnitResultFile.udsDbDatasetPath)    
     
     logger.info("PDI, PS, MSI and UDS dbs succesfully initialized !")
-
-    val execContext = buildJPAContext()
-    executionContext = execContext
+    
+    executionContext = buildJPAContext()
   }
 
-  @After
+  @AfterClass
   override def tearDown() {
     if (executionContext != null) executionContext.closeAll()
     super.tearDown()
   }
 
   def buildJPAContext() = {
-    val executionContext = ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true) // Full JPA
-    executionContext
+    ContextFactory.buildExecutionContext(dsConnectorFactoryForTest, 1, true) // Full JPA
   }
+  
+}
+
+class TypicalProteinChooserTest extends AbstractMultipleDBTestCase with Logging {
+
+  val targetRSMId = TypicalProteinChooserTest.targetRSMId
+  val executionContext = TypicalProteinChooserTest.executionContext
 
   @Test
   def testChangeTypicalProt() = {
 
     // Check which proteinSets should be modified by algo
-    var nbrTremblShouldChange: Int = 9 // # proteinSet matching following rule 
+    val nbrTremblShouldChange: Int = 9 // # proteinSet matching following rule 
     //VDS WARNING : Was 4, after modifying algo, changed proteinset was 9 dur to incorrect IS_IN_SUBSET value in XML file :  
     // Allways FALSE even for subset. Should be changed back to 4 with corrected XML file ! 
 
     val typicalChooser = new TypicalProteinChooser()
-    
+
     val ruleDesc = new TypicalProteinChooserRule(ruleName = "Sprot AC preferred", applyToAcc = true, rulePattern = "\\w{6,6}")
-    val rules =  Seq(ruleDesc)
+    val rules = Seq(ruleDesc)
     typicalChooser.changeTypical(targetRSMId, rules, executionContext.getMSIDbConnectionContext().getEntityManager())
 
     val nbrChangedTyp = typicalChooser.getChangedProteinSets.size
@@ -79,35 +81,32 @@ class TypicalProteinChooserTest extends AbstractMultipleDBTestCase with Logging 
 
   }
 
-  
   @Test
   def testChangeTypicalProtSameSubSet() = {
 
     // Check which proteinSets should be modified by algo
-    var nbrTremblShouldChange: Int =1   
+    var nbrTremblShouldChange: Int = 1
 
     val typicalChooser = new TypicalProteinChooser()
     val ruleDesc = new TypicalProteinChooserRule(ruleName = "Description ##SP  preferred", applyToAcc = false, rulePattern = "##SP.*")
-    val rules =  Seq(ruleDesc)
+    val rules = Seq(ruleDesc)
     typicalChooser.changeTypical(targetRSMId, rules, executionContext.getMSIDbConnectionContext().getEntityManager())
 
     val nbrChangedTyp = typicalChooser.getChangedProteinSets.size
 
     Assert.assertEquals(nbrTremblShouldChange, nbrChangedTyp)
 
-        // Check which proteinSets should be modified by algo
-    nbrTremblShouldChange = 0   
-    
+    // Check which proteinSets should be modified by algo
+    nbrTremblShouldChange = 0
+
     val ruleDesc2 = new TypicalProteinChooserRule(ruleName = "Description ##DEV_ preferred", applyToAcc = false, rulePattern = "##DEV_.*")
-    val rules2 =  Seq(ruleDesc2)
+    val rules2 = Seq(ruleDesc2)
     typicalChooser.changeTypical(targetRSMId, rules2, executionContext.getMSIDbConnectionContext().getEntityManager())
 
     val nbrChangedTyp2 = typicalChooser.getChangedProteinSets.size
 
     Assert.assertEquals(nbrTremblShouldChange, nbrChangedTyp2)
 
-
-    
   }
 
 }
