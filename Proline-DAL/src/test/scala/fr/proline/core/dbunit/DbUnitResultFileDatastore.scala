4ac2dacc2f180c3e04678a8b0f7c6ff86495980a
David Bouyssié <David.Bouyssie@ipbs.fr>
2014-08-26 14:27:57 +0000

    Rename DbUnitResultFileDatastore into DbUnitResultFileLocation
    Former-commit-id: 213f3aa60426807373e510c6e66878cbc2b4e22e

--- Proline-DAL/src/test/scala/fr/proline/core/dbunit/DbUnitResultFileDatastore.scala
+++ Proline-DAL/src/test/scala/fr/proline/core/dbunit/DbUnitResultFileDatastore.scala
@@ -4,7 +4,8 @@ package fr.proline.core.dbunit
  * @author David Bouyssie
  *
  */
-sealed abstract class DbUnitResultFileDatastore {
+// TODO: rename the file in DbUnitResultFileLocation
+sealed abstract class DbUnitResultFileLocation {
   
   def datastoreDirPath: String
   val udsDbDatasetPath = datastoreDirPath + "/uds-db.xml"
@@ -13,22 +14,22 @@ sealed abstract class DbUnitResultFileDatastore {
 
 }
 
-case object GRE_F068213_M2_4_TD_EColi extends DbUnitResultFileDatastore {
+case object GRE_F068213_M2_4_TD_EColi extends DbUnitResultFileLocation {
   val datastoreDirPath = "/dbunit_samples/GRE_F068213_M2.4_TD_EColi"
 }
 
-case object STR_F063442_F122817_MergedRSMs extends DbUnitResultFileDatastore {
+case object STR_F063442_F122817_MergedRSMs extends DbUnitResultFileLocation {
   val datastoreDirPath = "/dbunit_samples/STR_F063442_F122817_MergedRSMs"
 }
 
-case object STR_F122817_Mascot_v2_3 extends DbUnitResultFileDatastore {
+case object STR_F122817_Mascot_v2_3 extends DbUnitResultFileLocation {
   val datastoreDirPath = "/dbunit_samples/STR_F122817_Mascot_v2.3"
 }
 
-case object STR_F136482_CTD extends DbUnitResultFileDatastore {
+case object STR_F136482_CTD extends DbUnitResultFileLocation {
   val datastoreDirPath = "/dbunit_samples/STR_F136482_CTD"
 }
 
-case object TLS_F027737_MTD_no_varmod extends DbUnitResultFileDatastore {
+case object TLS_F027737_MTD_no_varmod extends DbUnitResultFileLocation {
   val datastoreDirPath = "/dbunit_samples/TLS_F027737_MTD_no_varmod"
 }
