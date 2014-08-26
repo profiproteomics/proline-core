1f7a50a5d3f2f340c43b4a56ca192ac397a50df6
David Bouyssié <David.Bouyssie@ipbs.fr>
2014-08-26 07:03:38 +0000

    Instantiate the DatabaseTester with the appropriate DataTypeFactory (remove DbUnit verbose warnings)
    Former-commit-id: c0f2e64f222c5221f56e2cc39315b335d9a75c30

--- Proline-Databases/src/test/java/fr/proline/repository/util/DatabaseTestConnector.java
+++ Proline-Databases/src/test/java/fr/proline/repository/util/DatabaseTestConnector.java
@@ -1,5 +1,7 @@
 package fr.proline.repository.util;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 
 import javax.persistence.EntityManagerFactory;
@@ -7,14 +9,20 @@ import javax.sql.DataSource;
 
 import org.dbunit.DataSourceDatabaseTester;
 import org.dbunit.IDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.datatype.DefaultDataTypeFactory;
+import org.dbunit.dataset.datatype.IDataTypeFactory;
+import org.dbunit.ext.h2.H2DataTypeFactory;
+import org.dbunit.ext.postgresql.PostgresqlDataTypeFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.profi.util.StringUtils;
 import fr.proline.repository.DatabaseConnectorFactory;
 import fr.proline.repository.DriverType;
 import fr.proline.repository.IDatabaseConnector;
 import fr.proline.repository.ProlineDatabaseType;
-import fr.profi.util.StringUtils;
 
 /**
  * Implements IDatabaseConnector to add a new feature allowing dbUnit handling (IDatabaseTester for dbUnit).
@@ -25,108 +33,149 @@ import fr.profi.util.StringUtils;
  */
 public class DatabaseTestConnector implements IDatabaseConnector {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DatabaseTestConnector.class);
+	private static final Logger LOG = LoggerFactory.getLogger(DatabaseTestConnector.class);
 
-    private final Object m_closeLock = new Object();
+	private final Object m_closeLock = new Object();
 
-    /* m_closed, onTearDown() and close() are @GuardedBy("m_closeLock") */
+	/* m_closed, onTearDown() and close() are @GuardedBy("m_closeLock") */
 
-    private final IDatabaseConnector m_realConnector;
+	private final IDatabaseConnector m_realConnector;
 
-    private final IDatabaseTester m_databaseTester;
+	private final IDatabaseTester m_databaseTester;
 
-    private boolean m_closed;
+	private boolean m_closed;
 
-    public DatabaseTestConnector(final ProlineDatabaseType database, final Map<Object, Object> properties) {
+	public DatabaseTestConnector(final ProlineDatabaseType database, final Map<Object, Object> properties) {
 
-	if (database == null) {
-	    throw new IllegalArgumentException("Database is null");
-	}
-
-	if (properties == null) {
-	    throw new IllegalArgumentException("Properties Map is null");
-	}
-
-	m_realConnector = DatabaseConnectorFactory.createDatabaseConnectorInstance(database, properties);
-
-	m_databaseTester = new DataSourceDatabaseTester(m_realConnector.getDataSource());
-    }
-
-    public DatabaseTestConnector(final ProlineDatabaseType database, final String propertiesFileName) {
-
-	if (database == null) {
-	    throw new IllegalArgumentException("Database is null");
-	}
-
-	if (StringUtils.isEmpty(propertiesFileName)) {
-	    throw new IllegalArgumentException("Invalid propertiesFileName");
-	}
-
-	m_realConnector = DatabaseConnectorFactory.createDatabaseConnectorInstance(database,
-		propertiesFileName);
-
-	m_databaseTester = new DataSourceDatabaseTester(m_realConnector.getDataSource());
-    }
-
-    public ProlineDatabaseType getProlineDatabaseType() {
-	return m_realConnector.getProlineDatabaseType();
-    }
-
-    public DriverType getDriverType() {
-	return m_realConnector.getDriverType();
-    }
-
-    public boolean isMemory() {
-	return m_realConnector.isMemory();
-    }
-
-    @Override
-    public void setAdditionalProperties(Map<Object, Object> additionalProperties) {
-	m_realConnector.setAdditionalProperties(additionalProperties);
-    }
-
-    public DataSource getDataSource() {
-	return m_realConnector.getDataSource();
-    }
-
-    public EntityManagerFactory getEntityManagerFactory() {
-	return m_realConnector.getEntityManagerFactory();
-    }
-
-    public void close() {
-
-	synchronized (m_closeLock) {
-
-	    if (!m_closed) { // Close only once
-		m_closed = true;
-
-		LOG.debug("Tearing down DataSourceDatabaseTester");
-
-		try {
-		    m_databaseTester.onTearDown();
-		} catch (Exception ex) {
-		    LOG.error("Error tearing down DataSourceDatabaseTester", ex);
+		if (database == null) {
+			throw new IllegalArgumentException("Database is null");
 		}
 
-		m_realConnector.close();
-	    }
+		if (properties == null) {
+			throw new IllegalArgumentException("Properties Map is null");
+		}
 
-	} // End of synchronized block on m_closeLock
+		m_realConnector = DatabaseConnectorFactory.createDatabaseConnectorInstance(database, properties);
+		
+		m_databaseTester = this.createDatabaseTester(m_realConnector.getDataSource());
+	}
 
-    }
+	public DatabaseTestConnector(final ProlineDatabaseType database, final String propertiesFileName) {
 
-    public boolean isClosed() {
-	boolean result;
+		if (database == null) {
+			throw new IllegalArgumentException("Database is null");
+		}
 
-	synchronized (m_closeLock) {
-	    result = (m_closed || m_realConnector.isClosed());
-	} // End of synchronized block on m_closeLock
+		if (StringUtils.isEmpty(propertiesFileName)) {
+			throw new IllegalArgumentException("Invalid propertiesFileName");
+		}
 
-	return result;
-    }
+		m_realConnector = DatabaseConnectorFactory.createDatabaseConnectorInstance(
+			database,
+			propertiesFileName
+		);
 
-    public IDatabaseTester getDatabaseTester() {
-	return m_databaseTester;
-    }
+		m_databaseTester = this.createDatabaseTester(m_realConnector.getDataSource());
+	}
+	
+	private IDatabaseTester createDatabaseTester( DataSource dataSource ) {
+		
+		final DriverType driverType = this.getDriverType();
+		
+		// Return a DataSourceDatabaseTester configured with the appropriate IDataTypeFactory
+		return new DataSourceDatabaseTester(dataSource) {
+			@Override
+			public IDatabaseConnection getConnection() throws Exception {
+				IDatabaseConnection dbC = super.getConnection();
+				
+				// Retrieve the IDataTypeFactory corresponding to the DriverType
+				IDataTypeFactory dataTypeFactory = null;
+				switch (driverType) {
+				case H2:
+					dataTypeFactory = new H2DataTypeFactory();
+					break;
+				case POSTGRESQL:
+					dataTypeFactory = new PostgresqlDataTypeFactory();
+					break;
+				case SQLITE:
+					dataTypeFactory = new DefaultDataTypeFactory() {
+						@SuppressWarnings("rawtypes")
+						@Override
+						public Collection getValidDbProducts() {
+							return Arrays.asList(new String[]{"sqlite"});
+						}
+					};
+					break;
+				}
+				
+				dbC.getConfig().setProperty(
+					DatabaseConfig.PROPERTY_DATATYPE_FACTORY, dataTypeFactory
+				);
+				
+				return dbC;
+			}
+		};
+	}
+
+	public ProlineDatabaseType getProlineDatabaseType() {
+		return m_realConnector.getProlineDatabaseType();
+	}
+
+	public DriverType getDriverType() {
+		return m_realConnector.getDriverType();
+	}
+
+	public boolean isMemory() {
+		return m_realConnector.isMemory();
+	}
+
+	@Override
+	public void setAdditionalProperties(Map<Object, Object> additionalProperties) {
+		m_realConnector.setAdditionalProperties(additionalProperties);
+	}
+
+	public DataSource getDataSource() {
+		return m_realConnector.getDataSource();
+	}
+
+	public EntityManagerFactory getEntityManagerFactory() {
+		return m_realConnector.getEntityManagerFactory();
+	}
+
+	public void close() {
+
+		synchronized (m_closeLock) {
+
+			if (!m_closed) { // Close only once
+				m_closed = true;
+
+				LOG.debug("Tearing down DataSourceDatabaseTester");
+
+				try {
+					m_databaseTester.onTearDown();
+				} catch (Exception ex) {
+					LOG.error("Error tearing down DataSourceDatabaseTester", ex);
+				}
+
+				m_realConnector.close();
+			}
+
+		} // End of synchronized block on m_closeLock
+
+	}
+
+	public boolean isClosed() {
+		boolean result;
+
+		synchronized (m_closeLock) {
+			result = (m_closed || m_realConnector.isClosed());
+		} // End of synchronized block on m_closeLock
+
+		return result;
+	}
+
+	public IDatabaseTester getDatabaseTester() {
+		return m_databaseTester;
+	}
 
 }
