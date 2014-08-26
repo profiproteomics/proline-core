5e8d15dfce8798719529c87e5e06bef9e0b28f22
David Bouyssié <David.Bouyssie@ipbs.fr>
2014-08-26 13:44:38 +0000

    Fix the AuthenticationTest (add user account manually instead of relying on the content of the dataset)
    Former-commit-id: fd0555ae4f4adf2996617e73b5fe1b71005be36f

--- Proline-OMP/src/test/scala/fr/proline/core/service/uds/AuthenticationTest.scala
+++ Proline-OMP/src/test/scala/fr/proline/core/service/uds/AuthenticationTest.scala
@@ -1,59 +1,73 @@
 package fr.proline.core.service.uds
 
 import org.junit.After
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import org.junit.Assert.assertTrue
+
 import com.typesafe.scalalogging.slf4j.Logging
-import fr.proline.context.IExecutionContext
-import fr.proline.repository.DriverType
+
+import fr.proline.context.DatabaseConnectionContext
+import fr.proline.core.dal.ContextFactory
+import fr.proline.core.dal.context._
+import fr.proline.core.dbunit.DbUnitInitDataset
+import fr.proline.core.orm.uds.UserAccount
 import fr.proline.repository.ProlineDatabaseType
 import fr.proline.repository.util.DatabaseTestCase
-import fr.proline.context.DatabaseConnectionContext
-import fr.proline.core.om.provider.msi.impl.SQLResultSummaryProvider
-import fr.proline.core.dal.ContextFactory
-import fr.proline.context.BasicExecutionContext
 
-@Test
 class AuthenticationTest extends DatabaseTestCase with Logging {
 
-  // Define the interface to be implemented
-  val driverType = DriverType.H2
-  val fileName = "STR_F136482_CTD"
-   
+  val udsUser = new UserAccount()
+  udsUser.setLogin("proline_user")
+  udsUser.setPasswordHash("993bd037c1ae6b87ea5adb936e71fca01a3b5c0505855444afdfa2f86405db1d")
+  udsUser.setCreationMode("MANUAL")
+  
   override def getProlineDatabaseType() = ProlineDatabaseType.UDS
-
 
   var udsDbCtx: DatabaseConnectionContext = null
 
   @Before
   @throws(classOf[Exception])
-  def setUp() = {
+  def setUp() {
 
     logger.info("Initializing UDS db")
     initDatabase()
-    loadDataSet("/dbunit_samples/" + fileName + "/uds-db.xml")
-   
+    //loadDataSet("/dbunit_samples/" + fileName + "/uds-db.xml")
+    loadDataSet(DbUnitInitDataset.UDS_DB.getResourcePath)
+    
     logger.info("UDS db succesfully initialized !")
     buildUDSConnectionContext()
-  }
 
-  @After
-  override def tearDown() = {
-    super.tearDown();
-  }
-  
-  def buildUDSConnectionContext() = {
-    udsDbCtx = ContextFactory.buildDbConnectionContext(getConnector, false)
+    // Persist the user account
+    udsDbCtx.tryInTransaction {
+      udsDbCtx.getEntityManager.persist(udsUser)
+    }
     
   }
 
+  @After
+  override def tearDown() {
+    super.tearDown()
+  }
+
+  def buildUDSConnectionContext() {
+    udsDbCtx = ContextFactory.buildDbConnectionContext(getConnector, true)
+  }
+
   @Test
-  def authenticateUser() = {
-	  val service = new UserAuthenticator(udsConnectionCtxt = udsDbCtx,name = "proline_user", hashPassword = "993bd037c1ae6b87ea5adb936e71fca01a3b5c0505855444afdfa2f86405db1d" )
-      val result = service.runService()
-      
-      assertTrue(result)
+  def authenticateUser() {
+    val service = new UserAuthenticator(
+      udsConnectionCtxt = udsDbCtx,
+      name = udsUser.getLogin(),
+      hashPassword = udsUser.getPasswordHash()
+    )
+    val isLoginOK = service.runService()
+    
+    if( !isLoginOK) {
+      logger.error( service.getErrorMessage )
+    }
+
+    assertTrue(isLoginOK)
   }
 
 }
