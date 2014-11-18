d1eea1627de09cd7eac7e9a7e0f91c67382a7344
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-11-18 08:21:52 +0000

    Supprime l'héritage de AbstractXXX => Erreur hibernate
    Former-commit-id: 30d55e10f85fea8ac11f3c0c4443bb9a3968348d

--- Proline-ORM/src/main/java/fr/proline/core/orm/uds/ObjectTree.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/uds/ObjectTree.java
@@ -1,18 +1,86 @@
 package fr.proline.core.orm.uds;
 
-import javax.persistence.Entity;
+import java.io.Serializable;
 
-import fr.proline.core.orm.AbstractObjectTree;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  * The persistent class for the object_tree database table.
  * 
  */
 @Entity(name = "fr.proline.core.orm.uds.ObjectTree")
-public class ObjectTree extends AbstractObjectTree {
+@Table(name = "object_tree")
+public class ObjectTree implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    public ObjectTree() {}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
+	@Column(name = "blob_data")
+	private byte[] blobData;
+
+	@Column(name = "clob_data")
+	private String clobData;
+
+	@Column(name = "serialized_properties")
+	private String serializedProperties;
+
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "schema_name")
+	private ObjectTreeSchema schema;
+
+    public ObjectTree() {
+    }
+    
+	public long getId() {
+		return id;
+	}
+
+	public void setId(final long pId) {
+		id = pId;
+	}
+
+	public byte[] getBlobData() {
+		return this.blobData;
+	}
+
+	public void setBlobData(byte[] blobData) {
+		this.blobData = blobData;
+	}
+
+	public String getClobData() {
+		return clobData;
+	}
+
+	public void setClobData(String clobData) {
+		this.clobData = clobData;
+	}
+
+	public String getSerializedProperties() {
+		return serializedProperties;
+	}
+
+	public void setSerializedProperties(String serializedProperties) {
+		this.serializedProperties = serializedProperties;
+	}
+
+	public ObjectTreeSchema getSchema() {
+		return schema;
+	}
+
+	public void setSchema(ObjectTreeSchema schema) {
+		this.schema = schema;
+	}
+
+    
 }
