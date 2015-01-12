a15a5ed5dd6195dc16df2b9767db2297bc7f9cf0
David Bouyssié <David.Bouyssie@ipbs.fr>
2015-01-12 15:23:29 +0000

    Add Scoring.Type enumeration in Proline-ORM (this should be a replacement of the corresponding DevTools config file)
    Former-commit-id: b6b22230f4c48606aff5fd310e1f1d7022f94fa0

--- Proline-ORM/src/main/java/fr/proline/core/orm/msi/Scoring.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/msi/Scoring.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 
+import fr.profi.util.StringUtils;
+
 /**
  * The persistent class for the scoring database table.
  * 
@@ -76,5 +78,76 @@ public class Scoring implements Serializable {
     public void setSerializedProperties(String serializedProperties) {
 	this.serializedProperties = serializedProperties;
     }
+    
+    // TODO: use this enumeration in DevTools instead of the scoring.conf file
+    // TODO: change the hibernate definition of the class Scoring to use this enum ???
+	public enum Type {
+
+		MASCOT_IONS_SCORE(
+			"mascot", "ions score",
+			"The score provided for each Mascot peptide."
+		),
+		MASCOT_STANDARD_SCORE(
+			"mascot", "standard score",
+			"The score provided for each Mascot protein hit (it corresponds to the sum of ion scores)."
+		),
+		MASCOT_MUDPIT_SCORE(
+			"mascot", "mudpit score",
+			"The score provided for each Mascot protein hit when the number of MS/MS queries is high."
+		),
+		MASCOT_MODIFIED_MUDPIT_SCORE(
+			"mascot", "modified mudpit score",
+			"A modified version of the MudPIT score computed by Proline."
+		),
+		OMSSA_EVALUE(
+			"omssa", "expect value",
+			"The -log(E-value) provided by OMSSA for a peptide match."
+		),
+		COMET_EVALUE_LOG_SCALED(
+			"comet", "evalue log scaled",
+			"The -log(expectation value) provided by Comet for a peptide match."
+		),
+		MSGF_EVALUE_LOG_SCALED(
+			"msgf", "evalue log scaled",
+			"The -log(EValue) provided by MS-GF for a peptide match."
+		),
+		SEQUEST_EXPECT_LOG_SCALED(
+			"sequest", "expect log scaled",
+			"The -log(expect) provided by Sequest for a peptide match."
+		);
+
+		private final String m_searchEngine;
+		private final String m_name;
+		private final String m_description;
+
+		private Type(final String searchEngine, final String name, final String description) {
+			assert (!StringUtils.isEmpty(searchEngine)) : "Scoring.Type() invalid searchEngine";
+			assert (!StringUtils.isEmpty(name)) : "Scoring.Type() invalid name";
+			assert (!StringUtils.isEmpty(description)) : "Scoring.Type() invalid description";
+
+			m_searchEngine = searchEngine;
+			m_name = name;
+			m_description = description;
+		}
+
+		public String getSearchEngine() {
+			return m_searchEngine;
+		}
+
+		public String getName() {
+			return m_name;
+		}
+		
+		public String getDescription() {
+			return m_description;
+		}
+		
+		@Override
+		public String toString() {
+			return m_searchEngine + ":" + m_name;
+		}
+
+	}
+
 
 }
