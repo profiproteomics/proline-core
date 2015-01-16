2bb5696fb5ff032385934aec98b01537e2be5c5b
David Bouyssié <David.Bouyssie@ipbs.fr>
2015-01-16 16:00:07 +0000

    Fix the raw file name extraction (be sure all extensions are removed)
    Former-commit-id: f7276e6a4c809ab309e2b03a3088d4c5a92909bb

--- Proline-OMP/src/main/scala/fr/proline/core/service/lcms/io/ExtractMapSet.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/service/lcms/io/ExtractMapSet.scala
@@ -360,7 +360,7 @@ class ExtractMapSet(
     val mzDbFileDir = mzDbFile.getParent()
     val mzDbFileName = mzDbFile.getName()
     // FIXME: it should be retrieved from the mzDB file meta-data
-    val rawFileName = mzDbFileName.take(mzDbFileName.lastIndexOf("."))
+    val rawFileName = mzDbFileName.split(".").head
     
     // Check if the scan sequence already exists
     //val scanSeqId = lcmsDbHelper.getScanSequenceIdForRawFileName(rawFileName)
