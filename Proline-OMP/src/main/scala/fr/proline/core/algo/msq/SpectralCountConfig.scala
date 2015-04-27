0c7cd09a6770c692af8f1904f2460744f8d9ed9e
Veronique Dupierris <veronique.dupierris@cea.fr>
2015-04-27 08:11:25 +0000

    Issue #11209 : Change SC Config / Object to allow IDF weight reference definition
    Former-commit-id: af8ee2cd9be0af7f4927effad76d2fdead5183c4

--- Proline-OMP/src/main/scala/fr/proline/core/algo/msq/SpectralCountConfig.scala
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msq/SpectralCountConfig.scala
@@ -1,7 +1,29 @@
 package fr.proline.core.algo.msq
 
+import fr.proline.core.om.model.msi.ProteinSet
+
 case class SpectralCountConfig (
 	parentRSMId : Option[Long],
-	parentDSId : Option[Long]	
+	parentDSId : Option[Long],
+	weightRefRSMIds : Seq[Long] = Seq.empty[Long]
 )
 
+
+
+/**
+ * ProteinSet description for spectralCount calculation
+ */
+case class ProteinSetSCDescription(
+			val proteinSet: ProteinSet, 
+			val typicalPMAcc: String, 
+			val refRSMPeptidesInfo: PeptidesSCDescription, 
+			var peptideInfoByRSMId : Map[Long, PeptidesSCDescription] = Map.empty[Long, PeptidesSCDescription]
+			)
+		
+/**
+ * Peptide information for spectralCount calculation
+ */
+case class PeptidesSCDescription(		
+			var pepSpecificIds: Seq[Long], 
+			var nbrPSMSpecific: Int, 
+			var weightByPeptideId: scala.collection.mutable.Map[Long, Float] = null)
