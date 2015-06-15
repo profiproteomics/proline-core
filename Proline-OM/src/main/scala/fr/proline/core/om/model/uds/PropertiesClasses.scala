a96942ecbcafe6f14596a1c3da971d9ae94250f6
David Bouyssié <David.Bouyssie@ipbs.fr>
2015-06-15 15:33:37 +0000

    Remove duplicated case class
    Former-commit-id: 5ceb58f8d163ea7e46c343c3660644e03322a2ba

--- Proline-OM/src/main/scala/fr/proline/core/om/model/uds/PropertiesClasses.scala
+++ Proline-OM/src/main/scala/fr/proline/core/om/model/uds/PropertiesClasses.scala
@@ -6,7 +6,3 @@ case class ExternalDbProperties(
   @BeanProperty var driverType: Option[String] = None,
   @BeanProperty var hibernateDialect: Option[String] = None
 )
-
-case class RawFileProperties(
-  @BeanProperty var mzdbFilePath: Option[String] = None
-)
