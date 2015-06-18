2880094b9b756b6c0ab267c6328dee95e25f883c
Veronique Dupierris <veronique.dupierris@cea.fr>
2015-06-18 07:24:52 +0000

    #12765 Add all scoring in migration script (used also to create new MSI)
    Former-commit-id: 26629599ce7315036a6cce8d5f1eb8094962cf6a

--- Proline-Databases/src/main/resources/dbscripts/msi/postgresql/V0_8__core_0.4.0.sql
+++ Proline-Databases/src/main/resources/dbscripts/msi/postgresql/V0_8__core_0.4.0.sql
@@ -125,10 +125,26 @@ ALTER TABLE peptide_set ALTER COLUMN sequence_count SET NOT NULL;
 UPDATE peaklist SET raw_file_identifier = split_part(raw_file_identifier,'.', 1);
 
 DELETE FROM scoring WHERE search_engine = 'percolator';
-INSERT INTO scoring (search_engine,name,description) VALUES ('comet', 'evalue log scaled', 'The -log(expectation value) provided by Comet for a peptide match.');
-INSERT INTO scoring (search_engine,name,description) VALUES ('msgf', 'evalue log scaled', 'The -log(EValue) provided by MS-GF for a peptide match.');
-INSERT INTO scoring (search_engine,name,description) VALUES ('sequest', 'expect log scaled', 'The -log(expect) provided by Sequest for a peptide match.');
-INSERT INTO scoring (search_engine,name,description) VALUES ('xtandem', 'hyperscore', 'The hyperscore provided by X!Tandem for a peptide match.');
+-- Add Missing scoring 
+WITH all_values (search_engine,name,description) as (
+  values 
+  ('mascot', 'ions score', 'The score provided for each Mascot peptide.'),
+  ('mascot', 'standard score', 'The score provided for each Mascot protein hit (it corresponds to the sum of ion scores).'),
+  ('mascot', 'mudpit score', 'The score provided for each Mascot protein hit when the number of MS/MS queries is high.'),
+  ('mascot', 'modified mudpit score', 'A modified version of the MudPIT score computed by Proline.'),
+  ('omssa', 'expect value', 'The -log(E-value) provided by OMSSA for a peptide match.'), 
+  ('comet', 'evalue log scaled', 'The -log(expectation value) provided by Comet for a peptide match.'),
+  ('msgf', 'evalue log scaled', 'The -log(EValue) provided by MS-GF for a peptide match.'),
+  ('sequest', 'expect log scaled', 'The -log(expect) provided by Sequest for a peptide match.'),
+  ('xtandem', 'hyperscore', 'The hyperscore provided by X!Tandem for a peptide match.')
+)
+
+INSERT INTO scoring (search_engine,name,description)
+SELECT search_engine,name,description
+FROM all_values
+WHERE NOT EXISTS (SELECT 1 
+                  FROM scoring sc 
+                  WHERE sc.name = all_values.name AND sc.search_engine = all_values.search_engine)
 
 /* END OF ADDITIONAL SQL QUERIES USED FOR DATA UPDATE */
 
