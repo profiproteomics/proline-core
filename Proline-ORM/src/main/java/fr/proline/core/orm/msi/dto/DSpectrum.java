3d3839130969fe50cb54a5c94d3cabc036b47203
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-10-13 07:30:06 +0000

    first version of dto DSpectrum (used by Proline Studio)
    Former-commit-id: 06112aa927ee3d663174a3fc07227fed5558af1a

---
+++ Proline-ORM/src/main/java/fr/proline/core/orm/msi/dto/DSpectrum.java
@@ -0,0 +1,113 @@
+package fr.proline.core.orm.msi.dto;
+
+
+public class DSpectrum {
+
+    private long m_id;
+
+    private Integer m_firstScan;
+
+    private Float m_firstTime;
+    private Float m_lastTime;
+
+    private byte[] m_intensityList = null;
+    private byte[] m_mozList = null;
+
+    private Integer m_precursorCharge;
+    private Float m_precursorIntensity;
+    private Double m_precursorMoz;
+
+
+    private String m_title = null;
+
+    public DSpectrum() {
+    }
+
+    public long getId() {
+    	return m_id;
+    }
+
+    public void setId(long id) {
+    	m_id = id;
+    }
+
+
+    public Integer getFirstScan() {
+    	return m_firstScan;
+    }
+
+    public void setFirstScan(Integer firstScan) {
+    	m_firstScan = firstScan;
+    }
+
+    public Float getFirstTime() {
+    	return m_firstTime;
+    }
+
+    public void setFirstTime(Float firstTime) {
+    	m_firstTime = firstTime;
+    }
+
+
+    public byte[] getIntensityList() {
+    	return m_intensityList;
+    }
+
+    public void setIntensityList(byte[] intensityList) {
+    	m_intensityList = intensityList;
+    }
+
+
+
+    public Float getLastTime() {
+    	return m_lastTime;
+    }
+
+    public void setLastTime(Float lastTime) {
+    	m_lastTime = lastTime;
+    }
+
+    public byte[] getMozList() {
+    	return m_mozList;
+    }
+
+    public void setMozList(byte[] mozList) {
+    	m_mozList = mozList;
+    }
+
+
+    public Integer getPrecursorCharge() {
+    	return m_precursorCharge;
+    }
+
+    public void setPrecursorCharge(Integer precursorCharge) {
+    	m_precursorCharge = precursorCharge;
+    }
+
+    public Float getPrecursorIntensity() {
+    	return m_precursorIntensity;
+    }
+
+    public void setPrecursorIntensity(Float precursorIntensity) {
+    	m_precursorIntensity = precursorIntensity;
+    }
+
+    public Double getPrecursorMoz() {
+    	return m_precursorMoz;
+    }
+
+    public void setPrecursorMoz(final Double precursorMoz) {
+    	m_precursorMoz = precursorMoz;
+    }
+
+
+    public String getTitle() {
+    	return m_title;
+    }
+
+    public void setTitle(String title) {
+    	m_title = title;
+    }
+
+
+}
