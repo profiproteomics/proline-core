122d3d77549117a915c166aac7ca4e47449a3a44
Christophe Bruley <christophe.bruley@cea.fr>
2015-12-17 14:46:57 +0000

    - scoring records added in the H2 migration file. No more needed in init_dataset.xml file.
    Former-commit-id: 52dd94b7b869187686d80d54dccd403b91ca8bd9

--- Proline-Databases/src/main/resources/dbscripts/msi/h2/V0_8__core_0.4.0.sql
+++ Proline-Databases/src/main/resources/dbscripts/msi/h2/V0_8__core_0.4.0.sql
@@ -91,3 +91,15 @@ ON DELETE CASCADE
 ON UPDATE NO ACTION;
 
 /* END ADDITIONAL SQL QUERIES FIXING THE "ON DELETE CASCADE" CONSTRAINTS" */
+
+/* ADDITIONAL SQL QUERIES USED FOR DATA UPDATE */
+
+INSERT INTO scoring(search_engine,name,description) values ('mascot', 'ions score', 'The score provided for each Mascot peptide.'),
+  ('mascot', 'standard score', 'The score provided for each Mascot protein hit (it corresponds to the sum of ion scores).'),
+  ('mascot', 'mudpit score', 'The score provided for each Mascot protein hit when the number of MS/MS queries is high.'),
+  ('mascot', 'modified mudpit score', 'A modified version of the MudPIT score computed by Proline.'),
+  ('omssa', 'expect value', 'The -log(E-value) provided by OMSSA for a peptide match.'), 
+  ('comet', 'evalue log scaled', 'The -log(expectation value) provided by Comet for a peptide match.'),
+  ('msgf', 'evalue log scaled', 'The -log(EValue) provided by MS-GF for a peptide match.'),
+  ('sequest', 'expect log scaled', 'The -log(expect) provided by Sequest for a peptide match.'),
+  ('xtandem', 'hyperscore', 'The hyperscore provided by X!Tandem for a peptide match.');
