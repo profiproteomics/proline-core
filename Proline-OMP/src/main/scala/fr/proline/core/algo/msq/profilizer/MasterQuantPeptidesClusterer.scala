90ba3ac329395c232b9e84d95ff2d2218c3c1f4e
David Bouyssié <David.Bouyssie@ipbs.fr>
2016-02-17 12:45:51 +0000

    Implement different algorithms of Master Quant peptides clustering
    Former-commit-id: 9b1bd97125aad2f36cfa61427e739dc4026ce879

---
+++ Proline-OMP/src/main/scala/fr/proline/core/algo/msq/profilizer/MasterQuantPeptidesClusterer.scala
@@ -0,0 +1,299 @@
+package fr.proline.core.algo.msq.profilizer
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+import fr.profi.util.lang.EnhancedEnum
+import fr.proline.core.om.model.msi._
+import fr.proline.core.om.model.msq.MasterQuantPeptide
+import fr.proline.core.om.model.msq.MasterQuantProteinSet
+
+object MqPeptidesClusteringMethod extends EnhancedEnum {
+  val PEPTIDE_SEQUENCE = Value // Cluster name = SEQUENCE
+  val PEPTIDE_SET = Value // Cluster name = PROTEIN ACCESSION
+  val PTM_PATTERN = Value // Cluster name = MODIFIED/UNMODIFIED LOCATED PTM IN PROTEIN SEQUENCE AND ACCESSION FOR OTHERS
+  val QUANT_PROFILE = Value // Cluster name = RATIO STATES
+}
+
+case class MqPeptidesClustererConfig(
+  ptmPatternPtmDefIds: Seq[Long] = Seq() // only for PTM_PATTERN method
+)
+
+case class MasterQuantPeptidesCluster(
+  name: String,
+  mqProteinSet: MasterQuantProteinSet,
+  mqPeptides: Seq[MasterQuantPeptide]
+)
+
+trait IMqPeptidesClusterer {
+
+  def clustererConfig: MqPeptidesClustererConfig
+
+  def computeMqPeptidesClusters(
+    masterQuantProtSets: Seq[MasterQuantProteinSet]
+  ): Array[MasterQuantPeptidesCluster] = {
+
+    val clustersBuffer = new ArrayBuffer[MasterQuantPeptidesCluster](masterQuantProtSets.length)
+    for (masterQuantProtSet <- masterQuantProtSets) {
+
+      // Reset quant profiles for this masterQuantProtSet
+      // TODO: is this needed ???
+      /*for (masterQuantProtSetProps <- masterQuantProtSet.properties) {
+        masterQuantProtSetProps.setMqProtSetProfilesByGroupSetupNumber(None)
+      }*/
+      
+      // Retrieve selected master quant peptides in this protein set
+      val selMqPepIdSet = masterQuantProtSet.properties.get.selectedMasterQuantPeptideIds.map(_.toSet).getOrElse(Set())
+      
+      // Keep only selected master quant peptides
+      val selectedMqPeps = masterQuantProtSet.masterQuantPeptides.filter { mqPep =>
+        val isSelectedInDataset = mqPep.selectionLevel >= 2
+        val isSelectedInProtSet = selMqPepIdSet.contains(mqPep.id)
+        
+        ( isSelectedInDataset && isSelectedInProtSet )
+      }
+
+      // Compute clusters if we have at least one selected peptide
+      if( selectedMqPeps.isEmpty == false ) {
+        this.computeMqPeptidesClusters(masterQuantProtSet, selectedMqPeps, clustersBuffer)
+      }
+      
+    }
+
+    clustersBuffer.toArray
+  }
+
+  protected def computeMqPeptidesClusters(
+    masterQuantProtSet: MasterQuantProteinSet,
+    selectedMqPeps: Array[MasterQuantPeptide],
+    clustersBuffer: ArrayBuffer[MasterQuantPeptidesCluster]
+  ): Unit
+
+  protected def getRepresentativeProteinMatch(protSet: ProteinSet): ProteinMatch = {
+
+    val samesetProteinMatchesOpt = protSet.samesetProteinMatches
+    require(
+      samesetProteinMatchesOpt != null && samesetProteinMatchesOpt.isDefined,
+      "the protein matches must be linked to the protein set"
+    )
+
+    val reprProtMatchOpt = protSet.getRepresentativeProteinMatch()
+    val protMatch = Option(reprProtMatchOpt).flatten.getOrElse(samesetProteinMatchesOpt.get.head)
+
+    protMatch
+  }
+
+}
+
+object MasterQuantPeptidesClusterer {
+
+  def apply(
+    method: MqPeptidesClusteringMethod.Value,
+    clustererConfig: MqPeptidesClustererConfig,
+    groupSetupNumber: Int
+  ): IMqPeptidesClusterer = {
+
+    import MqPeptidesClusteringMethod._
+
+    val clusters = method match {
+      case PEPTIDE_SEQUENCE => new PeptideSequenceBasedClusterer()
+      case PEPTIDE_SET      => new PeptideSetBasedClusterer()
+      case PTM_PATTERN      => new PtmPatternBasedClusterer(clustererConfig)
+      case QUANT_PROFILE    => new QuantProfileBasedClusterer(groupSetupNumber)
+    }
+
+    clusters
+  }
+
+  def computeMqPeptidesClusters(
+    method: MqPeptidesClusteringMethod.Value,
+    clustererConfig: MqPeptidesClustererConfig,
+    groupSetupNumber: Int,
+    masterQuantProtSets: Seq[MasterQuantProteinSet]
+  ): Array[MasterQuantPeptidesCluster] = {
+    this.apply(method, clustererConfig, groupSetupNumber).computeMqPeptidesClusters(masterQuantProtSets)
+  }
+}
+
+class PeptideSequenceBasedClusterer() extends IMqPeptidesClusterer {
+
+  val clustererConfig = MqPeptidesClustererConfig()
+  
+  def computeMqPeptidesClusters(
+    masterQuantProtSet: MasterQuantProteinSet,
+    selectedMqPeps: Array[MasterQuantPeptide],
+    clustersBuffer: ArrayBuffer[MasterQuantPeptidesCluster]
+  ): Unit = {
+
+    // Group master quant peptides by sequence
+    val mqPepsBySameSeq = selectedMqPeps.groupBy(_.peptideInstance.map(_.peptide.sequence).getOrElse(""))
+
+    for ((pepSeq, mqPeps) <- mqPepsBySameSeq) {
+      clustersBuffer += MasterQuantPeptidesCluster(
+        name = pepSeq,
+        mqProteinSet = masterQuantProtSet,
+        mqPeptides = mqPeps
+      )
+    }
+
+    ()
+  }
+
+}
+
+/** Clustering based on identified peptide set for each protein set **/
+class PeptideSetBasedClusterer() extends IMqPeptidesClusterer {
+
+  val clustererConfig = MqPeptidesClustererConfig()
+  
+  def computeMqPeptidesClusters(
+    masterQuantProtSet: MasterQuantProteinSet,
+    selectedMqPeps: Array[MasterQuantPeptide],
+    clustersBuffer: ArrayBuffer[MasterQuantPeptidesCluster]
+  ): Unit = {
+
+    val reprProtMatch = this.getRepresentativeProteinMatch(masterQuantProtSet.proteinSet)
+
+    clustersBuffer += MasterQuantPeptidesCluster(
+      name = reprProtMatch.accession,
+      mqProteinSet = masterQuantProtSet,
+      mqPeptides = selectedMqPeps
+    )
+
+    ()
+  }
+
+}
+
+/** The goal of this clusterer is to group Master Quant Peptides having the same PTM pattern **/
+class PtmPatternBasedClusterer(val clustererConfig: MqPeptidesClustererConfig) extends IMqPeptidesClusterer {
+  
+  private val ptmDefIdSet = clustererConfig.ptmPatternPtmDefIds.toSet
+
+  def computeMqPeptidesClusters(
+    masterQuantProtSet: MasterQuantProteinSet,
+    selectedMqPeps: Array[MasterQuantPeptide],
+    clustersBuffer: ArrayBuffer[MasterQuantPeptidesCluster]
+  ): Unit = {
+
+    val protSet = masterQuantProtSet.proteinSet
+    val reprProtMatch = this.getRepresentativeProteinMatch(protSet)
+    val seqMatchesByPepId = reprProtMatch.sequenceMatches.groupBy( sm => sm.getPeptideId )
+    
+    // Group master quant peptides by PTM pattern
+    val mqPepsByPtmPattern = selectedMqPeps.groupBy { mqPep =>
+      
+      val peptide = mqPep.peptideInstance.get.peptide
+      val seqMatches = seqMatchesByPepId(peptide.id)
+      
+      this._buildPtmPattern(peptide, seqMatches.map(_.start) )
+    }
+    
+    // Search for master quant peptides which do not have a PTM
+    val otherMqPeps = mqPepsByPtmPattern.find(_._1.isEmpty).map(_._2).getOrElse(Array())
+    val otherMqPepBySeq = otherMqPeps.view.map { otherMqPep =>
+      otherMqPep.peptideInstance.get.peptide.sequence -> otherMqPep
+    } toMap
+    
+    val unmodifiedMqPeps = new ArrayBuffer[MasterQuantPeptide](otherMqPeps.length)
+    
+    // Add master quant peptides associtedto a PTM pattern to the clusters buffer
+    for ((ptmPattern, mqPeps) <- mqPepsByPtmPattern.filter(_._1.isEmpty == false) ) {
+      
+      clustersBuffer += MasterQuantPeptidesCluster(
+        name = "MODIFIED " + ptmPattern,
+        mqProteinSet = masterQuantProtSet,
+        mqPeptides = mqPeps
+      )
+      
+      // Add peptide with same sequence but no PTM to a cluster corresponding to the UNMODIFIED form of the pattern
+      for( unmodifiedMqPep <- otherMqPepBySeq.get(mqPeps.head.peptideInstance.get.peptide.sequence) ) {
+        unmodifiedMqPeps += unmodifiedMqPep
+        
+        clustersBuffer += MasterQuantPeptidesCluster(
+          name = "UNMODIFIED " + ptmPattern,
+          mqProteinSet = masterQuantProtSet,
+          mqPeptides = Array(unmodifiedMqPep)
+        )
+      }
+    }
+
+    // Add all other peptides to a "PROTEIN" cluster
+    val unmodifiedMqPepSet = unmodifiedMqPeps.toSet
+    for( otherMqPep <- otherMqPeps; if unmodifiedMqPepSet.contains(otherMqPep) == false ) {
+      clustersBuffer += MasterQuantPeptidesCluster(
+        name = reprProtMatch.accession,
+        mqProteinSet = masterQuantProtSet,
+        mqPeptides = Array(otherMqPep)
+      )
+    }
+    
+    ()
+  }
+  
+  private def _buildPtmPattern(peptide: Peptide, proteinStartLocations: Seq[Int] ): String = {
+    
+    val ptms = peptide.ptms
+    
+    val patterns = for (
+      ptm <- ptms.sortBy(_.seqPosition);
+      if ptmDefIdSet.contains(ptm.definition.id)
+    ) yield {
+      
+      val shortName = ptm.definition.names.shortName
+      
+      val peptidePtmLocation = if (ptm.isNTerm) 1
+      else if (ptm.isCTerm) peptide.sequence.length
+      else ptm.seqPosition
+      
+      val proteinPtmLocations = proteinStartLocations.map( start => start + peptidePtmLocation - 1 )
+     
+      s"${shortName}@${proteinPtmLocations.mkString(",")}"
+    }
+
+    patterns.mkString("; ")
+  }
+
+}
+
+/** Clustering based on similarities between profiles of quantified peptides **/
+class QuantProfileBasedClusterer(groupSetupNumber: Int) extends IMqPeptidesClusterer {
+
+  val clustererConfig = MqPeptidesClustererConfig()
+  
+  def computeMqPeptidesClusters(
+    masterQuantProtSet: MasterQuantProteinSet,
+    selectedMqPeps: Array[MasterQuantPeptide],
+    clustersBuffer: ArrayBuffer[MasterQuantPeptidesCluster]
+  ): Unit = {
+
+    // Clusterize MasterQuantPeptides according to their profile
+    val mqPepsByProfileAsStr = new HashMap[String, ArrayBuffer[MasterQuantPeptide]]()
+
+    selectedMqPeps.foreach { mqPep =>
+
+      val mqPepProfileOpt = mqPep.properties.get.getMqPepProfileByGroupSetupNumber.get.get(groupSetupNumber)
+
+      for (mqPepProfile <- mqPepProfileOpt) {
+
+        val profileSlopes = mqPepProfile.ratios.map(_.map(_.state).get)
+        val profileAsStr = _stringifyProfile(profileSlopes)
+
+        mqPepsByProfileAsStr.getOrElseUpdate(profileAsStr, new ArrayBuffer[MasterQuantPeptide]) += mqPep
+      }
+    }
+
+    // Build master quant protein set profiles
+    for ((profileAsStr, mqPeps) <- mqPepsByProfileAsStr) {
+      clustersBuffer += MasterQuantPeptidesCluster(
+        name = profileAsStr,
+        mqProteinSet = masterQuantProtSet,
+        mqPeptides = mqPeps
+      )
+    }
+
+    ()
+  }
+
+  private def _stringifyProfile(slopes: Seq[Int]): String = { slopes.mkString(";") }
+
+}
---
+++ Proline-OMP/src/test/scala/fr/proline/core/algo/msq/profilizer/MasterQuantPeptidesClustererTest.scala
@@ -0,0 +1,222 @@
+package fr.proline.core.algo.msq.profilizer
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+import org.junit.Assert._
+import org.junit.Test
+import org.scalatest.Assertions._
+import fr.proline.core.om.model.msi._
+import fr.proline.core.om.model.msq._
+
+@Test
+class MasterQuantPeptidesClustererTest {
+  
+  val phosphoSerinePtm = PtmDefinition(
+    id = PtmDefinition.generateNewId(),
+    location = PtmLocation.ANYWHERE.toString,
+    names = PtmNames("Phosphorylation","Phosphorylation"),
+    ptmEvidences = Array[PtmEvidence](
+      PtmEvidence(
+        ionType = IonTypes.Precursor,
+        composition = "H3PO4",
+        monoMass = 80.0,
+        averageMass = 80.0
+      )   
+    ),
+  
+    residue = 'S'
+  )
+  
+  val peptides = Array(
+    new Peptide(
+      sequence = "EGVVGAVEK",
+      ptms = Array()
+    ),
+    new Peptide(
+      sequence = "ENVVQSVTSVAEK",
+      ptms = Array()
+    ),
+    new Peptide(
+      sequence = "ENVVQSVTSVAEK",
+      ptms = Array(
+        new LocatedPtm(
+          definition = phosphoSerinePtm,
+          seqPosition = 6,
+          precursorDelta = phosphoSerinePtm.precursorDelta
+        )
+      )
+    ),
+    new Peptide(
+      sequence = "TKENVVQSVTSVAEK",
+      ptms = Array(
+        new LocatedPtm(
+          definition = phosphoSerinePtm,
+          seqPosition = 8,
+          precursorDelta = phosphoSerinePtm.precursorDelta
+        )
+      )
+    )
+  )
+  
+  val ir = ComputedRatio (
+    numerator = 1f,
+    denominator = 1f,
+    state = 0,
+    tTestPValue = None,
+    zTestPValue = None,
+    zScore = None
+  )
+  val vr = ir.copy(  numerator = 2f, state = 1 )
+  val computedRatios = List(ir,ir,vr,vr)
+  val computedRatioByPeptide = peptides.zip(computedRatios).toMap
+
+  val proteinMatch = ProteinMatch(
+    id = ProteinMatch.generateNewId(),
+    accession = "PPIA_HUMAN",
+    description = "Peptidyl-prolyl cis-trans isomerase A (Chain 1-165, N terminal His tag)- Homo sapiens (Human)",
+    sequenceMatches = Array(
+      SequenceMatch(
+        start = 1,
+        end = peptides(0).sequence.length,
+        residueBefore = '\0',
+        residueAfter = 'T',
+        peptide = Some(peptides(0))
+      ),
+      SequenceMatch(
+        start = peptides(0).sequence.length + 3,
+        end = 2 + peptides(0).sequence.length + peptides(1).sequence.length,
+        residueBefore = 'K',
+        residueAfter = 'A',
+        peptide = Some(peptides(1))
+      ),
+      SequenceMatch(
+        start = peptides(0).sequence.length + 3,
+        end = 2 + peptides(0).sequence.length + peptides(2).sequence.length,
+        residueBefore = 'K',
+        residueAfter = 'A',
+        peptide = Some(peptides(2))
+      ),
+      SequenceMatch(
+        start = peptides(0).sequence.length + 1,
+        end = peptides(0).sequence.length + peptides(3).sequence.length,
+        residueBefore = 'K',
+        residueAfter = 'A',
+        peptide = Some(peptides(3))
+      )
+    )
+  )
+
+  val mqProtSetId = ProteinSet.generateNewId()
+  val mqPep = MasterQuantPeptide(
+    id = 0,
+    peptideInstance = Option.empty[PeptideInstance],
+    quantPeptideMap = Map.empty[Long, QuantPeptide],
+    masterQuantPeptideIons = Array.empty[MasterQuantPeptideIon],
+
+    selectionLevel = 2,
+    resultSummaryId = -1L
+  )
+  
+  def buildMqPep( peptide: Peptide ) = {
+    mqPep.copy(
+      id = MasterQuantPeptide.generateNewId(),
+      peptideInstance = Some(
+        PeptideInstance(
+          id = PeptideInstance.generateNewId(),
+          peptide = peptide,
+          peptideMatchIds = Array(0)
+        )
+      ),
+      properties = Some(
+        MasterQuantPeptideProperties(
+          mqProtSetIds = Some(Array(mqProtSetId)),
+          mqPepProfileByGroupSetupNumber = HashMap[Int, MasterQuantPeptideProfile](
+            1 -> MasterQuantPeptideProfile(List(Some(computedRatioByPeptide(peptide))))
+          )
+        )
+      )
+    )
+  }
+  
+  val masterQuantPeptides = peptides.map( buildMqPep(_) )
+  
+  val masterQuantProtSet = MasterQuantProteinSet(
+    proteinSet = ProteinSet(
+      id = mqProtSetId,
+      samesetProteinMatchIds = Array(proteinMatch.id),
+      samesetProteinMatches = Some(Array(proteinMatch)),
+      peptideSet = PeptideSet(
+        id = PeptideSet.generateNewId(),
+        items = Array.empty[PeptideSetItem],
+        isSubset = false,
+        sequencesCount = 1,
+        peptideMatchesCount = 1,
+        proteinMatchIds = Array(proteinMatch.id)
+      ),
+      hasPeptideSubset = false,
+      isDecoy = false
+    ),
+    quantProteinSetMap = Map.empty[Long,QuantProteinSet], // QuantProteinSet by quant channel id
+    masterQuantPeptides = masterQuantPeptides,
+    
+    selectionLevel = 2,
+    properties = Some(MasterQuantProteinSetProperties( selectedMasterQuantPeptideIds = Some(masterQuantPeptides.map(_.id)) ))
+  )
+  
+  @Test
+  def testPeptideSequenceClustering {
+    val clusterer = new PeptideSequenceBasedClusterer()
+    val clusters = clusterer.computeMqPeptidesClusters(Array(masterQuantProtSet))
+    
+    assert( clusters.length === 3, " => expecting 3 different clusters" )
+    
+    /*clusters.foreach { cluster =>
+      println(cluster.name, cluster.mqPeptides.map { mqPep =>
+        val p = mqPep.peptideInstance.get.peptide
+        p.sequence + " " + p.readablePtmString
+      }.toList)
+    }*/
+  }
+  
+  @Test
+  def testPeptideSetClustering {
+    val clusterer = new PeptideSetBasedClusterer()    
+    val clusters = clusterer.computeMqPeptidesClusters(Array(masterQuantProtSet))
+    
+    assert( clusters.length === 1, " => expecting a single cluster" )
+  }
+  
+  @Test
+  def testPtmPatternClustering {
+    val clusterer = new PtmPatternBasedClusterer(MqPeptidesClustererConfig(ptmPatternPtmDefIds = Seq(phosphoSerinePtm.id) ))
+    val clusters = clusterer.computeMqPeptidesClusters(Array(masterQuantProtSet))
+    
+    assert( clusters.length === 3, " => expecting 3 different clusters" )
+    assert( clusters.find(_.name == "PPIA_HUMAN").isDefined === true, " => 'PPIA_HUMAN' cluster is missing" )
+    assert( clusters.find(_.name == "MODIFIED Phosphorylation@17").isDefined === true, " => 'MODIFIED Phosphorylation@17' cluster is missing" )
+    assert( clusters.find(_.name == "UNMODIFIED Phosphorylation@17").isDefined === true, " => 'UNMODIFIED Phosphorylation@17' cluster is missing" )
+    
+    /*clusters.foreach { cluster =>
+      println(cluster.name, cluster.mqPeptides.map { mqPep =>
+        val p = mqPep.peptideInstance.get.peptide
+        p.sequence + " " + p.readablePtmString
+      }.toList)
+    }*/
+  }
+  
+  @Test
+  def testQuantProfileClustering {
+    val clusterer = new QuantProfileBasedClusterer(groupSetupNumber = 1)
+    val clusters = clusterer.computeMqPeptidesClusters(Array(masterQuantProtSet))
+
+    assert(clusters.length === 2, " => expecting two different clusters")
+
+    clusters.foreach { cluster =>
+      println(cluster.name, cluster.mqPeptides.map { mqPep =>
+        val p = mqPep.peptideInstance.get.peptide
+        p.sequence + " " + p.readablePtmString
+      }.toList)
+    }
+  }
+  
+}
