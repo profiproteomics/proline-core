a06366b3b875dd72b2f76607172590043634fba3
David Bouyssié <David.Bouyssie@ipbs.fr>
2016-05-23 19:58:56 +0000

    Fix a bug in the SQLMasterQuantPeptideProvider (it may have side effects on SC and XIC quantitations data loading)
    refs #14868
    
    
    Former-commit-id: e9114fa2e2c785eadc44c02a21b2d08955720860

--- Proline-DAL/src/main/scala/fr/proline/core/om/provider/msq/impl/SQLMasterQuantPeptideProvider.scala
+++ Proline-DAL/src/main/scala/fr/proline/core/om/provider/msq/impl/SQLMasterQuantPeptideProvider.scala
@@ -14,6 +14,7 @@ import fr.proline.core.om.model.msi.PeptideInstance
 import fr.proline.core.om.model.msq._
 import fr.proline.core.om.provider.msi.impl.SQLPeptideInstanceProvider
 import fr.proline.core.om.provider.msi.impl.SQLPeptideProvider
+import fr.proline.core.orm.msi.ObjectTreeSchema.SchemaName
 import fr.proline.repository.ProlineDatabaseType
 
 class SQLMasterQuantPeptideProvider(
@@ -27,10 +28,14 @@ class SQLMasterQuantPeptideProvider(
   protected val pepInstProvider = new SQLPeptideInstanceProvider(msiDbCtx,pepProvider)
   protected val mqPepIonsProvider = new SQLMasterQuantPeptideIonProvider(msiDbCtx)
   
-  val MQComponentTable = MsiDbMasterQuantComponentTable
-  val MQCompCols = MQComponentTable.columns
-  val ObjectTreeTable = MsiDbObjectTreeTable
-  val ObjectTreeCols = ObjectTreeTable.columns
+  private val MQComponentTable = MsiDbMasterQuantComponentTable
+  private val MQCompCols = MQComponentTable.columns
+  private val ObjectTreeTable = MsiDbObjectTreeTable
+  private val ObjectTreeCols = ObjectTreeTable.columns
+  
+  private val isobaricTaggingQuantPeptidesSchema = SchemaName.ISOBARIC_TAGGING_QUANT_PEPTIDES.toString
+  private val labelFreeQuantPeptidesSchema = SchemaName.LABEL_FREE_QUANT_PEPTIDES.toString
+  private val spectralCountQuantPeptidesSchema = SchemaName.SPECTRAL_COUNTING_QUANT_PEPTIDES.toString
   
   def getMasterQuantPeptides(
     mqPepIds: Seq[Long]
@@ -127,10 +132,14 @@ class SQLMasterQuantPeptideProvider(
     quantRsmIds: Seq[Long]
   ): (IValueContainer => MasterQuantPeptide) => Seq[MasterQuantPeptide] = {
     
+    // TODO: index master_quant_component table and/or object_tree table on schema name
     val mqPepCompQueryBuilder = new SelectQueryBuilder2(MQComponentTable,ObjectTreeTable)
     val mqPepCompQuery = mqPepCompQueryBuilder.mkSelectQuery(
       (t1,c1,t2,c2) => List(t1.ID,t1.SELECTION_LEVEL,t1.RESULT_SUMMARY_ID,t1.SERIALIZED_PROPERTIES,t2.CLOB_DATA) -> 
       " WHERE "~ t1.RESULT_SUMMARY_ID ~" IN ("~ quantRsmIds.mkString(",") ~")" ~
+      " AND ( "~ t1.SCHEMA_NAME ~" = '"~ isobaricTaggingQuantPeptidesSchema ~
+      "' OR "~ t1.SCHEMA_NAME ~" = '"~ labelFreeQuantPeptidesSchema ~
+      "' OR "~ t1.SCHEMA_NAME ~" = '"~ spectralCountQuantPeptidesSchema ~"') " ~
       " AND "~ t1.OBJECT_TREE_ID ~" = "~ t2.ID
     )
      
