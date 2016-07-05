e0619313ab1879320f231321f9abd8fb293102de
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-07-05 14:46:09 +0000

    Add MAxQuant Scoring
    Former-commit-id: d20565832ca387750fcc1562fe8c4aa68e7e808c

---
+++ Proline-Databases/src/main/resources/dbscripts/msi/h2/V0_9__scoring_enum.sql
@@ -0,0 +1,6 @@
+
+/* ADDITIONAL SQL QUERIES USED FOR DATA UPDATE */
+-- Add Missing scoring 
+
+INSERT INTO scoring(search_engine,name,description) values ('maxquant','score','The score provided by MaxQuant for a peptide match.' );
+  
