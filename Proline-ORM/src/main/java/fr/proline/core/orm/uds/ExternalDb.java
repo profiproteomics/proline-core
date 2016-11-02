e4f393fa91db1cd39a0cdb137ecf82157f2ae56d
David Bouyssié <David.Bouyssie@ipbs.fr>
2016-11-02 09:25:39 +0000

    Apply Java code formatter
    Former-commit-id: a65e62d9ab9cde0e1e07c2d00258e77e2e351927

--- Proline-ORM/src/main/java/fr/proline/core/orm/uds/ExternalDb.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/uds/ExternalDb.java
@@ -34,325 +34,325 @@ import fr.profi.util.StringUtils;
  */
 @Entity
 @NamedQueries({
-	@NamedQuery(name = "findExternalDbByType", query = "select ed from fr.proline.core.orm.uds.ExternalDb ed"
-		+ " where ed.type = :type"),
+		@NamedQuery(name = "findExternalDbByType", query = "select ed from fr.proline.core.orm.uds.ExternalDb ed"
+			+ " where ed.type = :type"),
 
-	@NamedQuery(name = "findExternalDbByTypeAndProject", query = "select ed from fr.proline.core.orm.uds.ExternalDb ed"
-		+ " join ed.projects as dBProject where (ed.type = :type) and (dBProject = :project)") })
+		@NamedQuery(name = "findExternalDbByTypeAndProject", query = "select ed from fr.proline.core.orm.uds.ExternalDb ed"
+			+ " join ed.projects as dBProject where (ed.type = :type) and (dBProject = :project)") })
 @Table(name = "external_db")
 public class ExternalDb implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private static final Logger LOG = LoggerFactory.getLogger(ExternalDb.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ExternalDb.class);
 
-    private static final int URL_BUFFER_SIZE = 256;
+	private static final int URL_BUFFER_SIZE = 256;
 
-    private static final int MAX_PORT = 65535;
+	private static final int MAX_PORT = 65535;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-    @Transient
-    private DriverType driverType;
+	@Transient
+	private DriverType driverType;
 
-    @Column(name = "name")
-    private String dbName;
+	@Column(name = "name")
+	private String dbName;
 
-    @Column(name = "password")
-    private String dbPassword;
+	@Column(name = "password")
+	private String dbPassword;
 
-    @Column(name = "username")
-    private String dbUser;
+	@Column(name = "username")
+	private String dbUser;
 
-    @Column(name = "version")
-    private String dbVersion;
+	@Column(name = "version")
+	private String dbVersion;
 
-    private String host;
+	private String host;
 
-    @Column(name = "is_busy")
-    private boolean isBusy;
+	@Column(name = "is_busy")
+	private boolean isBusy;
 
-    private Integer port;
+	private Integer port;
 
-    @Column(name = "serialized_properties")
-    private String serializedProperties;
+	@Column(name = "serialized_properties")
+	private String serializedProperties;
 
-    @Enumerated(value = EnumType.STRING)
-    private ProlineDatabaseType type;
+	@Enumerated(value = EnumType.STRING)
+	private ProlineDatabaseType type;
 
-    @Column(name = "connection_mode")
-    @Enumerated(value = EnumType.STRING)
-    private ConnectionMode connectionMode;
+	@Column(name = "connection_mode")
+	@Enumerated(value = EnumType.STRING)
+	private ConnectionMode connectionMode;
 
-    // bi-directional many-to-many association to Project
-    @ManyToMany(mappedBy = "externalDatabases")
-    private Set<Project> projects;
+	// bi-directional many-to-many association to Project
+	@ManyToMany(mappedBy = "externalDatabases")
+	private Set<Project> projects;
 
-    public ExternalDb() {
-    }
-
-    public void setId(final long pId) {
-	id = pId;
-    }
-
-    public long getId() {
-	return id;
-    }
-
-    public void setDriverType(final DriverType dt) {
-	driverType = dt;
-    }
-
-    public DriverType getDriverType() {
-	return driverType;
-    }
-
-    public String getDbName() {
-	return this.dbName;
-    }
-
-    public void setDbName(String dbName) {
-	this.dbName = dbName;
-    }
-
-    public String getDbPassword() {
-	return this.dbPassword;
-    }
-
-    public void setDbPassword(String dbPassword) {
-	this.dbPassword = dbPassword;
-    }
-
-    public String getDbUser() {
-	return this.dbUser;
-    }
-
-    public void setDbUser(String dbUser) {
-	this.dbUser = dbUser;
-    }
-
-    public String getDbVersion() {
-	return this.dbVersion;
-    }
-
-    public void setDbVersion(String dbVersion) {
-	this.dbVersion = dbVersion;
-    }
-
-    public String getHost() {
-	return this.host;
-    }
-
-    public void setHost(String host) {
-	this.host = host;
-    }
-
-    public boolean getIsBusy() {
-	return isBusy;
-    }
-
-    public void setIsBusy(final boolean pIsBusy) {
-	isBusy = pIsBusy;
-    }
-
-    public Integer getPort() {
-	return this.port;
-    }
-
-    public void setPort(Integer port) {
-	this.port = port;
-    }
-
-    public String getSerializedProperties() {
-	return this.serializedProperties;
-    }
-
-    public void setSerializedProperties(String serializedProperties) {
-	this.serializedProperties = serializedProperties;
-    }
-
-    public ProlineDatabaseType getType() {
-	return type;
-    }
-
-    public void setType(final ProlineDatabaseType databaseType) {
-	type = databaseType;
-    }
-
-    public void setProjects(final Set<Project> pProjects) {
-	projects = pProjects;
-    }
-
-    public Set<Project> getProjects() {
-	return this.projects;
-    }
-
-    public void addProject(final Project project) {
-
-	if (project != null) {
-	    Set<Project> localProjects = getProjects();
-
-	    if (localProjects == null) {
-		localProjects = new HashSet<Project>();
-
-		setProjects(localProjects);
-	    }
-
-	    localProjects.add(project);
+	public ExternalDb() {
 	}
 
-    }
-
-    public void removeProject(final Project project) {
-	final Set<Project> localProjects = getProjects();
-
-	if (localProjects != null) {
-	    localProjects.remove(project);
+	public void setId(final long pId) {
+		id = pId;
 	}
 
-    }
-
-    public ConnectionMode getConnectionMode() {
-	return connectionMode;
-    }
-
-    public void setConnectionMode(final ConnectionMode connectMode) {
-	connectionMode = connectMode;
-    }
-
-    /**
-     * Retrieves a standard JDBC properties Map from this <code>ExternalDb</code>.
-     * <p>
-     * Note JDBC URL are built as a basic string, without specific URI rules for character quotation and
-     * encoding.
-     * 
-     * @return Properties Map usable to build a database connector.
-     */
-    public Map<Object, Object> toPropertiesMap() {
-	final DriverType dt = getDriverType();
-
-	if (dt == null) {
-	    throw new IllegalArgumentException("DriverType is null");
+	public long getId() {
+		return id;
 	}
 
-	final Map<Object, Object> properties = new HashMap<Object, Object>();
+	public void setDriverType(final DriverType dt) {
+		driverType = dt;
+	}
 
-	/* javax.persistence.jdbc.driver */
-	properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_DRIVER_KEY, dt.getJdbcDriver());
+	public DriverType getDriverType() {
+		return driverType;
+	}
 
-	/* Build javax.persistence.jdbc.url */
-	final StringBuilder urlBuilder = new StringBuilder(URL_BUFFER_SIZE);
+	public String getDbName() {
+		return this.dbName;
+	}
 
-	urlBuilder.append(AbstractDatabaseConnector.JDBC_SCHEME).append(':');
-	urlBuilder.append(driverType.name().toLowerCase()).append(':');
+	public void setDbName(String dbName) {
+		this.dbName = dbName;
+	}
 
-	final String databasePathname = getDbName();
+	public String getDbPassword() {
+		return this.dbPassword;
+	}
 
-	switch (getConnectionMode()) {
-	case MEMORY:
+	public void setDbPassword(String dbPassword) {
+		this.dbPassword = dbPassword;
+	}
 
-	    if (dt == DriverType.SQLITE) {
-		/* Char needed before memory => http://www.sqlite.org/inmemorydb.html */
-		urlBuilder.append(":memory:");
-	    } else {
-		urlBuilder.append("mem:");
+	public String getDbUser() {
+		return this.dbUser;
+	}
 
-		if (!StringUtils.isEmpty(databasePathname)) {
-		    urlBuilder.append(databasePathname);
+	public void setDbUser(String dbUser) {
+		this.dbUser = dbUser;
+	}
+
+	public String getDbVersion() {
+		return this.dbVersion;
+	}
+
+	public void setDbVersion(String dbVersion) {
+		this.dbVersion = dbVersion;
+	}
+
+	public String getHost() {
+		return this.host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public boolean getIsBusy() {
+		return isBusy;
+	}
+
+	public void setIsBusy(final boolean pIsBusy) {
+		isBusy = pIsBusy;
+	}
+
+	public Integer getPort() {
+		return this.port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+
+	public String getSerializedProperties() {
+		return this.serializedProperties;
+	}
+
+	public void setSerializedProperties(String serializedProperties) {
+		this.serializedProperties = serializedProperties;
+	}
+
+	public ProlineDatabaseType getType() {
+		return type;
+	}
+
+	public void setType(final ProlineDatabaseType databaseType) {
+		type = databaseType;
+	}
+
+	public void setProjects(final Set<Project> pProjects) {
+		projects = pProjects;
+	}
+
+	public Set<Project> getProjects() {
+		return this.projects;
+	}
+
+	public void addProject(final Project project) {
+
+		if (project != null) {
+			Set<Project> localProjects = getProjects();
+
+			if (localProjects == null) {
+				localProjects = new HashSet<Project>();
+
+				setProjects(localProjects);
+			}
+
+			localProjects.add(project);
 		}
 
-	    }
+	}
 
-	    break;
+	public void removeProject(final Project project) {
+		final Set<Project> localProjects = getProjects();
 
-	case FILE:
-
-	    if (dt != DriverType.SQLITE) {
-		urlBuilder.append("file:");
-	    }
-
-	    if (!StringUtils.isEmpty(databasePathname)) {
-		urlBuilder.append(databasePathname);
-	    }
-
-	    break;
-
-	case HOST:
-
-	    if (dt == DriverType.H2) {
-		urlBuilder.append("tcp:");
-	    }
-
-	    urlBuilder.append("//");
-
-	    final String serverHostName = getHost();
-	    if (!StringUtils.isEmpty(serverHostName)) {
-		urlBuilder.append(serverHostName);
-	    }
-
-	    final Integer serverPort = getPort();
-	    if (serverPort != null) {
-		final int portValue = serverPort.intValue();
-
-		if ((0 < portValue) && (portValue <= MAX_PORT)) {
-		    urlBuilder.append(':').append(portValue);
+		if (localProjects != null) {
+			localProjects.remove(project);
 		}
 
-	    }
-
-	    urlBuilder.append('/');
-
-	    if (!StringUtils.isEmpty(databasePathname)) {
-		urlBuilder.append(databasePathname);
-	    }
-
-	    break;
 	}
 
-	LOG.debug("Database URL [{}]", urlBuilder);
-
-	properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_URL_KEY, urlBuilder.toString());
-
-	/* javax.persistence.jdbc.user */
-	boolean authent = false;
-
-	final String user = getDbUser();
-	if (!StringUtils.isEmpty(user)) {
-	    authent = true;
-	    properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_USER_KEY, user);
+	public ConnectionMode getConnectionMode() {
+		return connectionMode;
 	}
 
-	/* javax.persistence.jdbc.password */
-	final String password = getDbPassword();
-	if (StringUtils.isEmpty(password)) {
-
-	    if (authent) {
-		properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_PASSWORD_KEY, "");
-	    }
-
-	} else {
-	    properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_PASSWORD_KEY, password);
+	public void setConnectionMode(final ConnectionMode connectMode) {
+		connectionMode = connectMode;
 	}
 
-	return properties;
-    }
+	/**
+	 * Retrieves a standard JDBC properties Map from this <code>ExternalDb</code>.
+	 * <p>
+	 * Note JDBC URL are built as a basic string, without specific URI rules for character quotation and
+	 * encoding.
+	 * 
+	 * @return Properties Map usable to build a database connector.
+	 */
+	public Map<Object, Object> toPropertiesMap() {
+		final DriverType dt = getDriverType();
 
-    /**
-     * Retrieves a standard JDBC properties Map from this <code>ExternalDb</code>.
-     * 
-     * @param dt
-     *            <code>DriverType</code> (H2, POSTGRESQL, SQLITE).
-     * @return Properties Map usable to build a database connector.
-     */
-    public Map<Object, Object> toPropertiesMap(final DriverType dt) {
+		if (dt == null) {
+			throw new IllegalArgumentException("DriverType is null");
+		}
 
-	if (dt != null) {
-	    setDriverType(dt);
+		final Map<Object, Object> properties = new HashMap<Object, Object>();
+
+		/* javax.persistence.jdbc.driver */
+		properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_DRIVER_KEY, dt.getJdbcDriver());
+
+		/* Build javax.persistence.jdbc.url */
+		final StringBuilder urlBuilder = new StringBuilder(URL_BUFFER_SIZE);
+
+		urlBuilder.append(AbstractDatabaseConnector.JDBC_SCHEME).append(':');
+		urlBuilder.append(driverType.name().toLowerCase()).append(':');
+
+		final String databasePathname = getDbName();
+
+		switch (getConnectionMode()) {
+		case MEMORY:
+
+			if (dt == DriverType.SQLITE) {
+				/* Char needed before memory => http://www.sqlite.org/inmemorydb.html */
+				urlBuilder.append(":memory:");
+			} else {
+				urlBuilder.append("mem:");
+
+				if (!StringUtils.isEmpty(databasePathname)) {
+					urlBuilder.append(databasePathname);
+				}
+
+			}
+
+			break;
+
+		case FILE:
+
+			if (dt != DriverType.SQLITE) {
+				urlBuilder.append("file:");
+			}
+
+			if (!StringUtils.isEmpty(databasePathname)) {
+				urlBuilder.append(databasePathname);
+			}
+
+			break;
+
+		case HOST:
+
+			if (dt == DriverType.H2) {
+				urlBuilder.append("tcp:");
+			}
+
+			urlBuilder.append("//");
+
+			final String serverHostName = getHost();
+			if (!StringUtils.isEmpty(serverHostName)) {
+				urlBuilder.append(serverHostName);
+			}
+
+			final Integer serverPort = getPort();
+			if (serverPort != null) {
+				final int portValue = serverPort.intValue();
+
+				if ((0 < portValue) && (portValue <= MAX_PORT)) {
+					urlBuilder.append(':').append(portValue);
+				}
+
+			}
+
+			urlBuilder.append('/');
+
+			if (!StringUtils.isEmpty(databasePathname)) {
+				urlBuilder.append(databasePathname);
+			}
+
+			break;
+		}
+
+		LOG.debug("Database URL [{}]", urlBuilder);
+
+		properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_URL_KEY, urlBuilder.toString());
+
+		/* javax.persistence.jdbc.user */
+		boolean authent = false;
+
+		final String user = getDbUser();
+		if (!StringUtils.isEmpty(user)) {
+			authent = true;
+			properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_USER_KEY, user);
+		}
+
+		/* javax.persistence.jdbc.password */
+		final String password = getDbPassword();
+		if (StringUtils.isEmpty(password)) {
+
+			if (authent) {
+				properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_PASSWORD_KEY, "");
+			}
+
+		} else {
+			properties.put(AbstractDatabaseConnector.PERSISTENCE_JDBC_PASSWORD_KEY, password);
+		}
+
+		return properties;
 	}
 
-	return toPropertiesMap();
-    }
+	/**
+	 * Retrieves a standard JDBC properties Map from this <code>ExternalDb</code>.
+	 * 
+	 * @param dt
+	 *            <code>DriverType</code> (H2, POSTGRESQL, SQLITE).
+	 * @return Properties Map usable to build a database connector.
+	 */
+	public Map<Object, Object> toPropertiesMap(final DriverType dt) {
+
+		if (dt != null) {
+			setDriverType(dt);
+		}
+
+		return toPropertiesMap();
+	}
 
 }
