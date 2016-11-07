9aae16b5906527de8432482c6af1839c84b6e6f0
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-11-07 15:03:17 +0000

    #15519 : Remove unneeded script migration
    Former-commit-id: 8374a26c1e6f1aa3190bed5c58eb04a03a66db92

--- Proline-Databases/src/main/resources/dbscripts/msi/h2/V0_9__scoring_enum.sql
+++
@@ -1,6 +0,0 @@
-
-/* ADDITIONAL SQL QUERIES USED FOR DATA UPDATE */
--- Add Missing scoring 
-
-INSERT INTO scoring(search_engine,name,description) values ('maxquant','score','The score provided by MaxQuant for a peptide match.' );
-  
