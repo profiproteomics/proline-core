3ef60f82aec279060752b1af9dd923de023a0574
David Bouyssié <David.Bouyssie@ipbs.fr>
2017-04-05 11:48:52 +0000

    [Proline-OM] Add isSetAutomatically/isSetManually methods to SelectionLevel singleton + add value class
    refs #16473
    
    
    Former-commit-id: 9423057f620770cc7e48386f71510b7c30dc491f

--- Proline-OM/src/main/scala/fr/proline/core/om/model/SelectionLevel.scala
+++ Proline-OM/src/main/scala/fr/proline/core/om/model/SelectionLevel.scala
@@ -6,6 +6,20 @@ object SelectionLevel {
   val SELECTED_AUTO = 2
   val SELECTED_MANUAL = 3
   
-  def isSelected(selectionLevel: Int): Boolean = selectionLevel >= 2
-  def isDeselected(selectionLevel: Int): Boolean = selectionLevel <= 1
+  @inline def isSetAutomatically(selectionLevel: Int): Boolean = {
+    selectionLevel == DESELECTED_AUTO || selectionLevel == SELECTED_AUTO
+  }
+  @inline def isSetManually(selectionLevel: Int): Boolean = {
+    selectionLevel == DESELECTED_MANUAL || selectionLevel == SELECTED_MANUAL
+  }
+  @inline def isSelected(selectionLevel: Int): Boolean = selectionLevel >= 2
+  @inline def isDeselected(selectionLevel: Int): Boolean = selectionLevel <= 1
+}
+
+// TODO: use this value class in Proline-OM classes and check if serialization works
+class SelectionLevel(val value: Int) extends AnyVal {
+  def isSetAutomatically(): Boolean = SelectionLevel.isSetAutomatically(value)
+  def isSetManually(): Boolean = SelectionLevel.isSetManually(value)
+  def isSelected(): Boolean = SelectionLevel.isSelected(value)
+  def isDeselected(): Boolean = SelectionLevel.isDeselected(value)
 }
