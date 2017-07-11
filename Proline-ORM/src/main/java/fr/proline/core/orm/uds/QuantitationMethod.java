c5aaffb1e5f2a207e9e0cde8c80f065c05e8a673
Veronique Dupierris <veronique.dupierris@cea.fr>
2017-07-11 12:44:38 +0000

    [Core] #15079 refactoring SpectralCount . Add method to get QuantitationMethod
    Former-commit-id: 5c7400d20e4c7aa786e03b95e6fe869de1f6a1b0

--- Proline-ORM/src/main/java/fr/proline/core/orm/uds/QuantitationMethod.java
+++ Proline-ORM/src/main/java/fr/proline/core/orm/uds/QuantitationMethod.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -16,6 +18,10 @@ import javax.persistence.Table;
  * 
  */
 @Entity
+@NamedQueries({
+	@NamedQuery(name = "findQuantMethodForTypeAndUnit", query = "select qm from fr.proline.core.orm.uds.QuantitationMethod qm"
+		+ " where (upper(qm.type) = :searchType) and (upper(qm.abundanceUnit) = :searchAbundanceUnit)")
+})
 @Table(name = "quant_method")
 public class QuantitationMethod implements Serializable {
 
---
+++ Proline-ORM/src/main/java/fr/proline/core/orm/uds/repository/QuantitationMethodRepository.java
@@ -0,0 +1,54 @@
+package fr.proline.core.orm.uds.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.TypedQuery;
+
+import fr.profi.util.StringUtils;
+import fr.proline.core.orm.uds.QuantitationMethod;
+import fr.proline.repository.util.JPAUtils;
+
+
+public class QuantitationMethodRepository {
+
+   private QuantitationMethodRepository() {
+    }
+
+    public static QuantitationMethod findQuantMethodForTypeAndAbundanceUnit(final EntityManager udsEm,
+	    final String typeName, final String abundanceUnit) {
+
+    	JPAUtils.checkEntityManager(udsEm);
+
+    	if (StringUtils.isEmpty(typeName)) {
+    	    throw new IllegalArgumentException("Invalid type name");
+    	}
+
+    	if (StringUtils.isEmpty(abundanceUnit)) {
+    	    throw new IllegalArgumentException("Invalid abundanceUnit");
+    	}
+    	
+    	QuantitationMethod result = null;
+
+    	TypedQuery<QuantitationMethod> query = udsEm.createNamedQuery("findQuantMethodForTypeAndUnit", QuantitationMethod.class);
+	    query.setParameter("searchType", typeName.toUpperCase());
+	    query.setParameter("searchAbundanceUnit", abundanceUnit.toUpperCase());
+	    
+	    final List<QuantitationMethod> quantMethods = query.getResultList();
+
+		if ((quantMethods != null) && !quantMethods.isEmpty()) {
+
+		    if (quantMethods.size() == 1) {
+		    	result = quantMethods.get(0);
+		    } else {
+		    	throw new NonUniqueResultException("There are more than one QuantitationMethod for given type and abundanceUnit");
+		    }
+
+		}
+
+		return result;
+	}
+    	
+    
+}
