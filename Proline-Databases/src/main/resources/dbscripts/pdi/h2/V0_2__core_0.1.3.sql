5a95eb1291dbd398a34b305fdeb9e40aacee5924
Veronique Dupierris <veronique.dupierris@cea.fr>
2018-09-28 09:46:29 +0000

    [Core] Merge RemPS on TRUNK rem dirs
    Former-commit-id: c88bfa06a78f3c621824085afb8287c4f245a296

--- Proline-Databases/src/main/resources/dbscripts/pdi/h2/V0_2__core_0.1.3.sql
+++
@@ -1,5 +0,0 @@
-
-ALTER TABLE public.bio_sequence ALTER COLUMN length INTEGER NOT NULL;
-COMMENT ON COLUMN public.bio_sequence.length IS 'The length of the sequence.';
-
-ALTER TABLE public.seq_db_instance ALTER COLUMN sequence_count INTEGER NOT NULL;
