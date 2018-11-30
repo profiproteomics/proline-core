db56745abdd916d477506d14f8351fef3217eea8
Veronique Dupierris <veronique.dupierris@cea.fr>
2018-11-30 11:01:25 +0000

    [DAL] Add Aggregation type
    Former-commit-id: 31e22028f0f0907eb9232a7cbfe48c669d391926

--- Proline-DAL/src/main/scala/fr/proline/core/om/provider/msq/impl/SQLQuantConfigProvider.scala
+++ Proline-DAL/src/main/scala/fr/proline/core/om/provider/msq/impl/SQLQuantConfigProvider.scala
@@ -1,5 +1,7 @@
 package fr.proline.core.om.provider.msq.impl
 
+import java.util.NoSuchElementException
+
 import fr.profi.util.serialization.ProfiJson
 import fr.proline.context.UdsDbConnectionContext
 import fr.proline.core.algo.msq.config._
@@ -10,7 +12,7 @@ import fr.proline.core.dal.tables.uds.UdsDbDataSetObjectTreeMapTable
 import fr.proline.core.dal.tables.uds.UdsDbObjectTreeTable
 import fr.proline.core.om.model.msq._
 import fr.proline.core.om.provider.msq.IQuantConfigProvider
-import fr.proline.core.orm.uds.ObjectTreeSchema.{ SchemaName => ObjectTreeSchemaName }
+import fr.proline.core.orm.uds.ObjectTreeSchema.{SchemaName => ObjectTreeSchemaName}
 
 class SQLQuantConfigProvider(val udsDbCtx: UdsDbConnectionContext) extends IQuantConfigProvider {
   
@@ -31,6 +33,7 @@ class SQLQuantConfigProvider(val udsDbCtx: UdsDbConnectionContext) extends IQuan
     val quantConfig: IQuantConfig = schemaName match {
       case LABEL_FREE_QUANT_CONFIG => ProfiJson.deserialize[LabelFreeQuantConfig](quantConfigAsStr)
       case ISOBARIC_TAGGING_QUANT_CONFIG => ProfiJson.deserialize[IsobaricTaggingQuantConfig](quantConfigAsStr)
+      case AGGREGATION_QUANT_CONFIG=> ProfiJson.deserialize[AggregationQuantConfig](quantConfigAsStr)
       case _ => throw new Exception("this quant method is not supported yet")
     }
     Some(quantConfig, quantMethod)
@@ -39,28 +42,47 @@ class SQLQuantConfigProvider(val udsDbCtx: UdsDbConnectionContext) extends IQuan
   def getQuantConfigAsString( quantitationId:Long ): Option[(String,ObjectTreeSchemaName, IQuantMethod)] = {
     
     DoJDBCReturningWork.withEzDBC(udsDbCtx) { udsEzDBC =>
-      
+
       val quantMethodOpt = quantMethodProvider.getQuantitationQuantMethod(quantitationId)
       if (quantMethodOpt.isEmpty) return None
-      
+
       val quantMethod = quantMethodOpt.get
-      
+
       val schemaName = quantMethod match {
         case LabelFreeQuantMethod => LABEL_FREE_QUANT_CONFIG
         case iqMethod: IsobaricTaggingQuantMethod => ISOBARIC_TAGGING_QUANT_CONFIG
         case _ => throw new Exception("this quant method is not supported yet")
       }
-      
+
       // Retrieve the object tree containing the quant config
       val objTreeQueryBuilder = new SelectQueryBuilder2(DsObjectTreeMapTable,ObjectTreeTable)
       val objTreeSqlQuery = objTreeQueryBuilder.mkSelectQuery( (t1,c1,t2,c2) => List(t2.CLOB_DATA) ->
         " WHERE "~ t1.DATA_SET_ID ~" = "~ quantitationId ~" AND "~ t1.OBJECT_TREE_ID ~" = "~ t2.ID ~
         " AND "~ t1.SCHEMA_NAME ~" = '"~ schemaName ~"'"
       )
-      
-      val quantConfigAsStr = udsEzDBC.selectString(objTreeSqlQuery)
-      Some( (quantConfigAsStr, schemaName, quantMethod) )
-      
+
+      try {
+        val quantConfigAsStr = udsEzDBC.selectString(objTreeSqlQuery)
+        Some((quantConfigAsStr, schemaName, quantMethod))
+      } catch {
+        case e: NoSuchElementException => {   //No objectTree found. In case of label free, test for aggregation params
+          if(schemaName.equals(LABEL_FREE_QUANT_CONFIG)) {
+            val objTreeSqlQuery2 = objTreeQueryBuilder.mkSelectQuery((t1, c1, t2, c2) => List(t2.CLOB_DATA) ->
+              " WHERE " ~ t1.DATA_SET_ID ~ " = " ~ quantitationId ~ " AND " ~ t1.OBJECT_TREE_ID ~ " = " ~ t2.ID ~
+                " AND " ~ t1.SCHEMA_NAME ~ " = '" ~ AGGREGATION_QUANT_CONFIG ~ "'"
+            )
+
+            try {
+              val quantConfigAsStr = udsEzDBC.selectString(objTreeSqlQuery2)
+              return Some((quantConfigAsStr, AGGREGATION_QUANT_CONFIG, quantMethod))
+            } catch {
+              case e: NoSuchElementException => return None
+            }
+          }
+          return None
+        }
+      }
+
     } // END of DoJDBCReturningWork
 
   }
