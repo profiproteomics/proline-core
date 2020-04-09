1e14355b8f6324cc7ad9334d44108788149d55a3
VeroDup <veronique.dupierris@cea.fr>
2020-04-09 11:25:35 +0200

    #21053 update MasterQuantPeptideProperties to be synchronized with information saved in quantitation process

--- proline-orm/src/main/java/fr/proline/core/orm/msi/dto/MasterQuantPeptideProperties.java
+++ proline-orm/src/main/java/fr/proline/core/orm/msi/dto/MasterQuantPeptideProperties.java
@@ -1,7 +1,6 @@
 package fr.proline.core.orm.msi.dto;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class MasterQuantPeptideProperties {
 
@@ -9,23 +8,41 @@ public class MasterQuantPeptideProperties {
 		super();
 	}
 
+	String m_discardingReason;
 	List<Long> m_mqProtSetIds;
-	HashMap<String, List<MasterQuantPeptideProfile>> m_mqPepProfileByGroupSetupNumber;
+	HashMap<String, MasterQuantPeptideProfile> m_mqPepProfileByGroupSetupNumber;
+	PepIonAbundanceSummarizingConfig m_pepIonAbSummarizingConfig;
 
-	public List<Long> getMQProtSetIds() {
+	public List<Long> getMqProtSetIds() {
 		return m_mqProtSetIds;
 	}
 
-	public void setMQProtSetIds(List<Long> mqProtSetIds) {
+	public void setMqProtSetIds(List<Long> mqProtSetIds) {
 		this.m_mqProtSetIds = mqProtSetIds;
 	}
 
-	public HashMap<String, List<MasterQuantPeptideProfile>> getMqProtSetProfilesByGroupSetupNumber() {
+	public String getDiscardingReason() {
+		return m_discardingReason;
+	}
+
+	public void setDiscardingReason(String discardingReason) {
+		this.m_discardingReason = discardingReason;
+	}
+
+	public HashMap<String, MasterQuantPeptideProfile> getMqPepProfileByGroupSetupNumber() {
 		return m_mqPepProfileByGroupSetupNumber;
 	}
 
-	public void setMqProtSetProfilesByGroupSetupNumber(HashMap<String, List<MasterQuantPeptideProfile>> mqPepProfileByGroupSetupNumber) {
+	public void setMqPepProfileByGroupSetupNumber(HashMap<String, MasterQuantPeptideProfile> mqPepProfileByGroupSetupNumber) {
 		this.m_mqPepProfileByGroupSetupNumber = mqPepProfileByGroupSetupNumber;
+	}
+
+	public PepIonAbundanceSummarizingConfig getMqPepIonAbundanceSummarizingConfig() {
+		return m_pepIonAbSummarizingConfig;
+	}
+
+	public void setMqPepIonAbundanceSummarizingConfig(PepIonAbundanceSummarizingConfig m_pepIonAbSummarizingConfig) {
+		this.m_pepIonAbSummarizingConfig = m_pepIonAbSummarizingConfig;
 	}
 
 	static class MasterQuantPeptideProfile {
@@ -43,5 +60,39 @@ public class MasterQuantPeptideProperties {
 		public void setRatios(List<ComputedRatio> ratios) {
 			this.ratios = ratios;
 		}
+	}
+
+	static class PepIonAbundanceSummarizingConfig {
+
+		String methodName;
+		Map<Long,Integer> mqPeptideIonSelLevelById;
+
+		public String getMethodName() {
+			return methodName;
+		}
+
+		public void setMethodName(String methodName) {
+			this.methodName = methodName;
+		}
+
+		public Map<Long,Integer> getmMqPeptideIonSelLevelById() {
+			return mqPeptideIonSelLevelById;
+		}
+
+		public void setMqPeptideIonSelLevelById(Map<Long,Integer> mqPepIonSelLevelByIds) {
+			mqPeptideIonSelLevelById = mqPepIonSelLevelByIds;
+		}
+
+		public List<Long> getSelectedMasterQuantPeptideIonIds() {
+			ArrayList<Long> selectedMQPepIonIds = new ArrayList<>();
+			Iterator<Long>  mqPepIonSelectionlevelIt= mqPeptideIonSelLevelById.keySet().iterator();
+			while (mqPepIonSelectionlevelIt.hasNext()){
+				Long nextMQPId = mqPepIonSelectionlevelIt.next();
+				if(mqPeptideIonSelLevelById.get(nextMQPId) >= 2)
+					selectedMQPepIonIds.add(nextMQPId);
+			}
+			return selectedMQPepIonIds;
+		}
+
 	}
 }
