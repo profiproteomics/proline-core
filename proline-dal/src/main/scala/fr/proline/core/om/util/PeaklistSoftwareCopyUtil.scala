2c5fea81b65a5eb1729ca8555d2daaca9e3f2e3e
VeroDup <veronique.dupierris@cea.fr>
2024-04-02 14:35:00 +0200

    Peaklist Software:  WART Until found better way to deal with regex and json serialisation( save  "" instead of null)

--- proline-dal/src/main/scala/fr/proline/core/om/util/PeaklistSoftwareCopyUtil.scala
+++ proline-dal/src/main/scala/fr/proline/core/om/util/PeaklistSoftwareCopyUtil.scala
@@ -1,7 +1,7 @@
 package fr.proline.core.om.util
 
 import fr.proline.context.{MsiDbConnectionContext, UdsDbConnectionContext}
-import fr.proline.core.dal.tables.msi.MsiDbPeaklistSoftwareTable
+import fr.proline.core.dal.tables.msi.{MsiDbPeaklistSoftwareColumns, MsiDbPeaklistSoftwareTable}
 import fr.proline.core.om.model.msi.PeaklistSoftware
 import fr.proline.core.om.provider.msi.impl.{SQLPeaklistSoftwareProvider => MsiSQLPklSoftProvider}
 import fr.proline.core.om.provider.uds.impl.{SQLPeaklistSoftwareProvider => UdsSQLPklSoftProvider}
@@ -29,23 +29,45 @@ object PeaklistSoftwareCopyUtil {
       if (pklSoft.properties.isDefined && pklSoft.properties.get.pifRegExp.isDefined) {
         propAsString = Some("{\"pif_reg_exp\":\"" + pklSoft.properties.get.pifRegExp.get + "\"}")
       }
-      // Then insert it in the current MSIdb
-      val jdbcWork = new JDBCWork {
 
-        override def execute(con: Connection) {
+      //VDS TODO WART Until found better way to deal with regex and json serialisation
+      if(propAsString.isDefined) {
+        // Then insert it in the current MSIdb
+        val jdbcWork = new JDBCWork {
 
-          val pStmt = con.prepareStatement("insert into " + MsiDbPeaklistSoftwareTable.name + " (" + MsiDbPeaklistSoftwareTable.columnsAsStrList.mkString(",") + ") VALUES (?,?,?,?)")
-          pStmt.setLong(1, pklSoft.id)
-          pStmt.setString(2, pklSoft.name)
-          pStmt.setString(3, pklSoft.version)
-          pStmt.setString(4, (if(propAsString.isDefined) propAsString.get else ""))
-          pStmt.executeUpdate()
-          pStmt.close()
-        }
+          override def execute(con: Connection) {
 
-      } // End of jdbcWork anonymous inner class
+            val pStmt = con.prepareStatement("insert into " + MsiDbPeaklistSoftwareTable.name + " (" + MsiDbPeaklistSoftwareTable.columnsAsStrList.mkString(",") + ") VALUES (?,?,?,?)")
+            pStmt.setLong(1, pklSoft.id)
+            pStmt.setString(2, pklSoft.name)
+            pStmt.setString(3, pklSoft.version)
+            pStmt.setString(4, propAsString.get)
+            pStmt.executeUpdate()
+            pStmt.close()
+          }
 
-      msiDbCtx.doWork(jdbcWork, false)
+        } // End of jdbcWork anonymous inner class
+
+        msiDbCtx.doWork(jdbcWork, false)
+      } else {
+        // Then insert it in the current MSIdb
+        val jdbcWork = new JDBCWork {
+
+          override def execute(con: Connection) {
+
+            val pStmt = con.prepareStatement("insert into " + MsiDbPeaklistSoftwareTable.name + " (" + MsiDbPeaklistSoftwareTable.columnsAsStrList.filter(_ != MsiDbPeaklistSoftwareTable.columns.SERIALIZED_PROPERTIES).mkString(",") + ") VALUES (?,?,?)")
+            pStmt.setLong(1, pklSoft.id)
+            pStmt.setString(2, pklSoft.name)
+            pStmt.setString(3, pklSoft.version)
+            pStmt.executeUpdate()
+            pStmt.close()
+          }
+
+        } // End of jdbcWork anonymous inner class
+
+        msiDbCtx.doWork(jdbcWork, false)
+      }
+
     }
 
     udsPklSoftOpt.get
