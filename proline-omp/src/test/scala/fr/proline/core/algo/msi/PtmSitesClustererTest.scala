1f49bb042ceaecd73f94d6b338ac8e6d9c54185b
vd225637 <veronique.dupierris@cea.fr>
2026-06-23 13:53:24 +0200

    Added some test to verify clustering result

--- proline-omp/src/test/scala/fr/proline/core/algo/msi/PtmSitesClustererTest.scala
+++ proline-omp/src/test/scala/fr/proline/core/algo/msi/PtmSitesClustererTest.scala
@@ -54,7 +54,7 @@ class PtmSitesClustererTest extends StrictLogging {
   }
 
 
-  private def _getClusters(ptmSites2 : Array[PtmSite2], rsm : ResultSummary): Array[PtmCluster] ={
+  private def _getClusters(ptmSites2 : Array[PtmSite2], rsm : ResultSummary, clusterizeMC : Boolean = true): Array[PtmCluster] ={
     val ptmIds = Array(50L, 51L, 52L)
     val sitesByProteinMatchIds = ptmSites2.filter{ s =>  ptmIds.contains(s.ptmDefinitionId) }.groupBy(_.proteinMatchId)
 
@@ -63,7 +63,7 @@ class PtmSitesClustererTest extends StrictLogging {
       peptideMatches.map( pm => pm.id -> pm).toMap
     }
 
-    val clusterizer = new PtmSiteExactClusterer(rsm,rsm.resultSet.get.proteinMatches)
+    val clusterizer = new PtmSiteExactClusterer(rsm,rsm.resultSet.get.proteinMatches, clusterizePartiallyIsomorphicPep=clusterizeMC)
     sitesByProteinMatchIds.flatMap{ case(protMatchId, sites) => clusterizer.clusterize(protMatchId, sites, _getPeptideMatchesByPeptideIds, IdGenerator) }.toArray
   }
 
@@ -113,7 +113,7 @@ class PtmSitesClustererTest extends StrictLogging {
 
     val clusters = _getClusters(ptmSites2, rsm)
 
-    assert(clusters.length == 7)
+    assert(clusters.length == 8)
   }
 
   @Test
@@ -137,7 +137,69 @@ class PtmSitesClustererTest extends StrictLogging {
 //    val clusterizer = new PtmSiteExactClusterer(rsm,rsm.resultSet.get.proteinMatches)
 //    val clusters = sitesByProteinMatchIds.flatMap{ case(protMatchId, sites) => clusterizer.clusterize(protMatchId, sites, _getPeptideMatchesByPeptideIds, IdGenerator) }
 
-    assert(clusters.length == 7)
+    assert(clusters.length == 8)
+  }
+
+  @Test
+  def identifyFromFakeRsV3(): Unit = {
+
+    val rsm = _createFakeRsm()
+    val pep5 = rsm.peptideInstances.filter(pi => pi.peptide.sequence.equals("LVSPQVTK")).head
+    val pep0 = rsm.peptideInstances.filter(pi => pi.peptide.sequence.equals("HAAFSLVSPQVTKASPGR") && (pi.peptide.ptms.length == 3)).head
+    val pep2 = rsm.peptideInstances.filter(pi => pi.peptide.sequence.equals("HAAFSLVSPQVTKASPGR") && (pi.peptide.ptms.length == 2) && (pi.peptide.ptms.toList.filter(lptm => lptm.seqPosition == 8).size > 0)).head
+    assert(pep5 != null)
+    assert(pep0 != null)
+    assert(pep2 != null)
+
+    val ptmSites = new PtmSitesIdentifier(rsm, rsm.resultSet.get.proteinMatches).identifyPtmSites()
+    val ptmSites2 = RsmPtmSitesIdentifierV2.toPtmSites2(ptmSites)
+    assert(ptmSites2.length == 7)
+
+    val clusterizeMC = false
+    val clusters = _getClusters(ptmSites2, rsm, clusterizeMC)
+    assert(clusters.length == 8)
+    // -- no more OK for new pep list
+    var foundC0 = false
+    var foundC2 = false
+    var foundC3 = false
+    var foundErr = false
+    if(clusterizeMC) {
+      for (c <- clusters) {
+        if (c.peptideIds.contains(pep5.peptide.id)) {
+          if (c.peptideIds.length == 2 && c.peptideIds.contains(pep0.peptide.id)) {
+            foundC0 = true
+          } else if (c.peptideIds.length == 4 && c.peptideIds.contains(pep2.peptide.id)) {
+            foundC2 = true
+          } else if (c.peptideIds.length == 3) {
+            foundC3 = true
+          } else
+            foundErr = true
+        }
+      }
+    } else {
+      for (c <- clusters) {
+        if (c.peptideIds.contains(pep5.peptide.id)) {
+          if (c.peptideIds.length == 3 ) {
+            foundC3 = true
+          } else
+            foundErr = true
+        } else if (c.peptideIds.contains(pep0.peptide.id)) {
+          if (c.peptideIds.length == 1) {
+            foundC0 = true
+          } else
+            foundErr = true
+        } else if (c.peptideIds.contains(pep2.peptide.id)) {
+          if (c.peptideIds.length == 1) {
+            foundC2 = true
+          } else
+            foundErr = true
+        }
+      }
+    }
+    assert(foundC0)
+    assert(foundC2)
+    assert(foundC3)
+    assert(!foundErr)
   }
 
   private def _createFakeRsm() : ResultSummary = {
@@ -168,6 +230,17 @@ class PtmSitesClustererTest extends StrictLogging {
     rsb.addPeptide(pepSeq = "CVAVAR", proteinMatch = proteinMatch)
     rsb.addPeptide(pepSeq = "CVAVARR", proteinMatch = proteinMatch)
 
+    //S5 -> Prot position 494
+    //S8 -> Prot position 497
+    //S15 -> Prot position 504
+
+    rsb.addPeptide(
+      pepSeq = "HAAFSLVSPQVTKASPGR",
+      ptms = _buildLocatedPtms(Array("S(5)","S(8)", "S(15)")),
+      probabilities = Array(0.98f,0.79f,  0.88f),
+      proteinMatch = proteinMatch
+    )
+
     rsb.addPeptide(
       pepSeq = "HAAFSLVSPQVTKASPGR",
       ptms = _buildLocatedPtms(Array("S(5)", "S(15)")),
@@ -193,6 +266,22 @@ class PtmSitesClustererTest extends StrictLogging {
       pepSeq = "HAAFSLVSPQVTK",
       ptms = _buildLocatedPtms(Array("S(8)")),
       probabilities = Array(0.98f),
+      proteinMatch = proteinMatch
+    )
+
+    //Fake Pep (no cleavage site) or cluster test...
+    rsb.addPeptide(
+      pepSeq = "LVSPQVTK",
+      ptms = _buildLocatedPtms(Array("S(3)")),
+      probabilities = Array(0.98f),
+      proteinMatch = proteinMatch
+    )
+
+    //Fake Pep (no cleavage site) or cluster test...
+    rsb.addPeptide(
+      pepSeq = "HAAFSLVSPQV",
+      ptms = _buildLocatedPtms(Array("S(8)")),
+      probabilities = Array(0.87f),
       proteinMatch = proteinMatch
     )
 
